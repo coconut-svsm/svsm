8d86d4df8a1b13e13f03bc8f575541a5a03ed632
Joerg Roedel <jroedel@suse.de>
2022-07-22 10:15:50 +0200

    SVSM: Add page_align() and page_align_up() helpers
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,17 @@
 use super::msr::{read_msr, write_msr, EFER};
+use crate::types::PAGE_SIZE;
 use core::arch::asm;
 
 pub fn align_up(addr : usize, align: usize) -> usize {
 	addr + (align -1) & !(align - 1)
+}
+
+pub fn page_align(addr : usize) -> usize {
+	addr & !(PAGE_SIZE - 1)
+}
+
+pub fn page_align_up(addr : usize) -> usize {
+	(addr + PAGE_SIZE - 1) & !(PAGE_SIZE - 1)
 }
 
 bitflags! {
