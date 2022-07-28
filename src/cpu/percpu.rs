4eacca53c1542275bf577f7d8182f70e29959f88
Joerg Roedel <jroedel@suse.de>
2022-07-28 10:35:33 +0200

    SVSM: Allocate an IST stack for the double-fault handler
    The double-fault handler will catch stack-overflows, but to do so it
    needs to be an IST vector. Allocate a stack for it.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- src/cpu/percpu.rs
+++ src/cpu/percpu.rs
@@ -7,32 +7,57 @@
 // vim: ts=4 sw=4 et
 
 use crate::cpu::msr::{write_msr, MSR_GS_BASE};
+use crate::mm::stack::allocate_stack;
 use crate::mm::alloc::allocate_page;
 use crate::sev::GHCB;
 use crate::types::VirtAddr;
 use core::ptr;
 use core::arch::asm;
 
+struct IstStacks {
+    double_fault_stack : VirtAddr,
+}
+
+impl IstStacks {
+    const fn new() -> Self {
+        IstStacks {
+            double_fault_stack : 0,
+        }
+    }
+
+    pub fn allocate_stacks(&mut self) -> Result<(), ()> {
+        self.double_fault_stack = allocate_stack()?;
+        Ok(())
+    }
+}
+
 pub struct PerCpu {
-    ghcb : *mut GHCB,
+    ghcb    : *mut GHCB,
+    ist     : IstStacks,
 }
 
 impl PerCpu {
     pub const fn new() -> Self {
         PerCpu {
-            ghcb : ptr::null_mut(),
+            ghcb    : ptr::null_mut(),
+            ist     : IstStacks::new(),
         }
     }
 
     pub fn setup(&mut self) -> Result<(), ()> {
+        // Setup GHCB
         let ghcb_page = allocate_page().expect("Failed to allocate GHCB page");
-
         self.ghcb = ghcb_page as *mut GHCB;
+        unsafe { (*self.ghcb).init()?; }
 
+        // Allocate IST stacks
+        self.ist.allocate_stacks()?;
+
+        // Write GS_BASE
         let gs_base : u64 = (self as *const PerCpu) as u64;
         write_msr(MSR_GS_BASE, gs_base);
 
-        unsafe { (*self.ghcb).init() }
+        Ok(())
     }
 
     pub fn shutdown(&mut self) -> Result<(), ()> {
