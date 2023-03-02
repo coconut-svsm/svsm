23345513c25f2d74ac455910a0887da185c622f5
Joerg Roedel <jroedel@suse.de>
2023-03-02 11:51:03 +0100

    SVSM/mm/memory: Check for VMSA addresses in valid_phys_address()
    Many requests in the core protocol need to check for valid physical
    addresses before proceeding. The SVSM spec defines in-use VMSAs as
    invalid, so check for those in valid_phys_address().
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- src/mm/memory.rs
+++ src/mm/memory.rs
@@ -8,6 +8,8 @@
 
 extern crate alloc;
 
+use crate::cpu::percpu::vmsa_exists;
+use crate::utils::page_align;
 use crate::types::PhysAddr;
 use crate::kernel_launch::KernelLaunchInfo;
 use crate::fw_cfg::{FwCfg, MemoryRegion};
@@ -40,8 +42,13 @@ pub fn init_memory_map(fwcfg: &FwCfg, launch_info: &KernelLaunchInfo) -> Result<
     Ok(())
 }
 
-pub fn valid_phys_address(addr: PhysAddr) -> bool {
-    let addr = addr as u64;
+pub fn valid_phys_address(paddr: PhysAddr) -> bool {
+    let page_addr = page_align(paddr);
+    let addr = paddr as u64;
+
+    if vmsa_exists(page_addr) {
+        return false;
+    }
 
     MEMORY_MAP.lock_read().iter()
         .any(|region| addr >= region.start && addr < region.end )
