7d4464898aaefb16236141bcb7464cb52807fbae
Nicolai Stange <nstange@suse.de>
2023-03-14 12:50:03 +0000

    mm/validate: fix index calculation in ValidBitmap::clear_all()
    The upper bound as calculated in ValidBitmap::clear_all() is in units of bitmap
    words, i.e. u32, while the subsequent ptr::writes_bytes() expects the write
    length to be specified in terms of octets. Fix this.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- src/mm/validate.rs
+++ src/mm/validate.rs
@@ -136,7 +136,10 @@ impl ValidBitmap {
     }
 
     pub fn clear_all(&mut self) {
-        let (i, _) = self.index(self.pend - 1);
+        let (mut i, bit) = self.index(self.pend);
+        if bit != 0 {
+            i += 1;
+        }
         let index: usize = i.try_into().unwrap();
 
         unsafe {
