9a078fc8e3503819d881fce55d75f484fc13a2b8
Nicolai Stange <nstange@suse.de>
2023-03-16 08:20:30 +0000

    Stage2, SVSM: pass kernel ELF file memory location to the SVSM kernel
    The SVSM kernel will need access to its ELF metadata in the future, for
    e.g. inferring the needed memory mappings or for getting access to its
    symbol table for pretty-printing stacktraces.
    
    The Stage2 has it mapped in memory and at early SVSM kernel execution,
    the Stage2 page tables are still in effect. Pass the ELF file memory
    location from the Stage2 loader to the SVSM kernel at handover so
    that the latter can grab all the information it needs from there before
    switching to its own runtime page tables.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- src/kernel_launch.rs
+++ src/kernel_launch.rs
@@ -11,6 +11,8 @@ pub struct KernelLaunchInfo {
     pub heap_area_phys_start: u64, // Start of trailing heap area within the physical memory region.
     pub kernel_region_virt_start: u64,
     pub heap_area_virt_start: u64, // Start of virtual heap area mapping.
+    pub kernel_elf_stage2_virt_start: u64, // Virtual address of kernel ELF in Stage2 mapping.
+    pub kernel_elf_stage2_virt_end: u64,
     pub cpuid_page: u64,
     pub secrets_page: u64,
 }
