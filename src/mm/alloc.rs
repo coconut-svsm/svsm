coconut-svsm/svsm

commit 581245bd3500d904be5563326524f236f6ac6b44

svsm/error: add error typing in the memory management subsystem
Swap the usual Result<_, ()> return type with Result<_, SvsmError> in
the memory management subsystem. For now, simply return
SvsmError::Mem, without giving more details. This can be improved in
the future by adding extra information in the error variant.

Unfortunately these APIs are used everywhere throughout the SVSM, so
this requires a lot of small adjustements to make error types
coherent.

Signed-off-by: Carlos López <carlos.lopez@suse.com>

src/mm/alloc.rs
@@ -4,6 +4,7 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use crate::error::SvsmError;
 use crate::locking::SpinLock;
 use crate::types::{PhysAddr, VirtAddr, PAGE_SHIFT, PAGE_SIZE};
 use crate::utils::{align_up, zero_mem_region};
@@ -283,9 +284,9 @@ impl MemoryRegion {
         Page::from_mem(info)
     }
 
-    pub fn get_page_info(&self, vaddr: VirtAddr) -> Result<Page, ()> {
+    pub fn get_page_info(&self, vaddr: VirtAddr) -> Result<Page, SvsmError> {
         if vaddr == 0 || !self.check_virt_addr(vaddr) {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         let pfn = (vaddr - self.start_virt) / PAGE_SIZE;
@@ -293,11 +294,11 @@ impl MemoryRegion {
         Ok(self.read_page_info(pfn))
     }
 
-    fn get_next_page(&mut self, order: usize) -> Result<usize, ()> {
+    fn get_next_page(&mut self, order: usize) -> Result<usize, SvsmError> {
         let pfn = self.next_page[order];
 
         if pfn == 0 {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         let pg = self.read_page_info(pfn);
@@ -329,9 +330,9 @@ impl MemoryRegion {
         }
     }
 
-    fn split_page(&mut self, pfn: usize, order: usize) -> Result<(), ()> {
+    fn split_page(&mut self, pfn: usize, order: usize) -> Result<(), SvsmError> {
         if order < 1 || order >= MAX_ORDER {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         let new_order = order - 1;
@@ -351,13 +352,13 @@ impl MemoryRegion {
         Ok(())
     }
 
-    fn refill_page_list(&mut self, order: usize) -> Result<(), ()> {
+    fn refill_page_list(&mut self, order: usize) -> Result<(), SvsmError> {
         if self.next_page[order] != 0 {
             return Ok(());
         }
 
         if order >= MAX_ORDER - 1 {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         self.refill_page_list(order + 1)?;
@@ -367,7 +368,7 @@ impl MemoryRegion {
         self.split_page(pfn, order + 1)
     }
 
-    pub fn allocate_pages(&mut self, order: usize) -> Result<VirtAddr, ()> {
+    pub fn allocate_pages(&mut self, order: usize) -> Result<VirtAddr, SvsmError> {
         self.refill_page_list(order)?;
         if let Ok(pfn) = self.get_next_page(order) {
             let pg = Page::Allocated(AllocatedInfo { order });
@@ -375,15 +376,15 @@ impl MemoryRegion {
             let vaddr = self.start_virt + (pfn * PAGE_SIZE);
             Ok(vaddr)
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
-    pub fn allocate_page(&mut self) -> Result<VirtAddr, ()> {
+    pub fn allocate_page(&mut self) -> Result<VirtAddr, SvsmError> {
         self.allocate_pages(0)
     }
 
-    pub fn allocate_zeroed_page(&mut self) -> Result<VirtAddr, ()> {
+    pub fn allocate_zeroed_page(&mut self) -> Result<VirtAddr, SvsmError> {
         let vaddr = self.allocate_page()?;
 
         zero_mem_region(vaddr, vaddr + PAGE_SIZE);
@@ -391,7 +392,7 @@ impl MemoryRegion {
         Ok(vaddr)
     }
 
-    pub fn allocate_slab_page(&mut self, slab: Option<VirtAddr>) -> Result<VirtAddr, ()> {
+    pub fn allocate_slab_page(&mut self, slab: Option<VirtAddr>) -> Result<VirtAddr, SvsmError> {
         self.refill_page_list(0)?;
 
         let slab_vaddr = slab.unwrap_or(0);
@@ -402,27 +403,27 @@ impl MemoryRegion {
             let vaddr = self.start_virt + (pfn * PAGE_SIZE);
             Ok(vaddr)
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
-    fn compound_neighbor(&self, pfn: usize, order: usize) -> Result<usize, ()> {
+    fn compound_neighbor(&self, pfn: usize, order: usize) -> Result<usize, SvsmError> {
         if order >= MAX_ORDER - 1 {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         assert_eq!(pfn & ((1usize << order) - 1), 0);
         let pfn = pfn ^ (1usize << order);
         if pfn >= self.page_count {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         Ok(pfn)
     }
 
-    fn merge_pages(&mut self, pfn1: usize, pfn2: usize, order: usize) -> Result<usize, ()> {
+    fn merge_pages(&mut self, pfn1: usize, pfn2: usize, order: usize) -> Result<usize, SvsmError> {
         if order >= MAX_ORDER - 1 {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         let nr_pages: usize = 1 << (order + 1);
@@ -456,13 +457,13 @@ impl MemoryRegion {
         }
     }
 
-    fn allocate_pfn(&mut self, pfn: usize, order: usize) -> Result<(), ()> {
+    fn allocate_pfn(&mut self, pfn: usize, order: usize) -> Result<(), SvsmError> {
         let first_pfn = self.next_page[order];
 
         // Handle special cases first
         if first_pfn == 0 {
             // No pages for that order
-            return Err(());
+            return Err(SvsmError::Mem);
         } else if first_pfn == pfn {
             // Requested pfn is first in list
             self.get_next_page(order).unwrap();
@@ -494,7 +495,7 @@ impl MemoryRegion {
             old_pfn = current_pfn;
         }
 
-        Err(())
+        Err(SvsmError::Mem)
     }
 
     fn free_page_raw(&mut self, pfn: usize, order: usize) {
@@ -510,13 +511,13 @@ impl MemoryRegion {
         self.free_pages[order] += 1;
     }
 
-    fn try_to_merge_page(&mut self, pfn: usize, order: usize) -> Result<usize, ()> {
+    fn try_to_merge_page(&mut self, pfn: usize, order: usize) -> Result<usize, SvsmError> {
         let neighbor_pfn = self.compound_neighbor(pfn, order)?;
         let neighbor_page = self.read_page_info(neighbor_pfn);
 
         if let Page::Free(fi) = neighbor_page {
             if fi.order != order {
-                return Err(());
+                return Err(SvsmError::Mem);
             }
 
             self.allocate_pfn(neighbor_pfn, order)?;
@@ -525,7 +526,7 @@ impl MemoryRegion {
 
             Ok(new_pfn)
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
@@ -624,19 +625,19 @@ pub fn print_memory_info(info: &MemInfo) {
 
 static ROOT_MEM: SpinLock<MemoryRegion> = SpinLock::new(MemoryRegion::new());
 
-pub fn allocate_page() -> Result<VirtAddr, ()> {
+pub fn allocate_page() -> Result<VirtAddr, SvsmError> {
     ROOT_MEM.lock().allocate_page()
 }
 
-pub fn allocate_pages(order: usize) -> Result<VirtAddr, ()> {
+pub fn allocate_pages(order: usize) -> Result<VirtAddr, SvsmError> {
     ROOT_MEM.lock().allocate_pages(order)
 }
 
-pub fn allocate_slab_page(slab: Option<VirtAddr>) -> Result<VirtAddr, ()> {
+pub fn allocate_slab_page(slab: Option<VirtAddr>) -> Result<VirtAddr, SvsmError> {
     ROOT_MEM.lock().allocate_slab_page(slab)
 }
 
-pub fn allocate_zeroed_page() -> Result<VirtAddr, ()> {
+pub fn allocate_zeroed_page() -> Result<VirtAddr, SvsmError> {
     ROOT_MEM.lock().allocate_zeroed_page()
 }
 
@@ -669,7 +670,11 @@ impl SlabPage {
         }
     }
 
-    pub fn init(&mut self, slab_vaddr: Option<VirtAddr>, mut item_size: u16) -> Result<(), ()> {
+    pub fn init(
+        &mut self,
+        slab_vaddr: Option<VirtAddr>,
+        mut item_size: u16,
+    ) -> Result<(), SvsmError> {
         if self.item_size != 0 {
             return Ok(());
         }
@@ -687,7 +692,7 @@ impl SlabPage {
             self.capacity = (PAGE_SIZE as u16) / item_size;
             self.free = self.capacity;
         } else {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         Ok(())
@@ -717,9 +722,9 @@ impl SlabPage {
         self.next_page = next_page;
     }
 
-    pub fn allocate(&mut self) -> Result<VirtAddr, ()> {
+    pub fn allocate(&mut self) -> Result<VirtAddr, SvsmError> {
         if self.free == 0 {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         for i in 0..self.capacity {
@@ -734,12 +739,12 @@ impl SlabPage {
             }
         }
 
-        Err(())
+        Err(SvsmError::Mem)
     }
 
-    pub fn free(&mut self, vaddr: VirtAddr) -> Result<(), ()> {
+    pub fn free(&mut self, vaddr: VirtAddr) -> Result<(), SvsmError> {
         if vaddr < self.vaddr || vaddr >= self.vaddr + PAGE_SIZE {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         assert!(self.item_size > 0);
@@ -781,9 +786,9 @@ impl SlabCommon {
         }
     }
 
-    fn init(&mut self, slab_vaddr: Option<VirtAddr>) -> Result<(), ()> {
+    fn init(&mut self, slab_vaddr: Option<VirtAddr>) -> Result<(), SvsmError> {
         if let Err(_e) = self.page.init(slab_vaddr, self.item_size) {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         self.capacity = self.page.get_capacity() as u32;
@@ -880,15 +885,13 @@ impl SlabPageSlab {
         }
     }
 
-    fn init(&mut self) -> Result<(), ()> {
+    fn init(&mut self) -> Result<(), SvsmError> {
         self.common.init(None)
     }
 
-    fn grow_slab(&mut self) -> Result<(), ()> {
+    fn grow_slab(&mut self) -> Result<(), SvsmError> {
         if self.common.capacity == 0 {
-            if let Err(_e) = self.init() {
-                return Err(());
-            }
+            self.init()?;
             return Ok(());
         }
 
@@ -904,7 +907,7 @@ impl SlabPageSlab {
         *slab_page = SlabPage::new();
         if let Err(_e) = slab_page.init(None, self.common.item_size) {
             self.common.deallocate_slot(page_vaddr);
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         self.common.add_slab_page(slab_page);
@@ -942,9 +945,9 @@ impl SlabPageSlab {
         }
     }
 
-    fn allocate(&mut self) -> Result<*mut SlabPage, ()> {
+    fn allocate(&mut self) -> Result<*mut SlabPage, SvsmError> {
         if self.grow_slab().is_err() {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         Ok(unsafe { &mut *(self.common.allocate_slot() as *mut SlabPage) })
@@ -967,15 +970,15 @@ impl Slab {
         }
     }
 
-    fn init(&mut self) -> Result<(), ()> {
+    fn init(&mut self) -> Result<(), SvsmError> {
         let slab_vaddr = (self as *mut Slab) as VirtAddr;
         self.common.init(Some(slab_vaddr))
     }
 
-    fn grow_slab(&mut self) -> Result<(), ()> {
+    fn grow_slab(&mut self) -> Result<(), SvsmError> {
         if self.common.capacity == 0 {
             if let Err(_e) = self.init() {
-                return Err(());
+                return Err(SvsmError::Mem);
             }
             return Ok(());
         }
@@ -986,13 +989,13 @@ impl Slab {
 
         let slab_page = match SLAB_PAGE_SLAB.lock().allocate() {
             Ok(slab_page) => unsafe { &mut *slab_page },
-            Err(_) => return Err(()),
+            Err(_) => return Err(SvsmError::Mem),
         };
         let slab_vaddr = (self as *mut Slab) as VirtAddr;
         *slab_page = SlabPage::new();
         if let Err(_e) = slab_page.init(Some(slab_vaddr), self.common.item_size) {
             SLAB_PAGE_SLAB.lock().deallocate(slab_page);
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         self.common.add_slab_page(&mut *slab_page);
@@ -1033,9 +1036,9 @@ impl Slab {
         assert!(freed_one);
     }
 
-    fn allocate(&mut self) -> Result<VirtAddr, ()> {
+    fn allocate(&mut self) -> Result<VirtAddr, SvsmError> {
         if let Err(_e) = self.grow_slab() {
-            return Err(());
+            return Err(SvsmError::Mem);
         }
 
         Ok(self.common.allocate_slot())
@@ -1075,7 +1078,7 @@ impl SvsmAllocator {
 
 unsafe impl GlobalAlloc for SvsmAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let ret: Result<VirtAddr, ()>;
+        let ret: Result<VirtAddr, SvsmError>;
         let size = layout.size();
 
         if size <= 32 {
@@ -1177,7 +1180,7 @@ use crate::locking::LockGuard;
 #[cfg(test)]
 // Allocate a memory region from the standard Rust allocator and pass it to
 // root_mem_init().
-fn setup_test_root_mem(size: usize) -> LockGuard<'static, ()> {
+fn setup_test_root_mem(size: usize) -> LockGuard<'static, SvsmError> {
     extern crate alloc;
     use alloc::alloc::{alloc, handle_alloc_error};
 
@@ -1199,7 +1202,7 @@ fn setup_test_root_mem(size: usize) -> LockGuard<'static, ()> {
 
 #[cfg(test)]
 // Undo the setup done from setup_test_root_mem().
-fn destroy_test_root_mem(lock: LockGuard<'static, ()>) {
+fn destroy_test_root_mem(lock: LockGuard<'static, SvsmError>) {
     extern crate alloc;
     use alloc::alloc::dealloc;
 

src/cpu/percpu.rs
@@ -10,6 +10,7 @@ use super::gdt::load_tss;
 use super::tss::{X86Tss, IST_DF};
 use crate::cpu::tss::TSS_LIMIT;
 use crate::cpu::vmsa::init_guest_vmsa;
+use crate::error::SvsmError;
 use crate::locking::{LockGuard, RWLock, SpinLock};
 use crate::mm::alloc::{allocate_page, allocate_zeroed_page};
 use crate::mm::pagetable::{get_init_pgtable_locked, PageTable, PageTableRef};
@@ -196,7 +197,7 @@ impl PerCpu {
         }
     }
 
-    pub fn alloc(apic_id: u32) -> Result<*mut PerCpu, ()> {
+    pub fn alloc(apic_id: u32) -> Result<*mut PerCpu, SvsmError> {
         let vaddr = allocate_zeroed_page()?;
         unsafe {
             let percpu = vaddr as *mut PerCpu;
@@ -219,7 +220,7 @@ impl PerCpu {
         self.apic_id
     }
 
-    fn allocate_page_table(&mut self) -> Result<(), ()> {
+    fn allocate_page_table(&mut self) -> Result<(), SvsmError> {
         let pgtable_ref = get_init_pgtable_locked().clone_shared()?;
         self.set_pgtable(pgtable_ref);
         Ok(())
@@ -230,14 +231,14 @@ impl PerCpu {
         *my_pgtable = pgtable;
     }
 
-    fn allocate_init_stack(&mut self) -> Result<(), ()> {
+    fn allocate_init_stack(&mut self) -> Result<(), SvsmError> {
         allocate_stack_addr(SVSM_STACKS_INIT_TASK, &mut self.get_pgtable())
             .expect("Failed to allocate per-cpu init stack");
         self.init_stack = Some(SVSM_STACKS_INIT_TASK);
         Ok(())
     }
 
-    fn allocate_ist_stacks(&mut self) -> Result<(), ()> {
+    fn allocate_ist_stacks(&mut self) -> Result<(), SvsmError> {
         allocate_stack_addr(SVSM_STACK_IST_DF_BASE, &mut self.get_pgtable())
             .expect("Failed to allocate percpu double-fault stack");
 
@@ -249,14 +250,14 @@ impl PerCpu {
         self.pgtbl.lock()
     }
 
-    pub fn setup_ghcb(&mut self) -> Result<(), ()> {
+    pub fn setup_ghcb(&mut self) -> Result<(), SvsmError> {
         let ghcb_page = allocate_page().expect("Failed to allocate GHCB page");
         self.ghcb = ghcb_page as *mut GHCB;
-        unsafe { (*self.ghcb).init().map_err(|_| ()) }
+        unsafe { (*self.ghcb).init() }
     }
 
-    pub fn register_ghcb(&self) -> Result<(), ()> {
-        unsafe { self.ghcb.as_ref().unwrap().register().map_err(|_| ()) }
+    pub fn register_ghcb(&self) -> Result<(), SvsmError> {
+        unsafe { self.ghcb.as_ref().unwrap().register() }
     }
 
     pub fn get_top_of_stack(&self) -> VirtAddr {
@@ -267,7 +268,7 @@ impl PerCpu {
         self.tss.ist_stacks[IST_DF] = stack_base_pointer(self.ist.double_fault_stack.unwrap());
     }
 
-    pub fn map_self(&mut self) -> Result<(), ()> {
+    pub fn map_self(&mut self) -> Result<(), SvsmError> {
         let vaddr = (self as *const PerCpu) as VirtAddr;
         let paddr = virt_to_phys(vaddr);
         let flags = PageTable::data_flags();
@@ -275,7 +276,7 @@ impl PerCpu {
         self.get_pgtable().map_4k(SVSM_PERCPU_BASE, paddr, flags)
     }
 
-    pub fn setup(&mut self) -> Result<(), ()> {
+    pub fn setup(&mut self) -> Result<(), SvsmError> {
         // Allocate page-table
         self.allocate_page_table()?;
 
@@ -298,7 +299,7 @@ impl PerCpu {
     }
 
     // Setup code which needs to run on the target CPU
-    pub fn setup_on_cpu(&self) -> Result<(), ()> {
+    pub fn setup_on_cpu(&self) -> Result<(), SvsmError> {
         self.register_ghcb()
     }
 
@@ -315,12 +316,12 @@ impl PerCpu {
         self.load_tss();
     }
 
-    pub fn shutdown(&mut self) -> Result<(), ()> {
+    pub fn shutdown(&mut self) -> Result<(), SvsmError> {
         if self.ghcb == ptr::null_mut() {
             return Ok(());
         }
 
-        unsafe { (*self.ghcb).shutdown().map_err(|_| ()) }
+        unsafe { (*self.ghcb).shutdown() }
     }
 
     pub fn set_reset_ip(&mut self, reset_ip: u64) {
@@ -331,9 +332,10 @@ impl PerCpu {
         unsafe { self.ghcb.as_mut().unwrap() }
     }
 
-    pub fn alloc_svsm_vmsa(&mut self) -> Result<(), ()> {
+    pub fn alloc_svsm_vmsa(&mut self) -> Result<(), SvsmError> {
         if let Some(_) = self.svsm_vmsa {
-            return Err(());
+            // FIXME: add a more explicit error variant for this condition
+            return Err(SvsmError::Mem);
         }
 
         let vaddr = allocate_new_vmsa(RMPFlags::VMPL1)?;
@@ -362,7 +364,7 @@ impl PerCpu {
         self.get_pgtable().unmap_4k(SVSM_PERCPU_VMSA_BASE);
     }
 
-    pub fn map_guest_vmsa(&self, paddr: PhysAddr) -> Result<(), ()> {
+    pub fn map_guest_vmsa(&self, paddr: PhysAddr) -> Result<(), SvsmError> {
         assert!(self.apic_id == this_cpu().get_apic_id());
 
         let flags = PageTable::data_flags();
@@ -412,7 +414,7 @@ impl PerCpu {
         unsafe { (SVSM_PERCPU_VMSA_BASE as *mut VMSA).as_mut().unwrap() }
     }
 
-    pub fn alloc_guest_vmsa(&mut self) -> Result<(), ()> {
+    pub fn alloc_guest_vmsa(&mut self) -> Result<(), SvsmError> {
         let vaddr = allocate_new_vmsa(RMPFlags::VMPL1)?;
         let paddr = virt_to_phys(vaddr);
 
@@ -428,7 +430,7 @@ impl PerCpu {
         self.get_pgtable().unmap_4k(SVSM_PERCPU_CAA_BASE);
     }
 
-    pub fn map_guest_caa(&self, paddr: PhysAddr) -> Result<(), ()> {
+    pub fn map_guest_caa(&self, paddr: PhysAddr) -> Result<(), SvsmError> {
         self.unmap_caa();
 
         let paddr_aligned = page_align(paddr);

src/fw_meta.rs
@@ -231,7 +231,7 @@ pub fn parse_fw_meta_data() -> Result<SevFWMetaData, ()> {
     let mut meta_data = SevFWMetaData::new();
 
     // Map meta-data location, it starts at 32 bytes below 4GiB
-    let guard = PerCPUPageMappingGuard::create(pstart, 0, false)?;
+    let guard = PerCPUPageMappingGuard::create(pstart, 0, false).map_err(|_e| ())?;
     let vstart = guard.virt_addr();
     let vend: VirtAddr = vstart + PAGE_SIZE;
 
@@ -332,7 +332,7 @@ fn validate_fw_mem_region(region: SevPreValidMem) -> Result<(), ()> {
         .expect("GHCB PSC call failed to validate firmware memory");
 
     for paddr in (pstart..pend).step_by(PAGE_SIZE) {
-        let guard = PerCPUPageMappingGuard::create(paddr, 0, false)?;
+        let guard = PerCPUPageMappingGuard::create(paddr, 0, false).map_err(|_e| ())?;
         let vaddr = guard.virt_addr();
 
         if pvalidate(vaddr, false, true).is_err() {

src/mm/address_space.rs
@@ -4,6 +4,7 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use crate::error::SvsmError;
 use crate::types::{PhysAddr, VirtAddr};
 use crate::utils::immut_after_init::ImmutAfterInitCell;
 
@@ -146,16 +147,16 @@ pub const SVSM_PERCPU_TEMP_4K_SLOTS: usize =
 pub const SVSM_PERCPU_TEMP_2M_SLOTS: usize =
     ((SVSM_PERCPU_TEMP_END_2M - SVSM_PERCPU_TEMP_BASE_2M) / PAGE_SIZE_2M) / 2;
 
-pub fn percpu_4k_slot_addr(slot: usize) -> Result<VirtAddr, ()> {
+pub fn percpu_4k_slot_addr(slot: usize) -> Result<VirtAddr, SvsmError> {
     if slot >= SVSM_PERCPU_TEMP_4K_SLOTS {
-        return Err(());
+        return Err(SvsmError::Mem);
     }
     Ok(SVSM_PERCPU_TEMP_BASE_4K + (2 * slot * PAGE_SIZE))
 }
 
-pub fn percpu_2m_slot_addr(slot: usize) -> Result<VirtAddr, ()> {
+pub fn percpu_2m_slot_addr(slot: usize) -> Result<VirtAddr, SvsmError> {
     if slot >= SVSM_PERCPU_TEMP_2M_SLOTS {
-        return Err(());
+        return Err(SvsmError::Mem);
     }
     Ok(SVSM_PERCPU_TEMP_BASE_2M + (2 * slot * PAGE_SIZE_2M))
 }

src/mm/pagetable.rs
@@ -8,6 +8,7 @@ use crate::cpu::control_regs::write_cr3;
 use crate::cpu::cpuid::cpuid_table;
 use crate::cpu::features::{cpu_has_nx, cpu_has_pge};
 use crate::cpu::flush_tlb_global_sync;
+use crate::error::SvsmError;
 use crate::locking::{LockGuard, SpinLock};
 use crate::mm::alloc::allocate_zeroed_page;
 use crate::mm::{phys_to_virt, virt_to_phys, PGTABLE_LVL3_IDX_SHARED};
@@ -159,7 +160,7 @@ impl PageTable {
         set_c_bit(cr3)
     }
 
-    pub fn clone_shared(&self) -> Result<PageTableRef, ()> {
+    pub fn clone_shared(&self) -> Result<PageTableRef, SvsmError> {
         let root_ptr = PageTable::allocate_page_table()?;
         let pgtable = root_ptr.cast::<PageTable>();
 
@@ -194,7 +195,7 @@ impl PageTable {
             | PTEntryFlags::DIRTY
     }
 
-    fn allocate_page_table() -> Result<*mut PTPage, ()> {
+    fn allocate_page_table() -> Result<*mut PTPage, SvsmError> {
         let ptr = allocate_zeroed_page()?;
         Ok(ptr as *mut PTPage)
     }
@@ -353,7 +354,7 @@ impl PageTable {
         }
     }
 
-    fn do_split_4k(entry: &mut PTEntry) -> Result<(), ()> {
+    fn do_split_4k(entry: &mut PTEntry) -> Result<(), SvsmError> {
         let page = PageTable::allocate_page_table()?;
         let mut flags = entry.flags();
 
@@ -379,12 +380,12 @@ impl PageTable {
         Ok(())
     }
 
-    pub fn split_4k(mapping: Mapping) -> Result<(), ()> {
+    pub fn split_4k(mapping: Mapping) -> Result<(), SvsmError> {
         match mapping {
             Mapping::Level0(_entry) => Ok(()),
             Mapping::Level1(entry) => PageTable::do_split_4k(entry),
-            Mapping::Level2(_entry) => Err(()),
-            Mapping::Level3(_entry) => Err(()),
+            Mapping::Level2(_entry) => Err(SvsmError::Mem),
+            Mapping::Level3(_entry) => Err(SvsmError::Mem),
         }
     }
 
@@ -404,33 +405,27 @@ impl PageTable {
         entry.set(set_c_bit(addr), flags);
     }
 
-    pub fn set_shared_4k(&mut self, vaddr: VirtAddr) -> Result<(), ()> {
+    pub fn set_shared_4k(&mut self, vaddr: VirtAddr) -> Result<(), SvsmError> {
         let mapping = self.walk_addr(vaddr);
-
-        if let Err(_e) = PageTable::split_4k(mapping) {
-            return Err(());
-        }
+        PageTable::split_4k(mapping)?;
 
         if let Mapping::Level0(entry) = self.walk_addr(vaddr) {
             PageTable::clear_c_bit(entry);
             Ok(())
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
-    pub fn set_encrypted_4k(&mut self, vaddr: VirtAddr) -> Result<(), ()> {
+    pub fn set_encrypted_4k(&mut self, vaddr: VirtAddr) -> Result<(), SvsmError> {
         let mapping = self.walk_addr(vaddr);
-
-        if let Err(_e) = PageTable::split_4k(mapping) {
-            return Err(());
-        }
+        PageTable::split_4k(mapping)?;
 
         if let Mapping::Level0(entry) = self.walk_addr(vaddr) {
             PageTable::set_c_bit(entry);
             Ok(())
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
@@ -447,7 +442,7 @@ impl PageTable {
         vaddr: VirtAddr,
         paddr: PhysAddr,
         flags: PTEntryFlags,
-    ) -> Result<(), ()> {
+    ) -> Result<(), SvsmError> {
         assert!(is_aligned(vaddr, PAGE_SIZE_2M));
         assert!(is_aligned(paddr, PAGE_SIZE_2M));
 
@@ -457,7 +452,7 @@ impl PageTable {
             entry.set(set_c_bit(paddr), flags | PTEntryFlags::HUGE);
             Ok(())
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
@@ -479,14 +474,14 @@ impl PageTable {
         vaddr: VirtAddr,
         paddr: PhysAddr,
         flags: PTEntryFlags,
-    ) -> Result<(), ()> {
+    ) -> Result<(), SvsmError> {
         let mapping = self.alloc_pte_4k(vaddr);
 
         if let Mapping::Level0(entry) = mapping {
             entry.set(set_c_bit(paddr), flags);
             Ok(())
         } else {
-            Err(())
+            Err(SvsmError::Mem)
         }
     }
 
@@ -501,14 +496,14 @@ impl PageTable {
         }
     }
 
-    pub fn phys_addr(&mut self, vaddr: VirtAddr) -> Result<PhysAddr, ()> {
+    pub fn phys_addr(&mut self, vaddr: VirtAddr) -> Result<PhysAddr, SvsmError> {
         let mapping = self.walk_addr(vaddr);
 
         match mapping {
             Mapping::Level0(entry) => {
                 let offset = vaddr & (PAGE_SIZE - 1);
                 if !entry.flags().contains(PTEntryFlags::PRESENT) {
-                    return Err(());
+                    return Err(SvsmError::Mem);
                 }
                 Ok(entry.address() + offset)
             }
@@ -517,13 +512,13 @@ impl PageTable {
                 if !entry.flags().contains(PTEntryFlags::PRESENT)
                     || !entry.flags().contains(PTEntryFlags::HUGE)
                 {
-                    return Err(());
+                    return Err(SvsmError::Mem);
                 }
 
                 Ok(entry.address() + offset)
             }
-            Mapping::Level2(_entry) => Err(()),
-            Mapping::Level3(_entry) => Err(()),
+            Mapping::Level2(_entry) => Err(SvsmError::Mem),
+            Mapping::Level3(_entry) => Err(SvsmError::Mem),
         }
     }
 
@@ -533,7 +528,7 @@ impl PageTable {
         end: VirtAddr,
         phys: PhysAddr,
         flags: PTEntryFlags,
-    ) -> Result<(), ()> {
+    ) -> Result<(), SvsmError> {
         for addr in (start..end).step_by(PAGE_SIZE) {
             let offset = addr - start;
             self.map_4k(addr, phys + offset, flags)?;
@@ -553,7 +548,7 @@ impl PageTable {
         end: VirtAddr,
         phys: PhysAddr,
         flags: PTEntryFlags,
-    ) -> Result<(), ()> {
+    ) -> Result<(), SvsmError> {
         for addr in (start..end).step_by(PAGE_SIZE_2M) {
             let offset = addr - start;
             self.map_2m(addr, phys + offset, flags)?;
@@ -573,7 +568,7 @@ impl PageTable {
         end: VirtAddr,
         phys: PhysAddr,
         flags: PTEntryFlags,
-    ) -> Result<(), ()> {
+    ) -> Result<(), SvsmError> {
         let mut vaddr = start;
         let mut paddr = phys;
 

src/mm/ptguards.rs
@@ -7,6 +7,7 @@
 use super::pagetable::{get_init_pgtable_locked, PageTable};
 use crate::cpu::percpu::this_cpu_mut;
 use crate::cpu::tlb::{flush_address_sync, flush_tlb_global_sync};
+use crate::error::SvsmError;
 use crate::mm::{percpu_2m_slot_addr, percpu_4k_slot_addr};
 use crate::types::{PhysAddr, VirtAddr, PAGE_SIZE, PAGE_SIZE_2M};
 use crate::utils::is_aligned;
@@ -27,7 +28,7 @@ pub struct PerCPUPageMappingGuard {
 }
 
 impl PerCPUPageMappingGuard {
-    pub fn create(paddr: VirtAddr, slot: usize, huge: bool) -> Result<Self, ()> {
+    pub fn create(paddr: VirtAddr, slot: usize, huge: bool) -> Result<Self, SvsmError> {
         let size = if huge { PAGE_SIZE_2M } else { PAGE_SIZE };
 
         assert!(is_aligned(paddr, size));
@@ -84,14 +85,14 @@ impl PTMappingGuard {
             Ok(()) => PTMappingGuard {
                 mapping: Some(raw_mapping),
             },
-            Err(()) => PTMappingGuard { mapping: None },
+            Err(_e) => PTMappingGuard { mapping: None },
         }
     }
 
-    pub fn check_mapping(&self) -> Result<(), ()> {
+    pub fn check_mapping(&self) -> Result<(), SvsmError> {
         match self.mapping {
             Some(_) => Ok(()),
-            None => Err(()),
+            None => Err(SvsmError::Mem),
         }
     }
 }

commit e9b75d58f2ffb530f87b6a51d775deec1d0231bc

svsm/mm/alloc: Fix memory allocator tests
A recent commit to improve error typing broke the unit-tests. Fix them
up.

Fixes: 581245bd3500 ('svsm/error: add error typing in the memory management subsystem')
Signed-off-by: Joerg Roedel <jroedel@suse.de>

src/mm/alloc.rs
@@ -1180,7 +1180,7 @@ use crate::locking::LockGuard;
 #[cfg(test)]
 // Allocate a memory region from the standard Rust allocator and pass it to
 // root_mem_init().
-fn setup_test_root_mem(size: usize) -> LockGuard<'static, SvsmError> {
+fn setup_test_root_mem(size: usize) -> LockGuard<'static, ()> {
     extern crate alloc;
     use alloc::alloc::{alloc, handle_alloc_error};
 
@@ -1202,7 +1202,7 @@ fn setup_test_root_mem(size: usize) -> LockGuard<'static, SvsmError> {
 
 #[cfg(test)]
 // Undo the setup done from setup_test_root_mem().
-fn destroy_test_root_mem(lock: LockGuard<'static, SvsmError>) {
+fn destroy_test_root_mem(lock: LockGuard<'static, ()>) {
     extern crate alloc;
     use alloc::alloc::dealloc;
 