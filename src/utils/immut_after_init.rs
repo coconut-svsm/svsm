coconut-svsm/svsm

commit 0b9c951122a8946b3a533e05e13b6de6c4c8c6a0

svsm: do not depend on nightly const_mut_refs feature
Luckily, mutable references depending on this feature can be converted
to immutable references or be removed alltogether.

The mutable pointer to a ConsoleWriter in Console can be const because
ConsoleWriter::put_byte() takes &self. On the other hand,
ImmutAfterInitRef::new_from_cell() requires using a raw pointer in a
const context, which will not work without the feature. Since it is
not in use, remove the method.

Signed-off-by: Carlos López <carlos.lopez@suse.com>

src/utils/immut_after_init.rs
@@ -183,7 +183,7 @@ pub struct ImmutAfterInitRef<'a, T: 'a> {
     #[doc(hidden)]
     ptr: ImmutAfterInitCell<*const T>,
     #[doc(hidden)]
-    _phantom: PhantomData<&'a mut &'a T>,
+    _phantom: PhantomData<&'a &'a T>,
 }
 
 impl<'a, T> ImmutAfterInitRef<'a, T> {
@@ -248,19 +248,6 @@ impl<'a, T: Copy> ImmutAfterInitRef<'a, T> {
         'b: 'a,
     {
         self.ptr.init(&(*cell.data.get()).as_ptr());
-    }
-
-    /// Create an initialized `ImmutAfterInitRef` instance pointing to a value
-    /// wrapped in a [`ImmutAfterInitCell`].
-    ///
-    /// * `cell` - The value to make the `ImmutAfterInitRef` to refer to. By
-    ///            convention, the referenced value must have been initialized
-    ///            already.
-    pub const fn new_from_cell(cell: &'a ImmutAfterInitCell<T>) -> Self {
-        Self {
-            ptr: ImmutAfterInitCell::new(unsafe { &*cell.data.get() }.as_ptr()),
-            _phantom: PhantomData,
-        }
     }
 }
 

src/console.rs
@@ -15,11 +15,11 @@ pub trait ConsoleWriter {
 }
 
 pub struct Console {
-    writer: *mut dyn ConsoleWriter,
+    writer: *const dyn ConsoleWriter,
 }
 
 impl Console {
-    pub fn set(&mut self, w: *mut dyn ConsoleWriter) {
+    pub fn set(&mut self, w: *const dyn ConsoleWriter) {
         self.writer = w;
     }
 }
@@ -42,7 +42,7 @@ impl fmt::Write for Console {
 
 pub static WRITER: SpinLock<Console> = SpinLock::new(unsafe {
     Console {
-        writer: &mut DEFAULT_SERIAL_PORT,
+        writer: &DEFAULT_SERIAL_PORT,
     }
 });
 static CONSOLE_INITIALIZED: ImmutAfterInitCell<bool> = ImmutAfterInitCell::new(false);

src/lib.rs
@@ -5,7 +5,6 @@
 // Author: Nicolai Stange <nstange@suse.de>
 
 #![no_std]
-#![feature(const_mut_refs)]
 
 pub mod acpi;
 pub mod address;

src/stage2.rs
@@ -6,7 +6,7 @@
 
 #![no_std]
 #![no_main]
-#![feature(const_mut_refs, rustc_private)]
+#![feature(rustc_private)]
 
 pub mod boot_stage2;
 
@@ -102,7 +102,7 @@ fn setup_env() {
     init_percpu();
 
     unsafe {
-        WRITER.lock().set(&mut CONSOLE_SERIAL);
+        WRITER.lock().set(&CONSOLE_SERIAL);
     }
     init_console();
 

src/svsm.rs
@@ -6,7 +6,6 @@
 
 #![no_std]
 #![no_main]
-#![feature(const_mut_refs)]
 pub mod svsm_paging;
 
 extern crate alloc;

commit 5ced9582da92c0a443cee3370d7ad26c004c3c78

utils/immut_after_init: remove deprecated doctest
Remove the deprecated documentation for
ImmutAfterInitRef::new_from_cell(), which caused a doctest to fail.
This method got got removed in order to not depend on a nightly
feature.

Fixes: 0b9c951122a8 ("svsm: do not depend on nightly const_mut_refs feature")
Signed-off-by: Carlos López <carlos.lopez@suse.com>

src/utils/immut_after_init.rs
@@ -203,27 +203,8 @@ unsafe impl<T> Sync for ImmutAfterInitCell<T> {}
 /// }
 /// ```
 ///
-/// Also, a `ImmutAfterInitRef` can get initialized by dereferencing another,
-/// possibly temporary `ImmutAfterInitRef`, with the temporary again either
-/// dereferencing a [`ImmutAfterInitCell`]'s contents,
-/// ```
-/// # use svsm::utils::immut_after_init::{ImmutAfterInitCell, ImmutAfterInitRef};
-/// static RX : ImmutAfterInitRef::<'static, i32> = ImmutAfterInitRef::uninit();
-///
-/// fn init_rx(r : ImmutAfterInitRef<'static, i32>) {
-///     unsafe { RX.init_from_ref(r.get()) };
-/// }
-///
-/// static X : ImmutAfterInitCell<i32> = ImmutAfterInitCell::uninit();
-///
-/// fn main() {
-///     unsafe { X.init(&123) };
-///
-///     init_rx(ImmutAfterInitRef::new_from_cell(&X));
-///     assert_eq!(*RX, 123);
-/// }
-/// ```
-/// or a plain value directly:
+/// Also, an `ImmutAfterInitRef` can be initialized by obtaining a reference
+/// from another `ImmutAfterInitRef`:
 /// ```
 /// # use svsm::utils::immut_after_init::ImmutAfterInitRef;
 /// static RX : ImmutAfterInitRef::<'static, i32> = ImmutAfterInitRef::uninit();