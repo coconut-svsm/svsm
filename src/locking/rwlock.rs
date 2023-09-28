coconut-svsm/svsm

commit b29acca1f751dfd6e43d1a8dddfff53369e7a161

SVSM/locking: Add RWLock implementation
There are a couple of cases in the code which are read-mostly and
protected by a SpinLock. Implement a Read-Write lock to allow parallel
readers.

Signed-off-by: Joerg Roedel <jroedel@suse.de>

src/locking/rwlock.rs
@@ -1,0 +1,143 @@
+// SPDX-License-Identifier: (GPL-2.0-or-later OR MIT)
+//
+// Copyright (c) 2023 SUSE LLC
+//
+// Author: Joerg Roedel <jroedel@suse.de>
+//
+// vim: ts=4 sw=4 et
+
+use core::sync::atomic::{AtomicU64, Ordering};
+use core::ops::{Deref, DerefMut};
+use core::cell::UnsafeCell;
+
+pub struct ReadLockGuard<'a, T> {
+    rwlock: &'a AtomicU64,
+    data: &'a mut T,
+}
+
+impl<'a, T> Drop for ReadLockGuard<'a, T> {
+    fn drop(&mut self) {
+        self.rwlock.fetch_sub(1, Ordering::Release);
+    }
+}
+
+impl<'a, T> Deref for ReadLockGuard<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        self.data
+    }
+}
+
+pub struct WriteLockGuard<'a, T> {
+    rwlock: &'a AtomicU64,
+    data: &'a mut T,
+}
+
+impl<'a, T> Drop for WriteLockGuard<'a, T> {
+    fn drop(&mut self) {
+        // There are no readers - safe to just set lock to 0
+        self.rwlock.store(0, Ordering::Release);
+    }
+}
+
+impl<'a, T> Deref for WriteLockGuard<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        self.data
+    }
+}
+
+impl<'a, T> DerefMut for WriteLockGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        self.data
+    }
+}
+
+pub struct RWLock<T> {
+    rwlock: AtomicU64,
+    data: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for RWLock<T> {}
+
+#[inline]
+fn split_val(val: u64) -> (u64, u64) {
+    (val & 0xffff_ffffu64, val >> 32)
+}
+
+#[inline]
+fn compose_val(readers: u64, writers: u64) -> u64 {
+    (readers & 0xffff_ffffu64) | (writers << 32)
+}
+
+impl<T> RWLock<T> {
+    pub const fn new(data: T) -> Self {
+        RWLock {
+            rwlock: AtomicU64::new(0),
+            data: UnsafeCell::new(data),
+        }
+    }
+
+    #[inline]
+    fn wait_for_writers(&self) -> u64 {
+        loop {
+            let val: u64 = self.rwlock.load(Ordering::Relaxed);
+            let (_, writers) = split_val(val);
+
+            if writers == 0 {
+                return val;
+            }
+        }
+    }
+
+    #[inline]
+    fn wait_for_readers(&self) -> u64 {
+        loop {
+            let val: u64 = self.rwlock.load(Ordering::Relaxed);
+            let (readers, _) = split_val(val);
+
+            if readers == 0 {
+                return val;
+            }
+        }
+    }
+
+    pub fn lock_read(&self) -> ReadLockGuard<T> {
+        loop {
+            let val = self.wait_for_writers();
+            let (readers, _) = split_val(val);
+            let new_val = compose_val(readers + 1, 0);
+
+            if self.rwlock.compare_exchange(val, new_val, Ordering::Acquire, Ordering::Relaxed).is_ok() {
+                break;
+            }
+        }
+
+        ReadLockGuard {
+            rwlock: &self.rwlock,
+            data: unsafe { &mut *self.data.get() },
+        }
+    }
+
+    pub fn lock_write(&self) -> WriteLockGuard<T> {
+        // Waiting for current writer to finish
+        loop {
+            let val = self.wait_for_writers();
+            let (readers, _) = split_val(val);
+            let new_val = compose_val(readers, 1);
+
+            if self.rwlock.compare_exchange(val, new_val, Ordering::Acquire, Ordering::Relaxed) .is_ok() {
+                break;
+            }
+        }
+
+        // Now locked for write - wait until all readers finished
+        let val: u64 = self.wait_for_readers();
+        assert!(val == compose_val(0,1));
+
+        WriteLockGuard {
+            rwlock: &self.rwlock,
+            data: unsafe { &mut *self.data.get() },
+        }
+    }
+}

src/locking/mod.rs
@@ -7,5 +7,7 @@
 // vim: ts=4 sw=4 et
 
 pub mod spinlock;
+pub mod rwlock;
 
 pub use spinlock::{SpinLock, LockGuard};
+pub use rwlock::{RWLock, ReadLockGuard, WriteLockGuard};

commit a79e24b61e9daf7c43539ce84836c646141ff58b

locking/rwlock: fix undefined behavior via RWLock::lock_read()
Fix an undefined behavior that could be triggered when acquiring a
read guard on a RWLock. This happened because a ReadLockGuard would
keep a mutable reference to the underlying shared data. Since one
might acquire multiple ReadLockGuards to the same data, this would
lead to undefined behavior under Rust's borrowing rules.

Fixes: b29acca1 ("SVSM/locking: Add RWLock implementation")
Reported-by: Tom Dohrmann <erbse.13@gmx.de>
Signed-off-by: Carlos López <carlos.lopez@suse.com>

src/locking/rwlock.rs
@@ -12,7 +12,7 @@ use core::sync::atomic::{AtomicU64, Ordering};
 #[derive(Debug)]
 pub struct ReadLockGuard<'a, T: Debug> {
     rwlock: &'a AtomicU64,
-    data: &'a mut T,
+    data: &'a T,
 }
 
 impl<'a, T: Debug> Drop for ReadLockGuard<'a, T> {
@@ -124,7 +124,7 @@ impl<T: Debug> RWLock<T> {
 
         ReadLockGuard {
             rwlock: &self.rwlock,
-            data: unsafe { &mut *self.data.get() },
+            data: unsafe { &*self.data.get() },
         }
     }
 