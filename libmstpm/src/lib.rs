b154fcad789cf9ea987561c2467e612cebfc5879
Claudio Carvalho <cclaudio@linux.ibm.com>
2024-02-19 22:43:46 +0200

    Add libmstpm workspace member
    Add the libmstpm workspace member to own the C libraries that the
    Microsoft TPM depends on:
    - libcrt: the libcrt.a
    - OpenSSL: the libcrypto.a
    - Microsoft TPM: the libplatform.a and libtpm.a
    
    This workspace member will be used to keep self-contained all the complexity to
    build the C libraries as well as generation of FFI bindings using bindgen.
    
    Signed-off-by: Claudio Carvalho <cclaudio@linux.ibm.com>

---
+++ libmstpm/src/lib.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 IBM Corporation
+//
+// Author: Claudio Carvalho <cclaudio@linux.ibm.com>
+
+//! This crate provides the libmstpm definitions used by the COCONUT-SVSM
+//! for the vTPM.
+
+#![no_std]
