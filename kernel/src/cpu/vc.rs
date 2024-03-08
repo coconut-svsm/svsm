7bf094f9583e7aca4b52496affcfda0f017fc5e8
Adam Dunlap <acdunlap@google.com>
2024-03-08 12:16:30 -0800

    cpu/vc Add #VC handler tests
    Adds tests that make sure a #VC exception is raised while executing
    various instructions and that the #VC handler properly handles them via
    the ghcb. Instructions tested are cpuid and port IO.
    
    Signed-off-by: Adam Dunlap <acdunlap@google.com>

--- kernel/src/cpu/vc.rs
+++ kernel/src/cpu/vc.rs
@@ -257,3 +257,133 @@ fn vc_decode_insn(ctx: &X86ExceptionContext) -> Result<Instruction, SvsmError> {
 fn vc_decoding_needed(error_code: usize) -> bool {
     !(SVM_EXIT_EXCP_BASE..=SVM_EXIT_LAST_EXCP).contains(&error_code)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::cpu::percpu::this_cpu_mut;
+    use crate::sev::ghcb::GHCB;
+    use core::arch::asm;
+    use core::arch::x86_64::__cpuid_count;
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_has_memory_encryption_info_cpuid() {
+        const CPUID_EXTENDED_FUNCTION_INFO: u32 = 0x8000_0000;
+        const CPUID_MEMORY_ENCRYPTION_INFO: u32 = 0x8000_001F;
+        let extended_info = unsafe { __cpuid_count(CPUID_EXTENDED_FUNCTION_INFO, 0) };
+        assert!(extended_info.eax >= CPUID_MEMORY_ENCRYPTION_INFO);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_has_amd_cpuid() {
+        const CPUID_VENDOR_INFO: u32 = 0;
+
+        let vendor_info = unsafe { __cpuid_count(CPUID_VENDOR_INFO, 0) };
+
+        let vendor_name_bytes = [vendor_info.ebx, vendor_info.edx, vendor_info.ecx]
+            .map(|v| v.to_le_bytes())
+            .concat();
+
+        assert_eq!(core::str::from_utf8(&vendor_name_bytes), Ok("AuthenticAMD"));
+    }
+
+    const GHCB_FILL_TEST_VALUE: u8 = b'1';
+
+    fn fill_ghcb_with_test_data() {
+        let ghcb = this_cpu_mut().ghcb_unsafe();
+        unsafe {
+            // The count param is 1 to only write one ghcb's worth of data
+            core::ptr::write_bytes(ghcb, GHCB_FILL_TEST_VALUE, 1);
+        }
+    }
+
+    fn verify_ghcb_was_altered() {
+        let ghcb = this_cpu_mut().ghcb_unsafe();
+        let ghcb_bytes =
+            unsafe { core::slice::from_raw_parts(ghcb.cast::<u8>(), core::mem::size_of::<GHCB>()) };
+        assert!(ghcb_bytes.iter().any(|v| *v != GHCB_FILL_TEST_VALUE));
+    }
+
+    // Calls `f` with an assertion that it ended up altering the ghcb.
+    fn verify_ghcb_gets_altered<R, F>(f: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        fill_ghcb_with_test_data();
+        let result = f();
+        verify_ghcb_was_altered();
+        result
+    }
+
+    const TESTDEV_ECHO_LAST_PORT: u16 = 0xe0;
+
+    fn inb(port: u16) -> u8 {
+        unsafe {
+            let ret: u8;
+            asm!("inb %dx, %al", in("dx") port, out("al") ret, options(att_syntax));
+            ret
+        }
+    }
+
+    fn outb(port: u16, value: u8) {
+        unsafe { asm!("outb %al, %dx", in("al") value, in("dx") port, options(att_syntax)) }
+    }
+
+    fn inw(port: u16) -> u16 {
+        unsafe {
+            let ret: u16;
+            asm!("inw %dx, %ax", in("dx") port, out("ax") ret, options(att_syntax));
+            ret
+        }
+    }
+
+    fn outw(port: u16, value: u16) {
+        unsafe { asm!("outw %ax, %dx", in("ax") value, in("dx") port, options(att_syntax)) }
+    }
+
+    fn inl(port: u16) -> u32 {
+        unsafe {
+            let ret: u32;
+            asm!("inl %dx, %eax", in("dx") port, out("eax") ret, options(att_syntax));
+            ret
+        }
+    }
+
+    fn outl(port: u16, value: u32) {
+        unsafe { asm!("outl %eax, %dx", in("eax") value, in("dx") port, options(att_syntax)) }
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_port_io_8() {
+        const TEST_VAL: u8 = 0x12;
+        verify_ghcb_gets_altered(|| outb(TESTDEV_ECHO_LAST_PORT, TEST_VAL));
+        assert_eq!(
+            TEST_VAL,
+            verify_ghcb_gets_altered(|| inb(TESTDEV_ECHO_LAST_PORT))
+        );
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_port_io_16() {
+        const TEST_VAL: u16 = 0x4321;
+        verify_ghcb_gets_altered(|| outw(TESTDEV_ECHO_LAST_PORT, TEST_VAL));
+        assert_eq!(
+            TEST_VAL,
+            verify_ghcb_gets_altered(|| inw(TESTDEV_ECHO_LAST_PORT))
+        );
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_port_io_32() {
+        const TEST_VAL: u32 = 0xabcd1234;
+        verify_ghcb_gets_altered(|| outl(TESTDEV_ECHO_LAST_PORT, TEST_VAL));
+        assert_eq!(
+            TEST_VAL,
+            verify_ghcb_gets_altered(|| inl(TESTDEV_ECHO_LAST_PORT))
+        );
+    }
+}
