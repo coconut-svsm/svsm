1e790007fa316cb7de30b2505d445a26e6efdf68
Carlos López <carlos.lopez@suse.com>
2024-04-30 15:45:37 +0200

    mm/page_visibility: use platform methods to change page visibility
    Use SvsmPlatform to change page states instead of directly using
    PVALIDATE or calling into the GHCB.
    
    Signed-off-by: Carlos López <carlos.lopez@suse.com>

--- kernel/src/mm/page_visibility.rs
+++ kernel/src/mm/page_visibility.rs
@@ -6,21 +6,22 @@
 
 use crate::address::VirtAddr;
 use crate::cpu::flush_tlb_global_sync;
-use crate::cpu::ghcb::current_ghcb;
 use crate::cpu::percpu::this_cpu_mut;
 use crate::mm::validate::{
     valid_bitmap_clear_valid_4k, valid_bitmap_set_valid_4k, valid_bitmap_valid_addr,
 };
 use crate::mm::virt_to_phys;
-use crate::platform::PageStateChangeOp;
-use crate::sev::utils::pvalidate;
+use crate::platform::{PageStateChangeOp, SVSM_PLATFORM};
 use crate::sev::PvalidateOp;
 use crate::types::{PageSize, PAGE_SIZE};
 use crate::utils::MemoryRegion;
 
 pub fn make_page_shared(vaddr: VirtAddr) {
+    let platform = SVSM_PLATFORM.as_dyn_ref();
+
     // Revoke page validation before changing page state.
-    pvalidate(vaddr, PageSize::Regular, PvalidateOp::Invalid)
+    platform
+        .pvalidate_range(MemoryRegion::new(vaddr, PAGE_SIZE), PvalidateOp::Invalid)
         .expect("Pvalidate failed when making page shared");
     let paddr = virt_to_phys(vaddr);
     if valid_bitmap_valid_addr(paddr) {
@@ -28,7 +29,7 @@ pub fn make_page_shared(vaddr: VirtAddr) {
     }
 
     // Ask the hypervisor to make the page shared.
-    current_ghcb()
+    platform
         .page_state_change(
             MemoryRegion::new(paddr, PAGE_SIZE),
             PageSize::Regular,
@@ -52,9 +53,11 @@ pub fn make_page_private(vaddr: VirtAddr) {
         .expect("Failed to remap private page in page tables");
     flush_tlb_global_sync();
 
+    let platform = SVSM_PLATFORM.as_dyn_ref();
+
     // Ask the hypervisor to make the page private.
     let paddr = virt_to_phys(vaddr);
-    current_ghcb()
+    platform
         .page_state_change(
             MemoryRegion::new(paddr, PAGE_SIZE),
             PageSize::Regular,
@@ -63,7 +66,8 @@ pub fn make_page_private(vaddr: VirtAddr) {
         .expect("Hypervisor failed to make page private");
 
     // Revoke page validation before changing page state.
-    pvalidate(vaddr, PageSize::Regular, PvalidateOp::Valid)
+    platform
+        .pvalidate_range(MemoryRegion::new(vaddr, PAGE_SIZE), PvalidateOp::Valid)
         .expect("Pvalidate failed when making page private");
     if valid_bitmap_valid_addr(paddr) {
         valid_bitmap_set_valid_4k(paddr);
