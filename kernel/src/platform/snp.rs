coconut-svsm/svsm

commit edf38b0c7fe38c917cdaca5910692b106a22081d

platform: Calculate PTE masks through platform abstraction
Use the platform abstraction to determine how to construct the PTE masks
for confidential/shared pages.

Signed-off-by: Jon Lange <jlange@microsoft.com>

kernel/src/platform/snp.rs
@@ -4,7 +4,9 @@
 //
 // Author: Jon Lange <jlange@microsoft.com>
 
-use crate::platform::SvsmPlatform;
+use crate::cpu::cpuid::cpuid_table;
+use crate::platform::{PageEncryptionMasks, SvsmPlatform};
+use crate::sev::status::vtom_enabled;
 use crate::sev::{sev_status_init, sev_status_verify};
 
 #[derive(Clone, Copy, Debug)]
@@ -29,5 +31,29 @@ impl SvsmPlatform for SnpPlatform {
 
     fn env_setup_late(&mut self) {
         sev_status_verify();
+    }
+
+    fn get_page_encryption_masks(&self, vtom: usize) -> PageEncryptionMasks {
+        // Find physical address size.
+        let res =
+            cpuid_table(0x80000008).expect("Can not get physical address size from CPUID table");
+        if vtom_enabled() {
+            PageEncryptionMasks {
+                private_pte_mask: 0,
+                shared_pte_mask: vtom,
+                addr_mask_width: vtom.leading_zeros(),
+                phys_addr_sizes: res.eax,
+            }
+        } else {
+            // Find C-bit position.
+            let res = cpuid_table(0x8000001f).expect("Can not get C-Bit position from CPUID table");
+            let c_bit = res.ebx & 0x3f;
+            PageEncryptionMasks {
+                private_pte_mask: 1 << c_bit,
+                shared_pte_mask: 0,
+                addr_mask_width: c_bit,
+                phys_addr_sizes: res.eax,
+            }
+        }
     }
 }

kernel/src/boot_stage2.rs
@@ -109,6 +109,12 @@ global_asm!(
 
     get_pte_c_bit:
         /*
+         * Check if this is an SNP platform.  If not, there is no C bit.
+         */
+        cmpl $1, 8(%esi)
+        jnz .Lvtom
+
+        /*
          * Check that the SNP_Active bit in the SEV_STATUS MSR is set.
          */
         movl $0xc0010131, %ecx

kernel/src/cpu/cpuid.rs
@@ -7,6 +7,8 @@
 use crate::utils::immut_after_init::ImmutAfterInitRef;
 use cpuarch::snp_cpuid::SnpCpuidTable;
 
+use core::arch::asm;
+
 static CPUID_PAGE: ImmutAfterInitRef<'_, SnpCpuidTable> = ImmutAfterInitRef::uninit();
 
 pub fn register_cpuid_table(table: &'static SnpCpuidTable) {
@@ -45,6 +47,34 @@ pub struct CpuidResult {
     pub ebx: u32,
     pub ecx: u32,
     pub edx: u32,
+}
+
+impl CpuidResult {
+    pub fn get(cpuid_fn: u32, cpuid_subfn: u32) -> Self {
+        let mut result_eax: u32;
+        let mut result_ebx: u32;
+        let mut result_ecx: u32;
+        let mut result_edx: u32;
+        unsafe {
+            asm!("push %rbx",
+                 "cpuid",
+                 "movl %ebx, %edi",
+                 "pop %rbx",
+                 in("eax") cpuid_fn,
+                 in("ecx") cpuid_subfn,
+                 lateout("eax") result_eax,
+                 lateout("edi") result_ebx,
+                 lateout("ecx") result_ecx,
+                 lateout("edx") result_edx,
+                 options(att_syntax));
+        }
+        Self {
+            eax: result_eax,
+            ebx: result_ebx,
+            ecx: result_ecx,
+            edx: result_edx,
+        }
+    }
 }
 
 pub fn cpuid_table_raw(eax: u32, ecx: u32, xcr0: u64, xss: u64) -> Option<CpuidResult> {

kernel/src/mm/pagetable.rs
@@ -6,14 +6,13 @@
 
 use crate::address::{Address, PhysAddr, VirtAddr};
 use crate::cpu::control_regs::write_cr3;
-use crate::cpu::cpuid::cpuid_table;
 use crate::cpu::features::{cpu_has_nx, cpu_has_pge};
 use crate::cpu::flush_tlb_global_sync;
 use crate::error::SvsmError;
 use crate::locking::{LockGuard, SpinLock};
 use crate::mm::alloc::{allocate_zeroed_page, free_page};
 use crate::mm::{phys_to_virt, virt_to_phys, PGTABLE_LVL3_IDX_SHARED};
-use crate::sev::status::vtom_enabled;
+use crate::platform::SvsmPlatform;
 use crate::types::{PageSize, PAGE_SIZE, PAGE_SIZE_2M};
 use crate::utils::immut_after_init::ImmutAfterInitCell;
 use crate::utils::MemoryRegion;
@@ -38,8 +37,8 @@ fn reinit_feature_mask(feature_mask: &PTEntryFlags) {
         .expect("could not reinit FEATURE_MASK");
 }
 
-pub fn paging_init_early(vtom: u64) {
-    init_encrypt_mask(vtom.try_into().unwrap());
+pub fn paging_init_early(platform: &dyn SvsmPlatform, vtom: u64) {
+    init_encrypt_mask(platform, vtom.try_into().unwrap());
 
     let mut feature_mask = PTEntryFlags::all();
     feature_mask.remove(PTEntryFlags::NX);
@@ -47,8 +46,8 @@ pub fn paging_init_early(vtom: u64) {
     reinit_feature_mask(&feature_mask);
 }
 
-pub fn paging_init(vtom: u64) {
-    init_encrypt_mask(vtom.try_into().unwrap());
+pub fn paging_init(platform: &dyn SvsmPlatform, vtom: u64) {
+    init_encrypt_mask(platform, vtom.try_into().unwrap());
 
     let mut feature_mask = PTEntryFlags::all();
     if !cpu_has_nx() {
@@ -60,33 +59,18 @@ pub fn paging_init(vtom: u64) {
     reinit_feature_mask(&feature_mask);
 }
 
-fn init_encrypt_mask(vtom: usize) {
-    // Determine whether VTOM is in use.
-
-    let zero: usize = 0;
-    let (private_pte_mask, shared_pte_mask, addr_mask_width) = if vtom_enabled() {
-        // Find the MSB of VTOM to define the number of bits in the effective
-        // address.
-        (zero, vtom, 63 - vtom.leading_zeros())
-    } else {
-        // Find C bit position
-        let res = cpuid_table(0x8000001f).expect("Can not get C-Bit position from CPUID table");
-        let c_bit = res.ebx & 0x3f;
-        let mask = 1u64 << c_bit;
-        (mask as usize, zero, c_bit)
-    };
+fn init_encrypt_mask(platform: &dyn SvsmPlatform, vtom: usize) {
+    let masks = platform.get_page_encryption_masks(vtom);
 
     PRIVATE_PTE_MASK
-        .reinit(&private_pte_mask)
+        .reinit(&masks.private_pte_mask)
         .expect("could not reinitialize PRIVATE_PTE_MASK");
     SHARED_PTE_MASK
-        .reinit(&shared_pte_mask)
+        .reinit(&masks.shared_pte_mask)
         .expect("could not reinitialize SHARED_PTE_MASK");
 
-    // Find physical address size.
-    let res = cpuid_table(0x80000008).expect("Can not get physical address size from CPUID table");
-    let guest_phys_addr_size = (res.eax >> 16) & 0xff;
-    let host_phys_addr_size = res.eax & 0xff;
+    let guest_phys_addr_size = (masks.phys_addr_sizes >> 16) & 0xff;
+    let host_phys_addr_size = masks.phys_addr_sizes & 0xff;
     let phys_addr_size = if guest_phys_addr_size == 0 {
         // When [GuestPhysAddrSize] is zero, refer to the PhysAddrSize field
         // for the maximum guest physical address size.
@@ -99,7 +83,7 @@ fn init_encrypt_mask(vtom: usize) {
     // If the C-bit is a physical address bit however, the guest physical
     // address space is effectively reduced by 1 bit.
     // - APM2, 15.34.6 Page Table Support
-    let effective_phys_addr_size = cmp::min(addr_mask_width, phys_addr_size);
+    let effective_phys_addr_size = cmp::min(masks.addr_mask_width, phys_addr_size);
 
     let max_addr = 1 << effective_phys_addr_size;
     MAX_PHYS_ADDR

kernel/src/platform/mod.rs
@@ -12,6 +12,14 @@ use bootlib::platform::SvsmPlatformType;
 pub mod native;
 pub mod snp;
 
+#[derive(Clone, Copy, Debug)]
+pub struct PageEncryptionMasks {
+    pub private_pte_mask: usize,
+    pub shared_pte_mask: usize,
+    pub addr_mask_width: u32,
+    pub phys_addr_sizes: u32,
+}
+
 /// This defines a platform abstraction to permit the SVSM to run on different
 /// underlying architectures.
 pub trait SvsmPlatform {
@@ -21,6 +29,9 @@ pub trait SvsmPlatform {
     /// Performs initialization of the platform runtime environment after
     /// console logging has been initialized.
     fn env_setup_late(&mut self);
+
+    /// Determines the paging encryption masks for the current architecture.
+    fn get_page_encryption_masks(&self, vtom: usize) -> PageEncryptionMasks;
 }
 
 //FIXME - remove Copy trait

kernel/src/platform/native.rs
@@ -4,7 +4,8 @@
 //
 // Author: Jon Lange <jlange@microsoft.com>
 
-use crate::platform::SvsmPlatform;
+use crate::cpu::cpuid::CpuidResult;
+use crate::platform::{PageEncryptionMasks, SvsmPlatform};
 
 #[derive(Clone, Copy, Debug)]
 pub struct NativePlatform {}
@@ -24,4 +25,14 @@ impl Default for NativePlatform {
 impl SvsmPlatform for NativePlatform {
     fn env_setup(&mut self) {}
     fn env_setup_late(&mut self) {}
+    fn get_page_encryption_masks(&self, _vtom: usize) -> PageEncryptionMasks {
+        // Find physical address size.
+        let res = CpuidResult::get(0x80000008, 0);
+        PageEncryptionMasks {
+            private_pte_mask: 0,
+            shared_pte_mask: 0,
+            addr_mask_width: 64,
+            phys_addr_sizes: res.eax,
+        }
+    }
 }

kernel/src/stage2.rs
@@ -99,7 +99,7 @@ fn setup_env(
         PhysAddr::null(),
     );
     register_cpuid_table(unsafe { &CPUID_PAGE });
-    paging_init_early(launch_info.vtom);
+    paging_init_early(platform, launch_info.vtom);
 
     // Bring up the GCHB for use from the SVSMIOPort console.
     verify_ghcb_version();

kernel/src/svsm.rs
@@ -297,7 +297,7 @@ pub extern "C" fn svsm_start(li: &KernelLaunchInfo, vb_addr: usize) {
         .expect("Already initialized launch info");
 
     let mut platform_cell = SvsmPlatformCell::new(li.platform_type);
-    let _platform = platform_cell.as_mut_dyn_ref();
+    let platform = platform_cell.as_mut_dyn_ref();
 
     init_cpuid_table(VirtAddr::from(launch_info.cpuid_page));
     dump_cpuid_table();
@@ -327,7 +327,7 @@ pub extern "C" fn svsm_start(li: &KernelLaunchInfo, vb_addr: usize) {
         Err(e) => panic!("error reading kernel ELF: {}", e),
     };
 
-    paging_init(li.vtom);
+    paging_init(platform, li.vtom);
     init_page_table(&launch_info, &kernel_elf).expect("Could not initialize the page table");
 
     // SAFETY: this PerCpu has just been allocated and no other CPUs have been

commit a3cba315d18edf12b82f0fe34b0110026c02f3cc

platform: Get correct phys addr sizes when not using vtom
The physical address sizes come from cpuid Fn8000_0008_EAX but due to a
variable shadowing problem, when vtom was disabled it was instead taking
it from cpuid Fn8000_001F_EAX.

Fixes: edf38b0c7f ("platform: Calculate PTE masks through platform abstraction")

Signed-off-by: Adam Dunlap <acdunlap@google.com>

kernel/src/platform/snp.rs
@@ -57,24 +57,25 @@ impl SvsmPlatform for SnpPlatform {
 
     fn get_page_encryption_masks(&self, vtom: usize) -> PageEncryptionMasks {
         // Find physical address size.
-        let res =
+        let processor_capacity =
             cpuid_table(0x80000008).expect("Can not get physical address size from CPUID table");
         if vtom_enabled() {
             PageEncryptionMasks {
                 private_pte_mask: 0,
                 shared_pte_mask: vtom,
                 addr_mask_width: vtom.leading_zeros(),
-                phys_addr_sizes: res.eax,
+                phys_addr_sizes: processor_capacity.eax,
             }
         } else {
             // Find C-bit position.
-            let res = cpuid_table(0x8000001f).expect("Can not get C-Bit position from CPUID table");
-            let c_bit = res.ebx & 0x3f;
+            let sev_capabilities =
+                cpuid_table(0x8000001f).expect("Can not get C-Bit position from CPUID table");
+            let c_bit = sev_capabilities.ebx & 0x3f;
             PageEncryptionMasks {
                 private_pte_mask: 1 << c_bit,
                 shared_pte_mask: 0,
                 addr_mask_width: c_bit,
-                phys_addr_sizes: res.eax,
+                phys_addr_sizes: processor_capacity.eax,
             }
         }
     }