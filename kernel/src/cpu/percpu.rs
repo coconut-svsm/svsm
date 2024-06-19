f13d49b4fd53147b70f4340b6eb82609aeabc5c7
Vasant Karasulli <vkarasulli@suse.de>
2024-06-19 09:43:08 +0200

    Implement a percpu line buffer to store the individual log messages.
    Each individual log message gets written into the global log buffer
    from the percpu line buffer.
    
    Signed-off-by: Vasant Karasulli <vkarasulli@suse.de>

--- kernel/src/cpu/percpu.rs
+++ kernel/src/cpu/percpu.rs
@@ -11,6 +11,7 @@ use super::tss::{X86Tss, IST_DF};
 use crate::address::{Address, PhysAddr, VirtAddr};
 use crate::cpu::apic::ApicError;
 use crate::cpu::idt::common::INT_INJ_VECTOR;
+use crate::cpu::line_buffer::LineBuffer;
 use crate::cpu::tss::TSS_LIMIT;
 use crate::cpu::vmsa::init_guest_vmsa;
 use crate::cpu::vmsa::vmsa_mut_ref_from_vaddr;
@@ -337,6 +338,7 @@ pub struct PerCpu {
     hv_doorbell: Cell<*const HVDoorbell>,
     init_stack: Cell<Option<VirtAddr>>,
     ist: IstStacks,
+    ln_buf: RefCell<LineBuffer>,
 
     /// Stack boundaries of the currently running task.
     current_stack: Cell<MemoryRegion<VirtAddr>>,
@@ -362,6 +364,7 @@ impl PerCpu {
             hv_doorbell: Cell::new(ptr::null()),
             init_stack: Cell::new(None),
             ist: IstStacks::new(),
+            ln_buf: RefCell::new(LineBuffer::new()),
             current_stack: Cell::new(MemoryRegion::new(VirtAddr::null(), 0)),
         }
     }
@@ -842,6 +845,10 @@ impl PerCpu {
         let mut tss = self.tss.get();
         tss.stacks[0] = addr;
         self.tss.set(tss);
+    }
+
+    pub fn get_line_buffer(&self) -> RefMut<'_, LineBuffer> {
+        self.ln_buf.borrow_mut()
     }
 }
 
