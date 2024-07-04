coconut-svsm/svsm

commit 978aef5a9d759a949a7c48cfc0773a02a94e4376

protocols/apic: update the registration-based APIC protocol
The current Alternate Injection specification describes the use of a
count-based registration mechanism for tracking the enablement of APIC
emulation, but the previous implementation of the code use the older
locking-based design.  The code must be revised to match the current
spec.

Signed-off-by: Jon Lange <jlange@microsoft.com>

kernel/src/platform/snp.rs
@@ -20,25 +20,18 @@ use crate::svsm_console::SVSMIOPort;
 use crate::types::PageSize;
 use crate::utils::MemoryRegion;
 
-use core::sync::atomic::{AtomicU8, Ordering};
+use core::sync::atomic::{AtomicU32, Ordering};
 
 static CONSOLE_IO: SVSMIOPort = SVSMIOPort::new();
 
-const APIC_EMULATION_DISABLED: u8 = 0;
-const APIC_EMULATION_ENABLED: u8 = 1;
-const APIC_EMULATION_LOCKED: u8 = 2;
-static APIC_EMULATION_STATE: AtomicU8 = AtomicU8::new(APIC_EMULATION_ENABLED);
+static APIC_EMULATION_REG_COUNT: AtomicU32 = AtomicU32::new(0);
 
 #[derive(Clone, Copy, Debug)]
-pub struct SnpPlatform {
-    use_alternate_injection: bool,
-}
+pub struct SnpPlatform {}
 
 impl SnpPlatform {
     pub fn new() -> Self {
-        Self {
-            use_alternate_injection: false,
-        }
+        Self {}
     }
 }
 
@@ -140,60 +133,47 @@ impl SvsmPlatform for SnpPlatform {
             return Err(SvsmError::NotSupported);
         }
 
-        self.use_alternate_injection = alt_inj_requested;
+        APIC_EMULATION_REG_COUNT.store(1, Ordering::Relaxed);
         Ok(())
     }
 
-    fn use_alternate_injection(&self) -> bool {
-        self.use_alternate_injection
-    }
-
-    fn lock_unlock_apic_emulation(&self, lock: bool) -> Result<(), SvsmError> {
-        // The lock state can only be changed if APIC emulation has not already
-        // been disabled on any CPU.
-        let new_state = if lock {
-            APIC_EMULATION_LOCKED
-        } else {
-            APIC_EMULATION_ENABLED
-        };
-        let mut current = APIC_EMULATION_STATE.load(Ordering::Relaxed);
+    fn change_apic_registration_state(&self, incr: bool) -> Result<bool, SvsmError> {
+        let mut current = APIC_EMULATION_REG_COUNT.load(Ordering::Relaxed);
         loop {
-            if current == APIC_EMULATION_DISABLED {
-                return Err(SvsmError::Apic);
-            }
-            match APIC_EMULATION_STATE.compare_exchange_weak(
+            let new = if incr {
+                // Incrementing is only possible if the registration count
+                // has not already dropped to zero, and only if the
+                // registration count will not wrap around.
+                if current == 0 {
+                    return Err(SvsmError::Apic);
+                }
+                current.checked_add(1).ok_or(SvsmError::Apic)?
+            } else {
+                // An attempt to decrement when the count is already zero is
+                // considered a benign race, which will not result in any
+                // actual change but will indicate that emulation is being
+                // disabled for the guest.
+                if current == 0 {
+                    return Ok(false);
+                }
+                current - 1
+            };
+            match APIC_EMULATION_REG_COUNT.compare_exchange_weak(
                 current,
-                new_state,
+                new,
                 Ordering::Relaxed,
                 Ordering::Relaxed,
             ) {
-                Ok(_) => break,
+                Ok(_) => {
+                    return Ok(new > 0);
+                }
                 Err(val) => current = val,
             }
         }
-
-        Ok(())
     }
 
-    fn disable_apic_emulation(&self) -> Result<(), SvsmError> {
-        // APIC emulation can only be disabled if it is not locked.
-        let mut current = APIC_EMULATION_STATE.load(Ordering::Relaxed);
-        loop {
-            if current == APIC_EMULATION_LOCKED {
-                return Err(SvsmError::Apic);
-            }
-            match APIC_EMULATION_STATE.compare_exchange_weak(
-                current,
-                APIC_EMULATION_DISABLED,
-                Ordering::Relaxed,
-                Ordering::Relaxed,
-            ) {
-                Ok(_) => break,
-                Err(val) => current = val,
-            }
-        }
-
-        Ok(())
+    fn query_apic_registration_state(&self) -> bool {
+        APIC_EMULATION_REG_COUNT.load(Ordering::Relaxed) > 0
     }
 
     fn post_irq(&self, icr: u64) -> Result<(), SvsmError> {

kernel/src/cpu/percpu.rs
@@ -656,7 +656,7 @@ impl PerCpu {
 
     pub fn alloc_guest_vmsa(&self) -> Result<(), SvsmError> {
         // Enable alternate injection if the hypervisor supports it.
-        let use_alternate_injection = SVSM_PLATFORM.as_dyn_ref().use_alternate_injection();
+        let use_alternate_injection = SVSM_PLATFORM.as_dyn_ref().query_apic_registration_state();
         if use_alternate_injection {
             self.apic.replace(Some(LocalApic::new()));
 
@@ -704,20 +704,13 @@ impl PerCpu {
         Ok(())
     }
 
-    pub fn disable_apic_emulation(&self) -> Result<(), SvsmError> {
-        let mut apic_cell = self.apic_mut();
-        if apic_cell.is_some() {
-            // APIC emulation cannot be disabled if the platform has locked
-            // the use of APIC emulation.
-            SVSM_PLATFORM.as_dyn_ref().disable_apic_emulation()?;
+    pub fn disable_apic_emulation(&self) {
+        if let Some(mut apic) = self.apic_mut() {
             let mut vmsa_ref = self.guest_vmsa_ref();
             let caa_addr = vmsa_ref.caa_addr();
             let vmsa = vmsa_ref.vmsa();
-            let mut apic = apic_cell.take().unwrap();
             apic.disable_apic_emulation(vmsa, caa_addr);
-            drop(vmsa_ref);
         }
-        Ok(())
     }
 
     pub fn clear_pending_interrupts(&self) {

kernel/src/platform/mod.rs
@@ -82,14 +82,12 @@ pub trait SvsmPlatform {
     /// Configures the use of alternate injection as requested.
     fn configure_alternate_injection(&mut self, alt_inj_requested: bool) -> Result<(), SvsmError>;
 
-    /// Indicates whether this system should make use of alternate injection.
-    fn use_alternate_injection(&self) -> bool;
+    /// Changes the state of APIC registration on this system, returning either
+    /// the current registration state or an error.
+    fn change_apic_registration_state(&self, incr: bool) -> Result<bool, SvsmError>;
 
-    /// Locks or unlocks the use of APIC emulation on this system.
-    fn lock_unlock_apic_emulation(&self, lock: bool) -> Result<(), SvsmError>;
-
-    /// Determines whether APIC emulation can be disabled on this system.
-    fn disable_apic_emulation(&self) -> Result<(), SvsmError>;
+    /// Queries the state of APIC registration on this system.
+    fn query_apic_registration_state(&self) -> bool;
 
     /// Signal an IRQ on one or more CPUs.
     fn post_irq(&self, icr: u64) -> Result<(), SvsmError>;

kernel/src/platform/native.rs
@@ -85,16 +85,12 @@ impl SvsmPlatform for NativePlatform {
         Ok(())
     }
 
-    fn use_alternate_injection(&self) -> bool {
+    fn change_apic_registration_state(&self, _incr: bool) -> Result<bool, SvsmError> {
+        Err(SvsmError::NotSupported)
+    }
+
+    fn query_apic_registration_state(&self) -> bool {
         false
-    }
-
-    fn lock_unlock_apic_emulation(&self, _lock: bool) -> Result<(), SvsmError> {
-        Err(SvsmError::NotSupported)
-    }
-
-    fn disable_apic_emulation(&self) -> Result<(), SvsmError> {
-        Err(SvsmError::NotSupported)
     }
 
     fn post_irq(&self, icr: u64) -> Result<(), SvsmError> {

kernel/src/platform/tdp.rs
@@ -82,16 +82,12 @@ impl SvsmPlatform for TdpPlatform {
         Err(SvsmError::Tdx)
     }
 
-    fn use_alternate_injection(&self) -> bool {
+    fn change_apic_registration_state(&self, _incr: bool) -> Result<bool, SvsmError> {
+        Err(SvsmError::NotSupported)
+    }
+
+    fn query_apic_registration_state(&self) -> bool {
         false
-    }
-
-    fn lock_unlock_apic_emulation(&self, _lock: bool) -> Result<(), SvsmError> {
-        Err(SvsmError::Tdx)
-    }
-
-    fn disable_apic_emulation(&self) -> Result<(), SvsmError> {
-        Err(SvsmError::Tdx)
     }
 
     fn post_irq(&self, _icr: u64) -> Result<(), SvsmError> {

kernel/src/protocols/apic.rs
@@ -15,16 +15,11 @@ const SVSM_REQ_APIC_READ_REGISTER: u32 = 2;
 const SVSM_REQ_APIC_WRITE_REGISTER: u32 = 3;
 const SVSM_REQ_APIC_CONFIGURE_VECTOR: u32 = 4;
 
-const SVSM_APIC_CONFIGURE_DISABLED: u64 = 0;
-const SVSM_APIC_CONFIGURE_ENABLED: u64 = 1;
-const SVSM_APIC_CONFIGURE_LOCKED: u64 = 2;
-
 pub const APIC_PROTOCOL: u32 = 3;
 pub const APIC_PROTOCOL_VERSION_MIN: u32 = 1;
 pub const APIC_PROTOCOL_VERSION_MAX: u32 = 1;
 
-const SVSM_ERR_APIC_CANNOT_DISABLE: u64 = 0;
-const SVSM_ERR_APIC_CANNOT_LOCK: u64 = 1;
+const SVSM_ERR_APIC_CANNOT_REGISTER: u64 = 0;
 
 fn apic_query_features(params: &mut RequestParams) -> Result<(), SvsmReqError> {
     // No features are supported beyond the base feature set.
@@ -33,22 +28,40 @@ fn apic_query_features(params: &mut RequestParams) -> Result<(), SvsmReqError> {
 }
 
 fn apic_configure(params: &RequestParams) -> Result<(), SvsmReqError> {
-    match params.rcx {
-        SVSM_APIC_CONFIGURE_DISABLED => this_cpu()
-            .disable_apic_emulation()
-            .map_err(|_| SvsmReqError::protocol(SVSM_ERR_APIC_CANNOT_DISABLE)),
-        SVSM_APIC_CONFIGURE_ENABLED => {
-            // If this fails, the platform is known not to be in the locked
-            // state, so any error can be ignored in that case.
-            let _ = SVSM_PLATFORM.as_dyn_ref().lock_unlock_apic_emulation(false);
-            Ok(())
+    let platform = SVSM_PLATFORM.as_dyn_ref();
+    let enabled = match params.rcx {
+        0b00 => {
+            // Query the current registration state of APIC emulation to
+            // determine whether it should be disabled on the current CPU.
+            platform.query_apic_registration_state()
         }
-        SVSM_APIC_CONFIGURE_LOCKED => SVSM_PLATFORM
-            .as_dyn_ref()
-            .lock_unlock_apic_emulation(false)
-            .map_err(|_| SvsmReqError::protocol(SVSM_ERR_APIC_CANNOT_LOCK)),
-        _ => Err(SvsmReqError::invalid_parameter()),
+
+        0b01 => {
+            // Deregister APIC emulation if possible, noting whether it is now
+            // disabled for the platform.  This cannot fail.
+            platform.change_apic_registration_state(false).unwrap()
+        }
+
+        0b10 => {
+            // Increment the APIC emulation registration count.  If successful,
+            // this will not cause any change to the state of the current CPU.
+            return match platform.change_apic_registration_state(true) {
+                Ok(_) => Ok(()),
+                Err(_) => Err(SvsmReqError::protocol(SVSM_ERR_APIC_CANNOT_REGISTER)),
+            };
+        }
+
+        _ => {
+            return Err(SvsmReqError::invalid_parameter());
+        }
+    };
+
+    // Disable APIC emulation on the current CPU if required.
+    if !enabled {
+        this_cpu().disable_apic_emulation();
     }
+
+    Ok(())
 }
 
 fn apic_read_register(params: &mut RequestParams) -> Result<(), SvsmReqError> {

commit 6946a5496c2132d4610fc34c71caad159b413f3e

platform/snp: enable alternate injection only if requested
The current logic for enabling alternate injection is broken, as it
will enable it always. This is causing a crash during boot when
running under a hypervisor that does not support the feature:

    [SVSM] ERROR: Panic: CPU[0] panicked at kernel/src/svsm.rs:449:36:
    Failed to setup guest VMSA/CAA: Ghcb(VmgexitError(2, 6))
    [SVSM] ---BACKTRACE---:
    [SVSM]   [0xffffff8000128445]
    [SVSM]   Invalid frame
    [SVSM] ---END---

Fix this by checking upfront if the feature was requested.

Fixes: 978aef5a9d75 ("protocols/apic: update the registration-based APIC protocol")
Signed-off-by: Carlos López <carlos.lopez@suse.com>

kernel/src/platform/snp.rs
@@ -126,11 +126,13 @@ impl SvsmPlatform for SnpPlatform {
     }
 
     fn configure_alternate_injection(&mut self, alt_inj_requested: bool) -> Result<(), SvsmError> {
+        if !alt_inj_requested {
+            return Ok(());
+        }
+
         // If alternate injection was requested, then it must be supported by
         // the hypervisor.
-        if alt_inj_requested
-            && !hypervisor_ghcb_features().contains(GHCBHvFeatures::SEV_SNP_EXT_INTERRUPTS)
-        {
+        if !hypervisor_ghcb_features().contains(GHCBHvFeatures::SEV_SNP_EXT_INTERRUPTS) {
             return Err(SvsmError::NotSupported);
         }
 