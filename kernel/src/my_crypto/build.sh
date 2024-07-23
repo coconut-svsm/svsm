f1c69d732e74bfce3073b1f4d80a5d7828881ce8
Teofil Bodea <teofil.bodea@tum.de>
2024-07-23 12:59:47 +0000

    Automatically create folder for libmy_crypto

--- kernel/src/my_crypto/build.sh
+++ kernel/src/my_crypto/build.sh
@@ -14,6 +14,7 @@ gcc -mno-sse -mno-sse2 -mno-avx -fno-tree-vectorize -nostdlib -Ihacl/karamel/ -I
 gcc -mno-sse -mno-sse2 -mno-avx -fno-tree-vectorize -nostdlib -Ihacl/karamel/ -Ihacl/karamel/krmllib/dist/minimal -Ihacl/karamel/include/ -Ihacl/include -fPIC \
 	-c hacl/Hacl_MAC_Poly1305.c
 ar rcs libmy_crypto.a Hacl_Curve25519_51.o Hacl_NaCl.o Hacl_Hash_SHA3.o Hacl_Salsa20.o Hacl_MAC_Poly1305.o my_crypto.o 
+mkdir -p ../../../libmy_crypto/
 cp libmy_crypto.a ../../../libmy_crypto/libmy_crypto.a 
 cp my_crypto.h ../../../../module/include/
 cp libmy_crypto.a ../../../../module/include/
