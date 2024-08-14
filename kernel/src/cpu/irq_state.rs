6229afa08c05a2df107cab6cfdc55544ded12dd5
Joerg Roedel <jroedel@suse.de>
2024-08-14 10:05:21 +0200

    kernel/cpu: Introduce IRQ state handling infrastructure
    Introduce low-level helpers for IRQ managment and the IrqState struct.
    This struct will be used to track per-cpu IRQ states and counts the
    nesting level of irq_disable() calls. It will keep track of the IRQ
    state when the nesting level was 0 and restores is when the level
    reaches 0 again.
    
    Co-authored-by: Tom Dohrmann <Erbse.13@gmx.de>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

---
+++ kernel/src/cpu/irq_state.rs
@@ -0,0 +1,182 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2024 SUSE LLC
+//
+// Author: Joerg Roedel <jroedel@suse.de>
+
+use core::arch::asm;
+use core::marker::PhantomData;
+use core::sync::atomic::{AtomicBool, AtomicIsize, Ordering};
+
+/// Interrupt flag in RFLAGS register
+const EFLAGS_IF: u64 = 1 << 9;
+
+/// Unconditionally disable IRQs
+///
+/// # Safety
+///
+/// Callers need to take care of re-enabling IRQs.
+#[inline(always)]
+pub unsafe fn raw_irqs_disable() {
+    asm!("cli", options(att_syntax, preserves_flags, nomem));
+}
+
+/// Unconditionally enable IRQs
+///
+/// # Safety
+///
+/// Callers need to make sure it is safe to enable IRQs. e.g. that no data
+/// structures or locks which are accessed in IRQ handlers are used after IRQs
+/// have been enabled.
+#[inline(always)]
+pub unsafe fn raw_irqs_enable() {
+    asm!("sti", options(att_syntax, preserves_flags, nomem));
+}
+
+/// Query IRQ state on current CPU
+///
+/// # Returns
+///
+/// `true` when IRQs are enabled, `false` otherwise
+#[inline(always)]
+#[must_use = "Unused irqs_enabled() result - meant to be irq_enable()?"]
+pub fn irqs_enabled() -> bool {
+    // SAFETY: The inline assembly just reads the processors RFLAGS register
+    // and does not change any state.
+    let state: u64;
+    unsafe {
+        asm!("pushfq",
+             "popq {}",
+             out(reg) state,
+             options(att_syntax, preserves_flags));
+    };
+
+    (state & EFLAGS_IF) == EFLAGS_IF
+}
+
+/// Query IRQ state on current CPU
+///
+/// # Returns
+///
+/// `false` when IRQs are enabled, `true` otherwise
+#[inline(always)]
+#[must_use = "Unused irqs_disabled() result - meant to be irq_disable()?"]
+pub fn irqs_disabled() -> bool {
+    !irqs_enabled()
+}
+
+/// This structure keeps track of PerCpu IRQ states. It tracks the original IRQ
+/// state and how deep IRQ-disable calls have been nested. The use of atomics
+/// is necessary for interior mutability and to make state modifications safe
+/// wrt. to IRQs.
+///
+/// The original state needs to be stored to not accidentially enable IRQs in
+/// contexts which have IRQs disabled by other means, e.g. in an exception or
+/// NMI/HV context.
+#[derive(Debug, Default)]
+pub struct IrqState {
+    /// IRQ state when count was `0`
+    state: AtomicBool,
+    /// Depth of IRQ-disabled nesting
+    count: AtomicIsize,
+    /// Make the type !Send + !Sync
+    phantom: PhantomData<*const ()>,
+}
+
+impl IrqState {
+    /// Create a new instance of `IrqState`
+    pub fn new() -> Self {
+        Self {
+            state: AtomicBool::new(false),
+            count: AtomicIsize::new(0),
+            phantom: PhantomData,
+        }
+    }
+
+    /// Increase IRQ-disable nesting level by 1. The method will disable IRQs.
+    ///
+    /// # Safety
+    ///
+    /// The caller needs to make sure to match the number of `disable` calls
+    /// with the number of `enable` calls.
+    #[inline(always)]
+    pub unsafe fn disable(&self) {
+        let state = irqs_enabled();
+
+        raw_irqs_disable();
+        let val = self.count.fetch_add(1, Ordering::Relaxed);
+
+        assert!(val >= 0);
+
+        if val == 0 {
+            self.state.store(state, Ordering::Relaxed)
+        }
+    }
+
+    /// Decrease IRQ-disable nesting level by 1. The method will restore the
+    /// original IRQ state when the nesting level reaches 0.
+    ///
+    /// # Safety
+    ///
+    /// The caller needs to make sure to match the number of `disable` calls
+    /// with the number of `enable` calls.
+    #[inline(always)]
+    pub unsafe fn enable(&self) {
+        debug_assert!(irqs_disabled());
+
+        let val = self.count.fetch_sub(1, Ordering::Relaxed);
+
+        assert!(val > 0);
+
+        if val == 1 {
+            let state = self.state.load(Ordering::Relaxed);
+            if state {
+                raw_irqs_enable();
+            }
+        }
+    }
+}
+
+impl Drop for IrqState {
+    /// This struct should never be dropped. Add a debug check in case it is
+    /// dropped anyway.
+    fn drop(&mut self) {
+        let count = self.count.load(Ordering::Relaxed);
+        assert_eq!(count, 0);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn irq_enable_disable() {
+        assert!(irqs_disabled());
+        unsafe {
+            raw_irqs_enable();
+            assert!(irqs_enabled());
+            raw_irqs_disable();
+            assert!(irqs_disabled());
+        }
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn irq_state() {
+        assert!(irqs_disabled());
+        unsafe {
+            let state = IrqState::new();
+            raw_irqs_enable();
+            state.disable();
+            assert!(irqs_disabled());
+            state.disable();
+            state.enable();
+            assert!(irqs_disabled());
+            state.enable();
+            assert!(irqs_enabled());
+            raw_irqs_disable();
+        }
+    }
+}
