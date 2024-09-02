461b15e8b74c377fb4b554af26ea1e1cf2e4b060
Peter Fang <peter.fang@intel.com>
2024-09-02 18:32:37 +0800

    syscall: Implement exec()
    Implement the EXEC syscall interface in user space.
    
    Co-developed-by: Vijay Dhanraj <vijay.dhanraj@intel.com>
    Signed-off-by: Peter Fang <peter.fang@intel.com>

--- syscall/src/class0.rs
+++ syscall/src/class0.rs
@@ -4,8 +4,9 @@
 //
 // Author: Chuanxiao Dong <chuanxiao.dong@intel.com>
 
-use super::call::syscall1;
-use super::SYS_EXIT;
+use super::call::{syscall1, syscall3, SysCallError};
+use super::{SYS_EXEC, SYS_EXIT};
+use core::ffi::CStr;
 
 pub fn exit(code: u32) -> ! {
     // SAFETY: SYS_EXIT is supported syscall number by the svsm kernel.
@@ -13,4 +14,20 @@ pub fn exit(code: u32) -> ! {
         let _ = syscall1(SYS_EXIT, u64::from(code));
     }
     unreachable!("Should never return from SYS_EXIT syscall");
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Tid(u32);
+
+pub fn exec(file: &CStr, root: &CStr, flags: u32) -> Result<Tid, SysCallError> {
+    unsafe {
+        syscall3(
+            SYS_EXEC,
+            file.as_ptr() as u64,
+            root.as_ptr() as u64,
+            u64::from(flags),
+        )
+        .map(|ret| Tid(ret as u32))
+    }
 }
