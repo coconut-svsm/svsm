b063b8d71c0c12c0b2d333eccdb84d865ed28561
Peter Fang <peter.fang@intel.com>
2024-09-14 13:10:16 +0800

    kernel/fs: Add FsObj
    Add FsObj which is the primary kernel object used for Class 1 syscalls.
    This object represents both a file and a directory entry. Also, add
    as_fs() to the Obj trait that allows for these objects to downcast back
    to FsObjs.
    
    Co-developed-by: Vijay Dhanraj <vijay.dhanraj@intel.com>
    Signed-off-by: Peter Fang <peter.fang@intel.com>

---
+++ kernel/src/fs/obj.rs
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2024 Intel Corporation.
+//
+// Author: Peter Fang <peter.fang@intel.com>
+
+extern crate alloc;
+
+use super::{DirEntry, Directory, FileHandle, FileName};
+use crate::error::SvsmError;
+use crate::syscall::Obj;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+#[derive(Debug)]
+struct DirectoryHandle {
+    dir: Arc<dyn Directory>,
+    list: Vec<FileName>,
+    next: AtomicUsize,
+}
+
+impl DirectoryHandle {
+    fn new(dir: &Arc<dyn Directory>) -> Self {
+        DirectoryHandle {
+            dir: dir.clone(),
+            list: dir.list(),
+            next: AtomicUsize::new(0),
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum FsObjEntry {
+    File(FileHandle),
+    Directory(DirectoryHandle),
+}
+
+#[derive(Debug)]
+pub struct FsObj {
+    entry: FsObjEntry,
+}
+
+impl FsObj {
+    pub fn new_dir(dir: &Arc<dyn Directory>) -> Self {
+        FsObj {
+            entry: FsObjEntry::Directory(DirectoryHandle::new(dir)),
+        }
+    }
+
+    pub fn readdir(&self) -> Result<Option<(FileName, DirEntry)>, SvsmError> {
+        let FsObjEntry::Directory(ref dh) = self.entry else {
+            return Err(SvsmError::NotSupported);
+        };
+
+        let next = dh.next.fetch_add(1, Ordering::Relaxed);
+        if let Some(&name) = dh.list.get(next) {
+            let dirent = dh.dir.lookup_entry(name)?;
+            Ok(Some((name, dirent)))
+        } else {
+            dh.next.fetch_sub(1, Ordering::Relaxed);
+            Ok(None)
+        }
+    }
+}
+
+impl Obj for FsObj {
+    fn as_fs(&self) -> Option<&FsObj> {
+        Some(self)
+    }
+}
