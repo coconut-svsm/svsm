24be6b7766aa69ba66bb155cd4fde9b1b8d69992
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-09-14 10:51:44 +0800

    syscall/obj: Define ObjHandle for the user mode
    The object is exposed to the user mode via the object-opening related
    syscalls, which returns the id of the object created by the COCONUT-SVSM
    kernel. The user mode can make use this id to access the corresponding
    object via other syscalls.
    
    From the user mode's point of view, an ObjHanle is defined to wrap a u32
    which is the value returned by an object-opening syscall. This u32 value
    can be used as the input for the syscalls to access the corresponding
    kernel object. The ObjHandle doesn't implement Copy/Clone trait, and the
    From trait is implemented for a reference to ObjHandle to create a u32.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

---
+++ syscall/src/obj.rs
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2024 Intel Corporation.
+//
+// Author: Chuanxiao Dong <chuanxiao.dong@intel.com>
+
+/// The object is exposed to the user mode via the object-opening related
+/// syscalls, which returns the id of the object created by the COCONUT-SVSM
+/// kernel. The user mode can make use this id to access the corresponding
+/// object via other syscalls. From the user mode's point of view, an
+/// ObjHanle is defined to wrap a u32 which is the value returned by an
+/// object-opening syscall. This u32 value can be used as the input for the
+/// syscalls to access the corresponding kernel object.
+#[derive(Debug)]
+pub struct ObjHandle(u32);
+
+impl ObjHandle {
+    #[allow(dead_code)]
+    pub(crate) fn new(id: u32) -> Self {
+        Self(id)
+    }
+}
+
+impl From<&ObjHandle> for u32 {
+    #[inline]
+    fn from(obj_handle: &ObjHandle) -> Self {
+        obj_handle.0
+    }
+}
