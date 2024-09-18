e6cbe835f33879e27fd242aa185d67c0ab0013a0
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-09-18 12:41:50 +0800

    kernel/syscall: Implement close syscall
    The close syscall is implemented by using the obj_remove, to remove the
    ObjHandle associated witht the input object id from the current task.
    
    Co-developed-by: Vijay Dhanraj <vijay.dhanraj@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- kernel/src/cpu/idt/svsm.rs
+++ kernel/src/cpu/idt/svsm.rs
@@ -248,8 +248,10 @@ extern "C" fn ex_handler_system_call(
 
     ctxt.regs.rax = match input {
         SYS_EXIT => sys_exit(ctxt.regs.rdi as u32),
-        _ => !0,
-    };
+        SYS_CLOSE => sys_close(ctxt.regs.rdi as u32),
+        _ => Err(SysCallError::EINVAL),
+    }
+    .map_or_else(|e| e as usize, |v| v as usize);
 }
 
 #[no_mangle]
--- syscall/src/def.rs
+++ syscall/src/def.rs
@@ -9,3 +9,4 @@ const CLASS0: u64 = 0;
 
 // Syscall number in class0
 pub const SYS_EXIT: u64 = CLASS0;
+pub const SYS_CLOSE: u64 = CLASS0 + 10;
