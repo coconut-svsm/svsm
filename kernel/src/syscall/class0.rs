7e21ba8bce6da1e76d7cf86acedadcac15beabf5
Chuanxiao Dong <chuanxiao.dong@intel.com>
2024-09-18 12:41:50 +0800

    kernel/syscall: Implement close syscall
    The close syscall is implemented by using the obj_remove, to remove the
    ObjHandle associated witht the input object id from the current task.
    
    Co-developed-by: Vijay Dhanraj <vijay.dhanraj@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- kernel/src/syscall/class0.rs
+++ kernel/src/syscall/class0.rs
@@ -4,7 +4,9 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use super::obj::obj_close;
 use crate::task::{current_task_terminated, schedule};
+use syscall::SysCallError;
 
 pub fn sys_exit(exit_code: u32) -> ! {
     log::info!("Terminating current task, exit_code {exit_code}");
@@ -12,5 +14,12 @@ pub fn sys_exit(exit_code: u32) -> ! {
         current_task_terminated();
     }
     schedule();
-    panic!("schedule() returned in sys_exit()");
+    unreachable!("schedule() returned in sys_exit()");
+}
+
+pub fn sys_close(obj_id: u32) -> Result<u64, SysCallError> {
+    // According to syscall ABI/API spec, close always returns 0 even
+    // if called with an invalid handle
+    let _ = obj_close(obj_id.into());
+    Ok(0)
 }
