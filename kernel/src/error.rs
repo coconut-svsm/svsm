3121843f01a2926d35fea81e92ce910871d7177a
Vijay Dhanraj <vijay.dhanraj@intel.com>
2024-10-03 23:23:49 -0700

    mm/guestmem: Introduce UserPtr wrapper around GuestPtr
    In addition to ensuring invalid memory access, the UserPtr
    wrapper around GuestPtr validates the address passed from
    the userspace to make sure it isn't pointing to kernel memory.
    
    Signed-off-by: Vijay Dhanraj <vijay.dhanraj@intel.com>

--- kernel/src/error.rs
+++ kernel/src/error.rs
@@ -75,6 +75,8 @@ pub enum SvsmError {
     InvalidAddress,
     /// Error reported when convert a usize to Bytes
     InvalidBytes,
+    /// Error reported when converting to UTF-8
+    InvalidUtf8,
     /// Errors related to firmware parsing
     Firmware,
     /// Errors related to console operation
@@ -131,7 +133,8 @@ impl From<SvsmError> for SysCallError {
             | SvsmError::Obj(ObjError::InvalidHandle)
             | SvsmError::Mem
             | SvsmError::InvalidAddress
-            | SvsmError::InvalidBytes => SysCallError::EINVAL,
+            | SvsmError::InvalidBytes
+            | SvsmError::InvalidUtf8 => SysCallError::EINVAL,
 
             _ => SysCallError::UNKNOWN,
         }
