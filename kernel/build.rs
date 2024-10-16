7c521badd842460be0ba73906848077a114c8850
ziqiaozhou <ziqiaozhou@microsoft.com>
2024-10-15 19:39:43 -0700

    Verify setup (#1)
    Add verus tool setup scripts + CI + dependencies + import some proofs from verismo

--- kernel/build.rs
+++ kernel/build.rs
@@ -4,10 +4,27 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use rustc_version::{Channel, Version};
+
 fn main() {
-    // Extra cfgs
-    println!("cargo::rustc-check-cfg=cfg(fuzzing)");
-    println!("cargo::rustc-check-cfg=cfg(test_in_svsm)");
+    let rust_version = rustc_version::version_meta().unwrap();
+    // Check if the version is nightly and higher than 1.78.0
+    let is_expected_version = rust_version.semver >= Version::new(1, 78, 0);
+    if !is_expected_version {
+        if rust_version.channel == Channel::Nightly {
+            // Print the cargo:rustc-cfg directive to enable the feature
+            println!("cargo:rustc-cfg=RUST_BEFORE_1_78");
+        } else {
+            // Optionally handle the case for non-nightly versions
+            panic!("Requires the nightly version or stable version >= 1.78.");
+        }
+    } else {
+        // Extra cfgs
+        println!("cargo::rustc-check-cfg=cfg(fuzzing)");
+        println!("cargo::rustc-check-cfg=cfg(test_in_svsm)");
+        println!("cargo::rustc-check-cfg=cfg(verus_keep_ghost)");
+        println!("cargo::rustc-check-cfg=cfg(RUST_BEFORE_1_78)");
+    }
 
     // Stage 2
     println!("cargo:rustc-link-arg-bin=stage2=-nostdlib");
@@ -39,4 +56,23 @@ fn main() {
 
     println!("cargo:rerun-if-changed=kernel/src/stage2.lds");
     println!("cargo:rerun-if-changed=kernel/src/svsm.lds");
+    println!("cargo:rerun-if-changed=build.rs");
+    init_verify();
+}
+
+fn init_verify() {
+    if cfg!(feature = "noverify") {
+        println!("cargo:rustc-env=VERUS_ARGS=--no-verify");
+    } else {
+        let verus_args = [
+            "--rlimit=8000",
+            "--expand-errors",
+            "--multiple-errors=5",
+            "--triggers-silent",
+            "--no-auto-recommends-check",
+            "--trace",
+            "-Z unstable-options",
+        ];
+        println!("cargo:rustc-env=VERUS_ARGS={}", verus_args.join(" "));
+    }
 }
---
+++ scripts/vinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VERISMO_REV=7c9c445
+cargo install --git https://github.com/microsoft/verismo/ --rev $VERISMO_REV cargo-v
+builtin=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "builtin_macros") | .targets[].src_path'`
+verus=`dirname $builtin`/../../../source/target-verus/release/verus
+if [ -f ${verus} ]; then
+    echo "verus (${verus}) is already built"
+else
+    cargo v prepare-verus 
+fi
+cargo install --git https://github.com/microsoft/verismo/ --rev $VERISMO_REV verus-rustc
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/verus-lang/verusfmt/releases/download/v0.4.3/verusfmt-installer.sh | sh
