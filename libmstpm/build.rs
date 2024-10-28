922106df7ea477eba94e769cf5fb159fe125fff6
Tom Dohrmann <erbse.13@gmx.de>
2024-10-28 09:30:59 +0000

    kernel: move linker args in build.rs to libmstpm
    Linker args shouldn't be set by the crate consuming a library, but by
    the crate producing a library.
    
    Signed-off-by: Tom Dohrmann <erbse.13@gmx.de>

--- libmstpm/build.rs
+++ libmstpm/build.rs
@@ -4,6 +4,7 @@
 //
 // Authors: Claudio Carvalho <cclaudio@linux.ibm.com>
 
+use std::env::current_dir;
 use std::process::Command;
 use std::process::Stdio;
 
@@ -17,4 +18,10 @@ fn main() {
     if !output.status.success() {
         panic!();
     }
+
+    // Tell cargo to link libmstpm and where to find it.
+    let cwd = current_dir().unwrap();
+    let cwd = cwd.as_os_str().to_str().unwrap();
+    println!("cargo:rustc-link-search={cwd}");
+    println!("cargo:rustc-link-lib=mstpm");
 }
