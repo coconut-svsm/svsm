2d918acca5d5e541f4c9a62af3957c8a793c2f2f
Tom Dohrmann <erbse.13@gmx.de>
2024-10-28 09:08:54 +0000

    libmstpm: store generated bindings in OUT_DIR
    Generated code shouldn't be stored directly in the src directory. All
    files generated by build-scripts should be stored in the directory
    pointed to in the OUT_DIR environment variable.
    One advantage of this is that we can tell the build-script to emit
    rerun-if statements that tell cargo to only rerun if any of the files
    in this directory have changed. If we generate files into this
    directory, cargo may not always realize that the generated files
    shouldn't trigger a re-run.
    
    Signed-off-by: Tom Dohrmann <erbse.13@gmx.de>

--- libmstpm/src/lib.rs
+++ libmstpm/src/lib.rs
@@ -10,4 +10,12 @@
 #![no_std]
 
 /// C bindings
-pub mod bindings;
+pub mod bindings {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(unused)]
+    #![allow(improper_ctypes)]
+
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
