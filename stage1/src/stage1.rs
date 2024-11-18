51356e0821e5947fc7baeefab7a9ec987061db67
Peter Fang <peter.fang@intel.com>
2024-11-18 15:47:01 -0800

    bootlib: Add stage2 constants
    Turn numerous stage2 parameters into constants and use them in
    igvmbuilder/stage1/stage2 code. This allows for better stage2
    configuration and creates necessary dependencies among these components
    to reduce the possibility of breakage when there's a change in stage2.
    
    Signed-off-by: Peter Fang <peter.fang@intel.com>

--- stage1/src/stage1.rs
+++ stage1/src/stage1.rs
@@ -7,12 +7,15 @@
 #![no_std]
 #![no_main]
 
+#[cfg(not(feature = "load-stage2"))]
+use bootlib::kernel_launch::Stage2LaunchInfo;
+#[cfg(feature = "load-stage2")]
+use bootlib::kernel_launch::{CPUID_PAGE, SECRETS_PAGE, STAGE2_INFO_SZ, STAGE2_MAXLEN};
+use bootlib::kernel_launch::{STAGE2_STACK, STAGE2_START};
 use core::arch::global_asm;
+#[cfg(not(feature = "load-stage2"))]
+use core::mem::offset_of;
 use core::panic::PanicInfo;
-
-// Stage2 is loaded at 8 MB + 16 KB
-const STAGE2_START: u32 = 0x808000;
-const STAGE1_STACK: u32 = 0x806000;
 
 // Reset vector
 global_asm!(include_str!("reset.S"), options(att_syntax));
@@ -85,14 +88,14 @@ global_asm!(
         addl    %ebp, %edi
         pushl   %edi
 
-        /* The stage2 area ends at 0x8A0000. */
-        pushl   $0x8A0000
+        /* Push the end of stage 2 area as described by the metadata */
+        pushl   $({STAGE2_START} + {STAGE2_MAXLEN})
 
         /* Push the location of the secrets page.  It is at 8 MB plus 56 KB */
-        pushl   $0x806000
+        pushl   ${SECRETS_PAGE}
 
         /* Push the location of the CPUID page.  It is at 8 MB plus 60 KB */
-        pushl   $0x807000
+        pushl   ${CPUID_PAGE}
 
         /* Push the value 1 to indicate SNP */
         pushl   $1
@@ -106,7 +109,10 @@ global_asm!(
 
         jmp     .Lenter_stage2"#,
     STAGE2_START = const STAGE2_START,
-    STAGE1_STACK = const STAGE1_STACK,
+    STAGE2_MAXLEN = const STAGE2_MAXLEN,
+    STAGE1_STACK = const STAGE2_STACK + STAGE2_INFO_SZ,
+    SECRETS_PAGE = const SECRETS_PAGE,
+    CPUID_PAGE = const CPUID_PAGE,
     options(att_syntax)
 );
 
@@ -121,7 +127,7 @@ global_asm!(
          * Stage 2 launch info has been prepared
          * Make sure platform type is TDP
          */
-        movl    $({STAGE1_STACK} - 40), %eax
+        movl    $({STAGE2_STACK} + {PLATFORM_TYPE_OFF}), %eax
         movl    (%eax), %eax
         cmpl    $2, %eax
         je      .Lsetup_td
@@ -138,9 +144,9 @@ global_asm!(
 
     .Lsetup_bsp_stack:
         /* Set up BSP stack for stage 2 */
-        movl    $({STAGE1_STACK} - 48), %esp
+        movl    $({STAGE2_STACK}), %esp
         /* %ebx is initialized with GPAW - save (1u64 << (GPAW - 1)) to vtom */
-        mov     %esp, %eax
+        movl    $({STAGE2_STACK} + {VTOM_OFF}), %eax
         /* GPAW must be either 48 or 52 */
         xorl    %ecx, %ecx
         movl    %ecx, (%eax)
@@ -149,7 +155,9 @@ global_asm!(
         bts     %ebx, %ecx
         movl    %ecx, (%eax)
         jmp     .Lenter_stage2"#,
-    STAGE1_STACK = const STAGE1_STACK,
+    STAGE2_STACK = const STAGE2_STACK,
+    PLATFORM_TYPE_OFF = const offset_of!(Stage2LaunchInfo, platform_type) as u32,
+    VTOM_OFF = const offset_of!(Stage2LaunchInfo, vtom) as u32,
     options(att_syntax)
 );
 
