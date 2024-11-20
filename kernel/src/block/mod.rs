961c8a97f28db7eb1553fa049d76eaba064276f4
Stefano Garzarella <sgarzare@redhat.com>
2024-11-20 12:10:45 +0100

    kernel: Add block layer api
    Add a simple block layer api trait.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

---
+++ kernel/src/block/mod.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2024 Red Hat, Inc.
+//
+// Author: Oliver Steffen <osteffen@redhat.com>
+
+pub mod api;
+pub mod error;
+
+pub use error::BlockDeviceError;
