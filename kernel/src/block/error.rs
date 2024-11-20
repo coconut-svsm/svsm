bd4c51be6a903065d006d6f13060883226a922ca
Stefano Garzarella <sgarzare@redhat.com>
2024-11-20 12:10:45 +0100

    kernel: Add block layer api
    Add a simple block layer api trait.
    
    Co-developed-by: Oliver Steffen <osteffen@redhat.com>
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

---
+++ kernel/src/block/error.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2025 Red Hat, Inc.
+//
+// Author: Oliver Steffen <osteffen@redhat.com>
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BlockDeviceError {
+    Failed, // ToDo: insert proper errors
+}
