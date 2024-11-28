5127b872b8498fb0df8d432e8e3d25bcb369438f
Oliver Steffen <osteffen@redhat.com>
2024-11-28 12:39:40 +0100

    kernel: Add in-svsm tests for VirtioBlkDriver
    Add read and write tests for the VirtioBlkDriver.
    
    Random data is read from/written to a disk image and the
    sha256 sums calulcated in SVSM are then compared to the ones
    seen on the host to test the whole path of the data.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- kernel/src/block/virtio_blk.rs
+++ kernel/src/block/virtio_blk.rs
@@ -59,3 +59,163 @@ impl BlockDriver for VirtIOBlkDriver {
         self.0.device.lock().capacity() as usize * SECTOR_SIZE
     }
 }
+
+#[cfg(all(test, test_in_svsm))]
+mod tests {
+    use crate::{address::PhysAddr, fw_cfg::FwCfg, platform::SVSM_PLATFORM};
+    extern crate alloc;
+
+    use super::*;
+
+    /// Find the first virtio-blk device in the hardware-info list
+    fn get_blk_device() -> VirtIOBlkDriver {
+        let cfg = FwCfg::new(SVSM_PLATFORM.get_io_port());
+
+        let dev = cfg
+            .get_virtio_mmio_addresses()
+            .unwrap_or_default()
+            .iter()
+            .find_map(|a| VirtIOBlkDriver::new(PhysAddr::from(*a)).ok())
+            .expect("No virtio-blk device found");
+
+        dev
+    }
+
+    /// Get the sha256 sum of the disk image from the host (see `scripts/test-in-svsm.sh`)
+    fn get_image_hash_from_host() -> Option<[u8; 32]> {
+        use crate::serial::Terminal;
+        use crate::testing::{svsm_test_io, IORequest};
+        use crate::testutils::is_qemu_test_env;
+
+        if !is_qemu_test_env() {
+            return None;
+        }
+
+        let sp = svsm_test_io().unwrap();
+
+        sp.put_byte(IORequest::GetStateImageMd5 as u8);
+
+        let mut expected_measurement = [0u8; 32];
+        for byte in &mut expected_measurement {
+            *byte = sp.get_byte();
+        }
+
+        Some(expected_measurement)
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_read_4sectors() {
+        virtio_read(4);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_read_8sectors() {
+        virtio_read(8);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_read_9sectors() {
+        virtio_read(9);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_read_17sectors() {
+        virtio_read(17);
+    }
+
+    /// Read from the block device using the specified buffer size for earch
+    /// request. Then verify the data was correctly read by comparing the sha256
+    /// sums of the read data and the image as seen on the host.
+    fn virtio_read(sectors_at_once: usize) {
+        use alloc::vec;
+        use sha2::{Digest, Sha256};
+        assert!(sectors_at_once > 0);
+        let blk = get_blk_device();
+
+        let expected_hash = get_image_hash_from_host().unwrap();
+
+        let n_sectors = blk.size() / SECTOR_SIZE;
+        let mut buffer = vec![0u8; sectors_at_once * SECTOR_SIZE];
+
+        let mut hasher = Sha256::new();
+        let mut sector = 0usize;
+        while sector < n_sectors {
+            if n_sectors - sector < buffer.len() {
+                buffer.truncate(SECTOR_SIZE);
+            }
+
+            blk.read_blocks(sector, &mut buffer).unwrap();
+            hasher.update(&buffer);
+            sector += buffer.len() / SECTOR_SIZE;
+        }
+
+        let hash = hasher.finalize();
+        assert_eq!(expected_hash, *hash);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_write_4sectors() {
+        virtio_write(4);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_write_8sectors() {
+        virtio_write(8);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_write_9sectors() {
+        virtio_write(9);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    fn test_virtio_write_17sectors() {
+        virtio_write(17);
+    }
+
+    /// Write to the block device and fill it with (random) data, writing with the
+    /// specified buffer size each time. Then verify the data correctly reached the
+    /// disk image by comparing the sha256 sums of the witten data and the image as seen
+    /// on the host.
+    fn virtio_write(sectors_at_once: usize) {
+        use alloc::vec;
+        use sha2::{Digest, Sha256};
+
+        assert!(sectors_at_once > 0);
+
+        let blk = get_blk_device();
+
+        let n_sectors = blk.size() / SECTOR_SIZE;
+        let mut buffer = vec![0u8; sectors_at_once * SECTOR_SIZE];
+
+        let mut hasher = Sha256::new();
+        let mut sector = 0usize;
+
+        let mut gen = (0u64..).flat_map(|x| x.to_le_bytes());
+
+        while sector < n_sectors {
+            if n_sectors - sector < buffer.len() {
+                buffer.truncate(SECTOR_SIZE);
+            }
+
+            buffer.fill_with(|| gen.next().unwrap());
+
+            blk.write_blocks(sector, &buffer).unwrap();
+            hasher.update(&buffer);
+            sector += buffer.len() / SECTOR_SIZE;
+        }
+
+        let hash = hasher.finalize();
+        let expected_hash = get_image_hash_from_host().unwrap();
+
+        assert_eq!(expected_hash, *hash);
+    }
+}
