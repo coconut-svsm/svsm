2c773361a81acd67ca84aa7ad438d20c186837d5
Oliver Steffen <osteffen@redhat.com>
2024-11-28 12:39:40 +0100

    kernel: Add in-svsm tests for VirtioBlkDriver
    Add simple write+readback tests for the VirtioBlkDriver.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- kernel/src/block/virtio_blk.rs
+++ kernel/src/block/virtio_blk.rs
@@ -52,3 +52,63 @@ impl BlockDriver for VirtIOBlkDriver {
         self.0.device.lock().capacity() as usize * SECTOR_SIZE
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{address::PhysAddr, fw_cfg::FwCfg, platform::SVSM_PLATFORM};
+    extern crate alloc;
+    use alloc::vec::Vec;
+    use zerocopy::IntoBytes;
+
+    use super::*;
+
+    /// Find the first virtio-blk device in the hardware-info list
+    fn get_blk_device() -> VirtIOBlkDriver {
+        let cfg = FwCfg::new(SVSM_PLATFORM.get_io_port());
+
+        let dev = cfg
+            .get_virtio_mmio_addresses()
+            .unwrap_or_default()
+            .iter()
+            .find_map(|a| VirtIOBlkDriver::new(PhysAddr::from(*a)).ok())
+            .expect("No virtio-blk device found");
+
+        dev
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    pub fn test_virtio_blk_512() {
+        let drv = get_blk_device();
+        readback_test(&drv, 512);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    pub fn test_virtio_blk_4096() {
+        let drv = get_blk_device();
+        readback_test(&drv, 4096);
+    }
+
+    fn readback_test(blk: &VirtIOBlkDriver, block_size: usize) {
+        for l in [[0xaa, 0x55], [0x55, 0xaa]] {
+            let mut buf: Vec<u8> = core::iter::repeat(l).flatten().take(block_size).collect();
+            let blocks = blk.size() / block_size;
+
+            for i in 0..blocks {
+                buf[0..size_of::<usize>()].copy_from_slice(usize::as_bytes(&i));
+                blk.write_blocks(i * (block_size / SECTOR_SIZE), &buf)
+                    .unwrap();
+            }
+
+            let mut rbuf: Vec<u8> = alloc::vec![0; block_size];
+
+            for i in 0..blocks {
+                buf[0..size_of::<usize>()].copy_from_slice(usize::as_bytes(&i));
+                blk.read_blocks(i * (block_size / SECTOR_SIZE), &mut rbuf)
+                    .unwrap();
+                assert!(rbuf == buf, "Error in block {i}: {rbuf:x?} != {buf:x?}");
+            }
+        }
+    }
+}
