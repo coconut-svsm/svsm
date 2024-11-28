7b922f3963d3f0a755dedc494f2214e1a04566b0
Oliver Steffen <osteffen@redhat.com>
2024-11-28 12:39:40 +0100

    kernel: Add in-svsm tests for VirtioBlkDriver
    Add simple write+readback tests for the VirtioBlkDriver.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- kernel/src/block/virtio_blk.rs
+++ kernel/src/block/virtio_blk.rs
@@ -49,3 +49,57 @@ impl BlockDriver for VirtIOBlkDriver {
         self.0.device.lock().capacity() as usize * SECTOR_SIZE
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::address::PhysAddr;
+    extern crate alloc;
+    use alloc::vec::Vec;
+    use zerocopy::IntoBytes;
+
+    use super::*;
+
+    static MMIO_BASE: u64 = 0xfef03000; // Hard-coded in Qemu
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    pub fn test_virtio_blk_512() {
+        let drv = VirtIOBlkDriver::new(PhysAddr::from(MMIO_BASE));
+        readback_test(&drv, 512);
+    }
+
+    #[test]
+    #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
+    pub fn test_virtio_blk_4096() {
+        let drv = VirtIOBlkDriver::new(PhysAddr::from(MMIO_BASE));
+        readback_test(&drv, 4096);
+    }
+
+    // #[test]
+    // pub fn test_virtio_blk_8192() {
+    //     let drv = VirtIOBlkDriver::new(PhysAddr::from(MMIO_BASE));
+    //     readback_test(&drv, 8192);
+    // }
+
+    fn readback_test(blk: &VirtIOBlkDriver, block_size: usize) {
+        for l in [[0xaa, 0x55], [0x55, 0xaa]] {
+            let mut buf: Vec<u8> = core::iter::repeat(l).flatten().take(block_size).collect();
+            let blocks = blk.size() / block_size;
+
+            for i in 0..blocks {
+                buf[0..size_of::<usize>()].copy_from_slice(usize::as_bytes(&i));
+                blk.write_blocks(i * (block_size / SECTOR_SIZE), &buf)
+                    .unwrap();
+            }
+
+            let mut rbuf: Vec<u8> = alloc::vec![0; block_size];
+
+            for i in 0..blocks {
+                buf[0..size_of::<usize>()].copy_from_slice(usize::as_bytes(&i));
+                blk.read_blocks(i * (block_size / SECTOR_SIZE), &mut rbuf)
+                    .unwrap();
+                assert!(rbuf == buf, "Error in block {i}: {rbuf:x?} != {buf:x?}");
+            }
+        }
+    }
+}
