a3f6279848f73a99cb4590d37b4facb0d1032f31
Joerg Roedel <jroedel@suse.de>
2024-11-29 16:25:54 +0100

    syscall: Add definitions for OPEN syscall
    Add the system call number and flags defintions for the OPEN system
    call.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- syscall/src/def.rs
+++ syscall/src/def.rs
@@ -4,6 +4,8 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use bitflags::bitflags;
+
 // Syscall classes
 const CLASS0: u64 = 0;
 const CLASS1: u64 = 1 << 32;
@@ -14,6 +16,7 @@ pub const SYS_EXEC: u64 = CLASS0 + 4;
 pub const SYS_CLOSE: u64 = CLASS0 + 10;
 
 // Syscall number in class1
+pub const SYS_OPEN: u64 = CLASS1;
 pub const SYS_OPENDIR: u64 = CLASS1 + 4;
 pub const SYS_READDIR: u64 = CLASS1 + 5;
 
@@ -28,6 +31,34 @@ pub const F_NAME_SIZE: usize = 256;
 pub enum FileType {
     File,
     Directory,
+}
+
+//
+// Mode flags for Open system call
+//
+bitflags! {
+    #[derive(Debug, Copy, Clone, Default)]
+    pub struct FileModes: usize {
+        /// Open file for reading
+        const READ = 1 << 0;
+        /// Open file for writing
+        const WRITE = 1 << 1;
+        /// Place file pointer at EOF
+        const APPEND = 1 << 2;
+        /// Truncate file to zero
+        const TRUNC = 1 << 3;
+    }
+}
+
+//
+// File flags for Open system call
+//
+bitflags! {
+    #[derive(Debug, Copy, Clone, Default)]
+    pub struct FileFlags: usize {
+        /// Create file if it does not exist
+        const CREATE = 1 << 0;
+    }
 }
 
 #[repr(C)]
