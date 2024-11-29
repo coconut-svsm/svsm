e3df2eebfd9b01901513abb2175e2a794ae0362f
Joerg Roedel <jroedel@suse.de>
2024-11-29 10:31:29 +0100

    kernel/fs: Add Buffer trait
    The new Buffer trait is an absract interface to buffers for
    reading/writing file data to/from. One implementation will be built
    around user-space buffers.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- kernel/src/fs/mod.rs
+++ kernel/src/fs/mod.rs
@@ -5,12 +5,14 @@
 // Author: Joerg Roedel <jroedel@suse.de>
 
 mod api;
+mod buffer;
 mod filesystem;
 mod init;
 mod obj;
 mod ramfs;
 
 pub use api::*;
+pub use buffer::*;
 pub use filesystem::*;
 pub use init::populate_ram_fs;
 pub use obj::FsObj;
