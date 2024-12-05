b9c305584e26b419c37d031ae4ad9b1cc261c4fb
Joerg Roedel <jroedel@suse.de>
2024-12-05 12:04:37 +0100

    syscall: Add console writing abstractions
    Create a static FSObjHandle which is used to write to the console and
    a function to perform the writes.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- syscall/src/lib.rs
+++ syscall/src/lib.rs
@@ -8,11 +8,13 @@
 mod call;
 mod class0;
 mod class1;
+mod console;
 mod def;
 mod obj;
 
 pub use call::SysCallError;
 pub use class0::*;
 pub use class1::*;
+pub use console::*;
 pub use def::*;
 pub use obj::*;
