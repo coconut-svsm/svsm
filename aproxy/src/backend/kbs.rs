7cf50215f3ee2ef96b9ff3ee47f31e3d5f0c9560
Tyler Fanelli <tfanelli@redhat.com>
2024-12-15 01:13:14 -0500

    aproxy: Complete attestation with KBS protocol
    With serialized TEE evidence and key, the proxy can now transfer these
    to the attestation server for evaluation.
    
    KBS will first transfer the evidence to the /attest endpoint for
    evaluation. Upon successful attestation, the client can then retrieve
    resources from the server. The proxy attempts to retrieve a resource
    identified as "svsm_secret". If all is successful, an attestation
    response indicating success and containing the encrypted secret is
    written back to SVSM for decryption.
    
    NOTE: The proxy hard-codes the fetchable resource value as
    "svsm_secret". KBS servers will likely need to configure their RVPS to
    name this resource accordingly.
    
    Co-developed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- aproxy/src/backend/kbs.rs
+++ aproxy/src/backend/kbs.rs
@@ -7,7 +7,8 @@
 
 use super::*;
 use anyhow::Context;
-use kbs_types::{Challenge, Request, Tee};
+use kbs_types::{Attestation, Challenge, Request, Response, Tee, TeePubKey};
+use reqwest::StatusCode;
 use serde_json::Value;
 
 #[derive(Clone, Copy, Debug, Default)]
@@ -67,5 +68,82 @@ impl AttestationProtocol for KbsProtocol {
         };
 
         Ok(resp)
+    }
+
+    /// With the serialized TEE evidence and key, complete the attestation. Serialize the evidence
+    /// and send it to the /attest endpoint of the KBS server. Upon a successful attestation, fetch
+    /// a secret (identified as "svsm_secret"). If able to successfully fetch the secret, return a
+    /// successful AttestationResponse with the secret included.
+    fn attestation(
+        &self,
+        http: &HttpClient,
+        request: AttestationRequest,
+    ) -> anyhow::Result<AttestationResponse> {
+        // Create a KBS attestation object from the TEE evidence and key.
+        let attestation = Attestation {
+            tee_pubkey: match request.key {
+                AttestationKey::EC {
+                    crv,
+                    x_b64url,
+                    y_b64url,
+                } => TeePubKey::EC {
+                    crv,
+                    alg: "EC".to_string(),
+                    x: x_b64url,
+                    y: y_b64url,
+                },
+            },
+            tee_evidence: Value::String(request.evidence),
+        };
+
+        // Attest TEE evidence at KBS /attest endpoint.
+        let http_resp = http
+            .cli
+            .post(format!("{}/kbs/v0/attest", http.url))
+            .json(&attestation)
+            .send()
+            .context("unable to POST to KBS /attest endpoint")?;
+
+        // The JSON response from the /attest endpoint is basically ignored here. Instead, we check
+        // the HTTP status to indicate successful attestation.
+        //
+        // FIXME
+        if http_resp.status() != StatusCode::OK {
+            return Ok(AttestationResponse {
+                success: false,
+                secret: None,
+                pub_key: None,
+            });
+        }
+
+        // Successful attestation. Fetch the secret (which should be stored as "svsm_secret" within
+        // the KBS's RVPS.
+        let http_resp = http
+            .cli
+            .post(format!("{}/kbs/v0/svsm_secret", http.url))
+            .send()
+            .context("unable to POST to KBS /attest endpoint")?;
+
+        // Unsuccessful attempt at retrieving secret.
+        if http_resp.status() != StatusCode::OK {
+            return Ok(AttestationResponse {
+                success: false,
+                secret: None,
+                pub_key: None,
+            });
+        }
+
+        let text = http_resp
+            .text()
+            .context("unable to read KBS /resource response")?;
+
+        let resp: Response = serde_json::from_str(&text)
+            .context("unable to convert KBS /resource response to KBS Response object")?;
+
+        Ok(AttestationResponse {
+            success: true,
+            secret: Some(resp.ciphertext),
+            pub_key: Some(resp.encrypted_key),
+        })
     }
 }
