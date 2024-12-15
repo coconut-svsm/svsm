facc08bae1bd8f56a0081b2172a1288e6032b397
Tyler Fanelli <tfanelli@redhat.com>
2024-12-15 00:18:50 -0500

    attest: Hash negotiation params, write evidence
    Given the negotiation parameters from the proxy, SVSM can now hash the
    parameters and embed them in the attestation evidence. It can also read
    the negotiation key specified and create key accordingly.
    
    As SEV-SNP is the only supported TEE architecture at the moment, there's
    also a few changes required to serialize a SEV-SNP attestation report
    into bytes.
    
    Co-developed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- kernel/src/attest.rs
+++ kernel/src/attest.rs
@@ -9,13 +9,24 @@ extern crate alloc;
 
 use crate::{
     error::SvsmError,
+    greq::{
+        pld_report::{SnpReportRequest, SnpReportResponse},
+        services::get_regular_report,
+    },
     io::{Read, Write, DEFAULT_IO_DRIVER},
     serial::SerialPort,
 };
 use alloc::{string::ToString, vec, vec::Vec};
+use base64::prelude::*;
+use core::fmt;
 use kbs_types::Tee;
 use libaproxy::*;
+use p384::{ecdh, NistP384};
+use rand_chacha::{rand_core::SeedableRng, ChaChaRng};
+use rdrand::RdSeed;
 use serde::Serialize;
+use sha2::{Digest, Sha384, Sha512};
+use zerocopy::{FromBytes, IntoBytes};
 
 /// The attestation driver that communicates with the proxy via some communication channel (serial
 /// port, virtio-vsock, etc...).
@@ -46,9 +57,9 @@ impl TryFrom<Tee> for AttestationDriver<'_> {
 impl AttestationDriver<'_> {
     /// Attest SVSM's launch state by communicating with the attestation proxy.
     pub fn attest(&mut self) -> Result<Vec<u8>, SvsmError> {
-        let _negotiation = self.negotiation()?;
+        let negotiation = self.negotiation()?;
 
-        todo!();
+        Ok(self.attestation(negotiation)?)
     }
 
     /// Send a negotiation request to the proxy. Proxy should reply with Negotiation parameters
@@ -64,6 +75,27 @@ impl AttestationDriver<'_> {
         let payload = self.read()?;
 
         serde_json::from_slice(&payload).or(Err(AttestationError::NegotiationDeserialize))
+    }
+
+    /// Send an attestation request to the proxy. Proxy should reply with attestation response
+    /// containing the status (success/fail) and an optional secret returned from the server upon
+    /// successful attestation.
+    fn attestation(
+        &mut self,
+        negotiation: NegotiationResponse,
+    ) -> Result<Vec<u8>, AttestationError> {
+        // Generate TEE key and evidence for serialization to proxy.
+        let key = self.tee_key_generate(&negotiation)?;
+        let evidence = self.evidence(negotiation, &key)?;
+
+        let request = AttestationRequest {
+            evidence: BASE64_URL_SAFE.encode(evidence),
+            key: AttestationKey::try_from(&key)?,
+        };
+
+        self.write(request)?;
+
+        todo!();
     }
 
     /// Read attestation data from the serial port.
@@ -100,19 +132,185 @@ impl AttestationDriver<'_> {
 
         Ok(())
     }
+
+    /// Generate the TEE attestation key.
+    fn tee_key_generate(
+        &self,
+        negotiation: &NegotiationResponse,
+    ) -> Result<TeeKey, AttestationError> {
+        let key = match negotiation.key_type {
+            NegotiationKey::Ecdh384Sha256Aes128 => TeeKey::ec(384)?,
+        };
+
+        Ok(key)
+    }
+
+    /// Hash negotiation parameters and fetch TEE evidence.
+    fn evidence(
+        &self,
+        negotiation: NegotiationResponse,
+        key: &TeeKey,
+    ) -> Result<Vec<u8>, AttestationError> {
+        let mut hash = match negotiation.hash {
+            NegotiationHash::SHA384 => self.hash(&negotiation, key, Sha384::new())?,
+            NegotiationHash::SHA512 => self.hash(&negotiation, key, Sha512::new())?,
+        };
+
+        let evidence = match self.tee {
+            Tee::Snp => {
+                // SEV-SNP REPORT_DATA is 64 bytes in size. If a SHA384 was selected in the
+                // negotiation parameters, that array is 48 bytes in size and must be padded.
+                hash.resize(64, 0);
+
+                let mut user_data = [0u8; 64];
+                user_data.copy_from_slice(&hash);
+
+                let request = SnpReportRequest {
+                    user_data,
+                    vmpl: 0,
+                    flags: 1, // Sign with VCEK.
+                    rsvd: [0u8; 24],
+                };
+
+                let mut buf = request.as_bytes().to_vec();
+                // The buffer currently contains the the SnpReportRequest structure. However, SVSM
+                // will fill this buffer in with the SnpReportResponse when fetching the report.
+                // Ensure the array is large enough to contain the response (which is much larger
+                // than the request, as it contains the attestation report).
+                buf.resize(2048, 0);
+
+                let bytes = {
+                    let len =
+                        get_regular_report(&mut buf).or(Err(AttestationError::SnpGetReport))?;
+
+                    // We have the length of the response. The rest of the response is unused.
+                    // Parse the SnpReportResponse from the slice of the buf containing the
+                    // response (that is, &buf[0..len]).
+                    let resp = SnpReportResponse::ref_from_bytes(&buf[..len])
+                        .or(Err(AttestationError::SnpGetReport))?;
+
+                    // Get the attestation report as bytes for serialization in the
+                    // AttestationRequest.
+                    resp.report().as_bytes().to_vec()
+                };
+
+                bytes
+            }
+            // We check for supported TEE architectures in the AttestationDriver's constructor.
+            _ => unreachable!(),
+        };
+
+        Ok(evidence)
+    }
+
+    /// Hash the negotiation parameters from the attestation server for inclusion in the
+    /// attestation evidence.
+    fn hash(
+        &self,
+        n: &NegotiationResponse,
+        key: &TeeKey,
+        mut sha: impl Digest,
+    ) -> Result<Vec<u8>, AttestationError> {
+        for p in &n.params {
+            match p {
+                NegotiationParam::Base64StdBytes(s) => {
+                    let decoded = BASE64_STANDARD
+                        .decode(s)
+                        .or(Err(AttestationError::NegotiationParamDecode))?;
+
+                    sha.update(decoded);
+                }
+                #[allow(irrefutable_let_patterns)]
+                NegotiationParam::EcPublicKeySec1Bytes => {
+                    if let TeeKey::Ecdh384Sha256Aes128(ec) = key {
+                        sha.update(ec.public_key().to_sec1_bytes());
+                    } else {
+                        return Err(AttestationError::ProxyRead);
+                    }
+                }
+            }
+        }
+
+        Ok(sha.finalize().to_vec())
+    }
+}
+
+/// TEE key used to decrypt secrets sent from the attestation server.
+pub enum TeeKey {
+    Ecdh384Sha256Aes128(ecdh::EphemeralSecret),
+}
+
+impl TeeKey {
+    /// Generate an Elliptic Curve key as the TEE key.
+    fn ec(curve: usize) -> Result<Self, AttestationError> {
+        let mut rdseed = RdSeed::new().or(Err(AttestationError::TeeKeyGenerate))?;
+        let mut rng = ChaChaRng::from_rng(&mut rdseed).or(Err(AttestationError::TeeKeyGenerate))?;
+
+        let key = match curve {
+            384 => ecdh::EphemeralSecret::random(&mut rng),
+            _ => unreachable!(),
+        };
+
+        Ok(Self::Ecdh384Sha256Aes128(key))
+    }
+}
+
+impl fmt::Debug for TeeKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        match self {
+            Self::Ecdh384Sha256Aes128(_) => write!(f, "EC384"),
+        }
+    }
+}
+
+impl TryFrom<&TeeKey> for AttestationKey {
+    type Error = AttestationError;
+
+    fn try_from(key: &TeeKey) -> Result<AttestationKey, Self::Error> {
+        match key {
+            TeeKey::Ecdh384Sha256Aes128(k) => {
+                let jwk = k.public_key().to_jwk();
+                let crv = jwk.crv().to_string();
+                let epoint = jwk
+                    .to_encoded_point::<NistP384>()
+                    .or(Err(AttestationError::TeeKeyEncode))?;
+
+                let x = epoint.x().ok_or(AttestationError::TeeKeyEncode)?;
+                let y = epoint.y().ok_or(AttestationError::TeeKeyEncode)?;
+
+                Ok(AttestationKey::EC {
+                    crv,
+                    x_b64url: BASE64_URL_SAFE.encode(x),
+                    y_b64url: BASE64_URL_SAFE.encode(y),
+                })
+            }
+        }
+    }
 }
 
 /// Possible errors when attesting TEE evidence.
 #[derive(Clone, Copy, Debug)]
 pub enum AttestationError {
+    /// Error deserializing the attestation response from JSON bytes.
+    AttestationDeserialize,
+    /// Unsuccessful attestation.
+    Failed,
     /// Error deserializing the negotiation response from JSON bytes.
     NegotiationDeserialize,
+    /// Error decoding a negotiation parameter.
+    NegotiationParamDecode,
     /// Error serializing the negotiation request to JSON bytes.
     NegotiationSerialize,
     /// Error reading from the attestation proxy transport channel.
     ProxyRead,
     /// Error writing over the attestation proxy transport channel.
     ProxyWrite,
+    /// Error fetching the SEV-SNP attestation report.
+    SnpGetReport,
+    /// Error encoding the TEE public key to JSON.
+    TeeKeyEncode,
+    /// Error generating the TEE key.
+    TeeKeyGenerate,
     /// Unsupported TEE architecture.
     UnsupportedTee,
 }
