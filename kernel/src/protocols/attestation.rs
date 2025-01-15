a8a387a9961e82c3d075e641dd11a5a39d749a28
Geoffrey Ndu <gtn@hpe.com>
2025-01-15 15:09:03 +0000

    Rewrite guest page checks and mapping
    Rewrite guest page checks to follow spec extactly and include guest buffer
    sizes.
    
    Signed-off-by: Geoffrey Ndu <gtn@hpe.com>

--- kernel/src/protocols/attestation.rs
+++ kernel/src/protocols/attestation.rs
@@ -12,7 +12,6 @@ use crate::protocols::{errors::SvsmReqError, RequestParams};
 use crate::{
     address::{Address, PhysAddr},
     mm::{valid_phys_address, PerCPUPageMappingGuard},
-    types::PAGE_SIZE,
 };
 #[cfg(all(feature = "vtpm", not(test)))]
 use crate::{
@@ -30,6 +29,7 @@ use core::{mem::size_of, slice::from_raw_parts};
 use sha2::{Digest, Sha512};
 const SVSM_ATTEST_SERVICES: u32 = 0;
 const SVSM_ATTEST_SINGLE_SERVICE: u32 = 1;
+const ATTEST_SINGLE_SERVICE_OP_SIZE: usize = size_of::<AttestSingleServiceOp>();
 
 #[cfg(all(feature = "vtpm", not(test)))]
 const SVSM_ATTEST_VTPM_GUID: u128 = u128::from_le_bytes([
@@ -78,6 +78,7 @@ impl AttestSingleServiceOp {
 
         Ok(ops)
     }
+
     /// Checks if reserved fields are all set to zero
     pub fn is_reserved_clear(&self) -> bool {
         self.reserved_1.iter().all(|&x| x == 0)
@@ -90,68 +91,74 @@ impl AttestSingleServiceOp {
     /// Returns the nonce
     pub fn get_nonce(&self) -> Result<Vec<u8>, SvsmReqError> {
         let (gpa, size) = self.get_nonce_gpa_and_size()?;
-        // get_nonce_gpa_and_size() already validated that gpa is page
-        // aligned, valid  and does not cross page boundary.
+
+        // Check that the nonce size is not greater than 64 bytes.
+        // Nonce size is 64 bytes per Table 21 of
+        // "SEV Secure Nested Paging Firmware ABI Specification, Revision 1.56"
+        if size > 64 {
+            return Err(SvsmReqError::invalid_parameter());
+        }
+
+        // get_nonce_gpa_and_size() already validated that gpa is valid and does not cross page
+        // boundary. So we can safely map in the nonce.
         let start = gpa.page_align();
         let offset = gpa.page_offset();
 
         let guard = PerCPUPageMappingGuard::create_4k(start)?;
         let vaddr = guard.virt_addr() + offset;
 
-        // Check that the nonce length is not greater than 4k as we are going to map in only one
-        // page. Nonce size is 64 bytes per Table 21 of
-        // "SEV Secure Nested Paging Firmware ABI Specification, Revision 1.56"
-        if size > PAGE_SIZE {
-            return Err(SvsmReqError::invalid_parameter());
-        }
-
         // SAFETY: vaddr points to a new mapped page region. And get_nonce_gpa_and_size() already
-        // validated that gpa is page aligned, valid  and does not cross page boundaries. We also
-        // checked earlier that size is not greater than PAGE_SIZE, so we can safely read the nonce.
+        // validated that gpa is valid  and that the size of the nonce won't make us cross page
+        // boundaries.
+        // So we can safely read the nonce.
         let buffer = unsafe { from_raw_parts(vaddr.as_mut_ptr::<u8>(), size) };
         let nonce = buffer.to_vec();
 
         Ok(nonce)
     }
 
-    /// Returns the nonce GPA and size
-    /// Checks if gpa is page aligned, valid  and does not cross page boundary.
+    /// Returns the nonce buffer gpa and size
+    /// Checks if gpa is valid and does not cross page boundary.
+    /// Nonce not required to be page aligned.
     pub fn get_nonce_gpa_and_size(&self) -> Result<(PhysAddr, usize), SvsmReqError> {
         let gpa = PhysAddr::from(self.nonce_gpa);
-        if !gpa.is_aligned(8) || !valid_phys_address(gpa) || gpa.crosses_page(8) {
+        // Won't fail on amd64 as usize > u16 always
+        let size = self.nonce_size.into();
+        if !valid_phys_address(gpa) || gpa.crosses_page(size) {
             return Err(SvsmReqError::invalid_parameter());
         }
-
-        // Won't panic on amd64 as usize > u32 always
-        let size = self.nonce_size.into();
 
         Ok((gpa, size))
     }
 
-    /// Returns the manifest GPA and size
-    /// Checks if gpa is page aligned, valid  and does not cross page boundary.
+    /// Returns the manifest buffer gpa and size
+    /// Checks if gpa is page aligned and valid.
+    /// Manifest buffer size can be greater than 4k, so it can cross page boundary.
     pub fn get_manifest_gpa_and_size(&self) -> Result<(PhysAddr, usize), SvsmReqError> {
         let gpa = PhysAddr::from(self.manifest_gpa);
-        if !gpa.is_aligned(8) || !valid_phys_address(gpa) || gpa.crosses_page(8) {
+        // Won't fail on amd64 as usize > u32 always
+        // TODO return protocol specific error code
+        let size =
+            usize::try_from(self.manifest_size).map_err(|_| SvsmReqError::invalid_parameter())?;
+        if !gpa.is_page_aligned() || !valid_phys_address(gpa) {
             return Err(SvsmReqError::invalid_parameter());
         }
-
-        // Won't panic on amd64 as usize > u32 always
-        let size = self.nonce_size.into();
 
         Ok((gpa, size))
     }
 
-    /// Returns the guid
-    /// Checks if gpa is page aligned, valid  and does not cross page boundary
+    /// Returns the report buffer gpa and size
+    /// Checks if gpa is page aligned and valid.
+    /// Report buffer size can be greater than 4k, so it can cross page boundary.
     pub fn get_report_gpa_and_size(&self) -> Result<(PhysAddr, usize), SvsmReqError> {
         let gpa = PhysAddr::from(self.report_gpa);
-        if !gpa.is_aligned(8) || !valid_phys_address(gpa) || gpa.crosses_page(8) {
+        // Won't fail on amd64 as usize > u32 always
+        //TODO return protocol specific error code
+        let size =
+            usize::try_from(self.report_size).map_err(|_| SvsmReqError::invalid_parameter())?;
+        if !gpa.is_page_aligned() || !valid_phys_address(gpa) {
             return Err(SvsmReqError::invalid_parameter());
         }
-
-        // Won't panic on amd64 as usize > u32 always
-        let size = self.nonce_size.into();
 
         Ok((gpa, size))
     }
@@ -161,9 +168,11 @@ impl AttestSingleServiceOp {
     }
 
     fn is_manifest_version_valid(&self) -> bool {
-        //Currently only manifest version 0 is supported
+        // Currently only manifest version 0 is supported
         self.manifest_ver == 0
     }
+
+    /// Returns the guid
     pub fn get_guid(&self) -> u128 {
         u128::from_le_bytes(self.guid)
     }
@@ -171,7 +180,7 @@ impl AttestSingleServiceOp {
 
 #[cfg(all(feature = "vtpm", not(test)))]
 fn get_attestation_report(nonce: &[u8]) -> Result<Vec<u8>, SvsmReqError> {
-    //Construct attestation request message to send to SNP
+    // Construct attestation request message to send to SNP
     let mut report_req = Vec::<u8>::with_capacity(size_of::<SnpReportResponse>());
     let mut buf = Vec::<u8>::with_capacity(USER_DATA_SIZE);
     if nonce.len() > USER_DATA_SIZE {
@@ -221,67 +230,75 @@ fn attest_single_vtpm(
     // Get attestation report from PSP with Sha512(nonce||manifest) as REPORT_DATA.
     let report = get_attestation_report(hash.as_slice())?;
 
-    // Get attestation report buffer's gPA from call's Attest Single Service Operation structure
-    let (report_gpa, _) = ops.get_report_gpa_and_size()?;
+    // Get attestation report buffer's gPA from call's Attest Single Service Operation structure.
+    // The buffer is required to be page aligned but can be bigger than 4K so can cross pages.
+    // If it is bigger than 4K, it must be physically contiguous.
+    let (report_gpa, report_size) = ops.get_report_gpa_and_size()?;
     let report_start = report_gpa.page_align();
     let report_offset = report_gpa.page_offset();
+    let report_end = (report_start + report_size).page_align_up();
 
-    let report_guard = PerCPUPageMappingGuard::create_4k(report_start)?;
+    let report_guard = PerCPUPageMappingGuard::create(report_start, report_end, 0)?;
     let report_vaddr = report_guard.virt_addr() + report_offset;
 
-    // Check that the attestation report length is not greater than 4K as we going to map in one
-    // page only below. If it is, return an error as something is wrong with the report or SNP
-    // Per page 32 of "Secure VM Service Module for SEV-SNP Guests 58019 Rev. 1.00",
-    // return 0x8000_1000 i.e. SVSM::UNSUPPORTED_PROTOCOL.
-    // The Attestation report length is 0x5F (95) per  Table 21 of
-    // "SEV Secure Nested Paging Firmware ABI Specification, Revision 1.56"
-    if report.len() > PAGE_SIZE {
+    // Check that the attestation report will fit in the buffer by checking that the length of the
+    // report is less than the size of the buffer. The size of the buffer was used to create the
+    // guard, so can not be tricked into writing outside the buffer.
+    // If the report is larger than the buffer, it is either a malformed report or buffer too small.
+    // In either case, return an error.
+    if report.len() > report_size {
         log::error!("Malformed VTPM service attestation report");
+        // TODO: Return a more protocol specific error code
         return Err(SvsmReqError::unsupported_protocol());
     }
 
-    // SAFETY: report_vaddr points to a new mapped region of size PAGE_SIZE. report_gpa is obtained
-    // from a guest-provided physical address (untrusted), so it needs to be validated that it
-    // belongs to the guest and only the guest. That was done inside get_report_gpa_and_size().
-    // get_report_gpa_and_size() also validated that report_gpa is page aligned and does not cross
-    // a page boundary.
-    // Since we also checked that report.len() is not greater than PAGE_SIZE, we can safely write
-    // the report to guest_report_buffer.
+    // SAFETY: Writing to guest_report_buffer via report_vaddr is safe due to the following checks:
+    // 1. report_vaddr is a newly mapped region based on report_gpa
+    // 2. report_gpa is mapped only after get_report_gpa_and_size() validates that it is
+    //    guest-owned and page-aligned
+    // 3. report_vaddr + report_size is fully within valid memory region created with
+    //    PerCPUPageMappingGuard::create()
+    // 4. report.len() is checked to be within report_size, preventing out-of-bounds writes.
+    //
+    // These validations collectively guarantee safe writes to the buffer.
     let guest_report_buffer =
-        unsafe { from_raw_parts_mut(report_vaddr.as_mut_ptr::<u8>(), PAGE_SIZE) };
+        unsafe { from_raw_parts_mut(report_vaddr.as_mut_ptr::<u8>(), report_size) };
     guest_report_buffer[..report.len()].copy_from_slice(&report);
 
     // Set report size in bytes in r8 register
     params.r8 = report.len() as u64;
 
     // Get manifest buffer's GPA from call's Attest Single Service Operation structure
-    let (manifest_gpa, _) = ops.get_manifest_gpa_and_size()?;
+    // The buffer is required to be page aligned but can be bigger than 4K so can cross pages.
+    // If it is bigger than 4K, it must be physically contiguous.
+    let (manifest_gpa, manifest_size) = ops.get_manifest_gpa_and_size()?;
     let manifest_start = manifest_gpa.page_align();
     let manifest_offset = manifest_gpa.page_offset();
+    let manifest_end = (manifest_start + manifest_size).page_align_up();
 
-    let manifest_guard = PerCPUPageMappingGuard::create_4k(manifest_start)?;
+    let manifest_guard = PerCPUPageMappingGuard::create(manifest_start, manifest_end, 0)?;
     let manifest_vaddr = manifest_guard.virt_addr() + manifest_offset;
 
-    // Check that the length of the manifest is not greater than 4k as we going to map in one page
-    // only. If it is, return an error as something is wrong with the manifest or vTPM
-    // Per page 32 of Secure VM Service Module for SEV-SNP Guests 58019 Rev. 1.00 July 2023
-    // return SVSM::UNSUPPORTED_PROTOCOL (i.e., 0x8000_1000).
-    // Per "TCG EK Credential Profile For TPM Family 2.0; Level 0  Version 2.5 Revision 2"
-    // none of the TCG EK profiles will produce a manifest i.e. TPMT_PUBLIC larger than 4K.
-    if manifest.len() > PAGE_SIZE {
+    // Check that the manifest will fit in the buffer by checking that the length of the manifest
+    // is less than the size of the buffer. The size of the buffer was used to create the guard,
+    // so can not be tricked into writing outside the buffer.
+    // If the manifest is larger than the buffer, it is either a malformed manifest or buffer too
+    // small. In either case, return an error.
+    if manifest.len() > manifest_size {
         log::error!("Malformed VTPM service attestation manifest");
+        // TODO: Return a more protocol specific error code
         return Err(SvsmReqError::unsupported_protocol());
     }
 
-    // SAFETY: manifest_vaddr points to a new mapped region of size PAGE_SIZE. report_gpa is obtained
-    // from a guest-provided physical address (untrusted), so it needs to be validated that it
-    // belongs to the guest and only the guest. That was done inside get_manifest_gpa_and_size().
-    // get_manifest_gpa_and_size() also validated that manifest_gpa is page aligned and does not cross
-    // a page boundary.
-    // Since we also checked that manifest.len() is not greater than PAGE_SIZE, we can safely write
-    // the report to guest_manifest_buffer.
+    // SAFETY: Writing to guest_manifest_buffer via manifest_vaddr is safe due to the following checks:
+    // 1. manifest_vaddr is a newly mapped region based on manifest_gpa
+    // 2. manifest_gpa is mapped only after get_manifest_gpa_and_size() validates that it is
+    //    guest-owned and page-aligned
+    // 3. manifest_vaddr + manifest_size is fully within valid memory region created with
+    //    PerCPUPageMappingGuard::create()
+    // 4. manifest.len() is checked to be within manifest_size, preventing out-of-bounds writes.
     let guest_manifest_buffer =
-        unsafe { from_raw_parts_mut(manifest_vaddr.as_mut_ptr::<u8>(), PAGE_SIZE) };
+        unsafe { from_raw_parts_mut(manifest_vaddr.as_mut_ptr::<u8>(), manifest_size) };
     guest_manifest_buffer[..manifest.len()].copy_from_slice(&manifest);
 
     // Set the manifest size in bytes in rcx register
@@ -302,20 +319,22 @@ fn attest_single_service_handler(params: &mut RequestParams) -> Result<(), SvsmR
     // Get the gpa of Attest Single Service Operation structure
     let gpa = PhysAddr::from(params.rcx);
 
-    if !gpa.is_aligned(8) || !valid_phys_address(gpa) || gpa.crosses_page(8) {
+    if !valid_phys_address(gpa) || gpa.crosses_page(ATTEST_SINGLE_SERVICE_OP_SIZE) {
         return Err(SvsmReqError::invalid_parameter());
     }
 
+    let start = gpa.page_align();
     let offset = gpa.page_offset();
-    let paddr = gpa.page_align();
+    let end = (gpa + ATTEST_SINGLE_SERVICE_OP_SIZE).page_align_up();
 
-    // Map the Attest Single Service Operation structure page
+    // Map the Attest Single Service Operation structure table into a page
     // Per Table 13 of the spec "Secure VM Service Module for SEV-SNP Guests
-    // 58019 Rev. 1.00", we only need the first 0x58 bytes.
-    let guard = PerCPUPageMappingGuard::create_4k(paddr)?;
+    // 58019 Rev. 1.00", we only need  0x58 bytes.
+    // The structure is not required to be page aligned.
+    let guard = PerCPUPageMappingGuard::create(start, end, 0)?;
     let vaddr = guard.virt_addr() + offset;
 
-    // SAFETY: The untrusted GPA from the guest is validated above as a valid address.
+    // SAFETY: The untrusted gpa from the guest is validated above as a valid address.
     // The guard ensures that the page is newly mapped and not controlled by the guest.
     // We only use a portion of the page, less than the full page size.
     let buffer =
