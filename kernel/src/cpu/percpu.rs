coconut-svsm/svsm

commit a448d45051e96d84c4752b6a956cd876034f0ab8

schedule: switch to special stack during context switches
We need to guard against IRQs coming in after switching to the new page tables
and before switching to the new stack.

Signed-off-by: Tom Dohrmann <erbse.13@gmx.de>

kernel/src/cpu/percpu.rs
@@ -25,8 +25,8 @@ use crate::mm::vm::{
     VMReserved, VMR,
 };
 use crate::mm::{
-    virt_to_phys, PageBox, SVSM_PERCPU_BASE, SVSM_PERCPU_CAA_BASE, SVSM_PERCPU_END,
-    SVSM_PERCPU_TEMP_BASE_2M, SVSM_PERCPU_TEMP_BASE_4K, SVSM_PERCPU_TEMP_END_2M,
+    virt_to_phys, PageBox, SVSM_CONTEXT_SWITCH_STACK, SVSM_PERCPU_BASE, SVSM_PERCPU_CAA_BASE,
+    SVSM_PERCPU_END, SVSM_PERCPU_TEMP_BASE_2M, SVSM_PERCPU_TEMP_BASE_4K, SVSM_PERCPU_TEMP_END_2M,
     SVSM_PERCPU_TEMP_END_4K, SVSM_PERCPU_VMSA_BASE, SVSM_SHADOW_STACKS_INIT_TASK,
     SVSM_SHADOW_STACK_ISST_DF_BASE, SVSM_STACKS_INIT_TASK, SVSM_STACK_IST_DF_BASE,
 };
@@ -499,6 +499,11 @@ impl PerCpu {
         Ok(())
     }
 
+    fn allocate_context_switch_stack(&self) -> Result<(), SvsmError> {
+        self.allocate_stack(SVSM_CONTEXT_SWITCH_STACK)?;
+        Ok(())
+    }
+
     fn allocate_ist_stacks(&self) -> Result<(), SvsmError> {
         let double_fault_stack = self.allocate_stack(SVSM_STACK_IST_DF_BASE)?;
         self.ist.double_fault_stack.set(Some(double_fault_stack));
@@ -628,6 +633,9 @@ impl PerCpu {
         if cfg!(feature = "shadow-stacks") {
             self.allocate_init_shadow_stack()?;
         }
+
+        // Allocate per-cpu context switch stack
+        self.allocate_context_switch_stack()?;
 
         // Allocate IST stacks
         self.allocate_ist_stacks()?;

kernel/src/task/schedule.rs
@@ -32,7 +32,7 @@ extern crate alloc;
 
 use super::INITIAL_TASK_ID;
 use super::{Task, TaskListAdapter, TaskPointer, TaskRunListAdapter};
-use crate::address::Address;
+use crate::address::{Address, VirtAddr};
 use crate::cpu::msr::write_msr;
 use crate::cpu::percpu::{irq_nesting_count, this_cpu};
 use crate::cpu::shadow_stack::PL0_SSP;
@@ -41,6 +41,7 @@ use crate::cpu::sse::sse_save_context;
 use crate::cpu::IrqGuard;
 use crate::error::SvsmError;
 use crate::locking::SpinLock;
+use crate::mm::{STACK_TOTAL_SIZE, SVSM_CONTEXT_SWITCH_STACK};
 use alloc::sync::Arc;
 use core::arch::{asm, global_asm};
 use core::cell::OnceCell;
@@ -414,6 +415,9 @@ global_asm!(
         jz      1f
         movq    %rsp, {TASK_RSP_OFFSET}(%rsi)
 
+        // Switch to a stack pointer that's valid in both the old and new page tables.
+        mov ${CONTEXT_SWITCH_STACK}, %rsp
+
     1:
         // Switch to the new task state
         mov     %rdx, %cr3
@@ -445,5 +449,16 @@ global_asm!(
         ret
     "#,
     TASK_RSP_OFFSET = const offset_of!(Task, rsp),
+    CONTEXT_SWITCH_STACK = const CONTEXT_SWITCH_STACK.as_usize(),
     options(att_syntax)
 );
+
+/// The location of a cpu-local stack that's mapped into every set of page
+/// tables for use during context switches.
+///
+/// If an IRQ is raised after switching the page tables but before switching
+/// to the new stack, the CPU will try to access the old stack in the new page
+/// tables. To protect against this, we switch to another stack that's mapped
+/// into both the old and the new set of page tables. That way we always have a
+/// valid stack to handle exceptions on.
+const CONTEXT_SWITCH_STACK: VirtAddr = SVSM_CONTEXT_SWITCH_STACK.const_add(STACK_TOTAL_SIZE);

kernel/src/mm/address_space.rs
@@ -172,8 +172,11 @@ pub const SVSM_STACKS_INIT_TASK: VirtAddr = SVSM_PERCPU_STACKS_BASE;
 pub const SVSM_SHADOW_STACKS_INIT_TASK: VirtAddr =
     SVSM_STACKS_INIT_TASK.const_add(STACK_TOTAL_SIZE);
 
+/// Stack address to use during context switches
+pub const SVSM_CONTEXT_SWITCH_STACK: VirtAddr = SVSM_SHADOW_STACKS_INIT_TASK.const_add(PAGE_SIZE);
+
 ///  IST Stacks base address
-pub const SVSM_STACKS_IST_BASE: VirtAddr = SVSM_SHADOW_STACKS_INIT_TASK.const_add(PAGE_SIZE);
+pub const SVSM_STACKS_IST_BASE: VirtAddr = SVSM_CONTEXT_SWITCH_STACK.const_add(STACK_TOTAL_SIZE);
 
 /// DoubleFault IST stack base address
 pub const SVSM_STACK_IST_DF_BASE: VirtAddr = SVSM_STACKS_IST_BASE;

commit d85f6e82b3b3688127d69d76a08bdebeff776107

task/schedule: Fix context switch stack address
Using stack_base + STACK_TOTAL_SIZE is not the correct way to derive a
VMKernelStack's top-of-stack address because there are unmapped guard
pages on both ends of the stack. Use the output of
VMKernelStack::top_of_stack() instead during context switches.

Fixes: a448d4505 ("schedule: switch to special stack during context switches")
Signed-off-by: Peter Fang <peter.fang@intel.com>

kernel/src/cpu/percpu.rs
@@ -384,6 +384,7 @@ pub struct PerCpu {
 
     init_stack: Cell<Option<VirtAddr>>,
     init_shadow_stack: Cell<Option<VirtAddr>>,
+    context_switch_stack: Cell<Option<VirtAddr>>,
     ist: IstStacks,
 
     /// Stack boundaries of the currently running task.
@@ -419,6 +420,7 @@ impl PerCpu {
             hv_doorbell: Cell::new(None),
             init_stack: Cell::new(None),
             init_shadow_stack: Cell::new(None),
+            context_switch_stack: Cell::new(None),
             ist: IstStacks::new(),
             current_stack: Cell::new(MemoryRegion::new(VirtAddr::null(), 0)),
         }
@@ -624,6 +626,10 @@ impl PerCpu {
         self.init_shadow_stack.get().unwrap()
     }
 
+    pub fn get_top_of_context_switch_stack(&self) -> VirtAddr {
+        self.context_switch_stack.get().unwrap()
+    }
+
     pub fn get_top_of_df_stack(&self) -> VirtAddr {
         self.ist.double_fault_stack.get().unwrap()
     }
@@ -690,7 +696,8 @@ impl PerCpu {
     }
 
     fn allocate_context_switch_stack(&self) -> Result<(), SvsmError> {
-        self.allocate_stack(SVSM_CONTEXT_SWITCH_STACK)?;
+        let cs_stack = Some(self.allocate_stack(SVSM_CONTEXT_SWITCH_STACK)?);
+        self.context_switch_stack.set(cs_stack);
         Ok(())
     }
 

kernel/src/task/schedule.rs
@@ -42,7 +42,7 @@ use crate::cpu::IrqGuard;
 use crate::error::SvsmError;
 use crate::fs::Directory;
 use crate::locking::SpinLock;
-use crate::mm::{STACK_TOTAL_SIZE, SVSM_CONTEXT_SWITCH_SHADOW_STACK, SVSM_CONTEXT_SWITCH_STACK};
+use crate::mm::SVSM_CONTEXT_SWITCH_SHADOW_STACK;
 use crate::platform::SVSM_PLATFORM;
 use alloc::string::String;
 use alloc::sync::Arc;
@@ -338,7 +338,17 @@ unsafe fn task_pointer(taskptr: TaskPointer) -> *const Task {
 #[inline(always)]
 unsafe fn switch_to(prev: *const Task, next: *const Task) {
     unsafe {
-        let cr3: u64 = (*next).page_table.lock().cr3_value().bits() as u64;
+        let cr3 = (*next).page_table.lock().cr3_value().bits() as u64;
+
+        // The location of a cpu-local stack that's mapped into every set of
+        // page tables for use during context switches.
+        //
+        // If an IRQ is raised after switching the page tables but before
+        // switching to the new stack, the CPU will try to access the old stack
+        // in the new page tables. To protect against this, we switch to another
+        // stack that's mapped into both the old and the new set of page tables.
+        // That way we always have a valid stack to handle exceptions on.
+        let tos_cs: u64 = this_cpu().get_top_of_context_switch_stack().into();
 
         // Switch to new task
         asm!(
@@ -347,6 +357,7 @@ unsafe fn switch_to(prev: *const Task, next: *const Task) {
             "#,
             in("rsi") prev as u64,
             in("rdi") next as u64,
+            in("r14") tos_cs,
             in("rdx") cr3,
             options(att_syntax));
     }
@@ -466,7 +477,7 @@ global_asm!(
         movq    %rsp, {TASK_RSP_OFFSET}(%rsi)
 
         // Switch to a stack pointer that's valid in both the old and new page tables.
-        mov ${CONTEXT_SWITCH_STACK}, %rsp
+        mov     %r14, %rsp
 
         .if CFG_SHADOW_STACKS
 	cmpb $0, {IS_CET_SUPPORTED}(%rip)
@@ -528,20 +539,9 @@ global_asm!(
     TASK_RSP_OFFSET = const offset_of!(Task, rsp),
     TASK_SSP_OFFSET = const offset_of!(Task, ssp),
     IS_CET_SUPPORTED = sym IS_CET_SUPPORTED,
-    CONTEXT_SWITCH_STACK = const CONTEXT_SWITCH_STACK.as_usize(),
     CONTEXT_SWITCH_RESTORE_TOKEN = const CONTEXT_SWITCH_RESTORE_TOKEN.as_usize(),
     options(att_syntax)
 );
-
-/// The location of a cpu-local stack that's mapped into every set of page
-/// tables for use during context switches.
-///
-/// If an IRQ is raised after switching the page tables but before switching
-/// to the new stack, the CPU will try to access the old stack in the new page
-/// tables. To protect against this, we switch to another stack that's mapped
-/// into both the old and the new set of page tables. That way we always have a
-/// valid stack to handle exceptions on.
-const CONTEXT_SWITCH_STACK: VirtAddr = SVSM_CONTEXT_SWITCH_STACK.const_add(STACK_TOTAL_SIZE);
 
 /// The location of a cpu-local shadow stack restore token that's mapped into
 /// every set of page tables for use during context switches.