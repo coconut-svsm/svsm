coconut-svsm/svsm

commit 676e7412129119fc837cdf206c2138f17a41d804

task/schedule: Free init stack during first context switch
init_stack is now completely unused by the BSP and stops being used by
any CPU after switching to the idle task in schedule_init(). Free up
these memory pages during context switching. Since a Rust function is
called for this purpose, make sure callee-saved registers are used for
switch_context's input arguments.

Signed-off-by: Peter Fang <peter.fang@intel.com>

kernel/src/cpu/percpu.rs
@@ -688,6 +688,12 @@ impl PerCpu {
         Ok(())
     }
 
+    pub fn free_init_stack(&self) -> Result<(), SvsmError> {
+        let _ = self.vm_range.remove(SVSM_STACKS_INIT_TASK)?;
+        self.init_stack.set(None);
+        Ok(())
+    }
+
     fn allocate_init_shadow_stack(&self) -> Result<(), SvsmError> {
         let init_stack =
             Some(self.allocate_shadow_stack(SVSM_SHADOW_STACKS_INIT_TASK, ShadowStackInit::Init)?);

kernel/src/svsm.rs
@@ -250,9 +250,6 @@ pub extern "C" fn svsm_start(li: &KernelLaunchInfo, vb_addr: usize) {
 
     boot_stack_info();
 
-    let bp = this_cpu().get_top_of_stack();
-    log::info!("BSP Runtime stack starts @ {:#018x}", bp);
-
     platform
         .configure_alternate_injection(launch_info.use_alternate_injection)
         .expect("Alternate injection required but not available");

kernel/src/task/schedule.rs
@@ -355,10 +355,10 @@ unsafe fn switch_to(prev: *const Task, next: *const Task) {
             r#"
             call switch_context
             "#,
-            in("rsi") prev as u64,
-            in("rdi") next as u64,
+            in("r12") prev as u64,
+            in("r13") next as u64,
             in("r14") tos_cs,
-            in("rdx") cr3,
+            in("r15") cr3,
             options(att_syntax));
     }
 }
@@ -438,6 +438,11 @@ pub fn schedule_task(task: TaskPointer) {
     schedule();
 }
 
+#[no_mangle]
+extern "C" fn free_init_stack() {
+    this_cpu().free_init_stack().unwrap();
+}
+
 global_asm!(
     // Make the value of the `shadow-stacks` feature usable in assembly.
     ".set const_false, 0",
@@ -470,25 +475,25 @@ global_asm!(
         pushq   %rsp
 
         // If `prev` is not null...
-        testq   %rsi, %rsi
-        jz      1f
+        testq   %r12, %r12
+        jz      3f
 
         // Save the current stack pointer
-        movq    %rsp, {TASK_RSP_OFFSET}(%rsi)
+        movq    %rsp, {TASK_RSP_OFFSET}(%r12)
 
         // Switch to a stack pointer that's valid in both the old and new page tables.
         mov     %r14, %rsp
 
         .if CFG_SHADOW_STACKS
-	cmpb $0, {IS_CET_SUPPORTED}(%rip)
-        je 1f
+        cmpb    $0, {IS_CET_SUPPORTED}(%rip)
+        je      1f
         // Save the current shadow stack pointer
         rdssp   %rax
-        sub $8, %rax
-        movq    %rax, {TASK_SSP_OFFSET}(%rsi)
+        sub     $8, %rax
+        movq    %rax, {TASK_SSP_OFFSET}(%r12)
         // Switch to a shadow stack that's valid in both page tables and move
         // the "shadow stack restore token" to the old shadow stack.
-        mov ${CONTEXT_SWITCH_RESTORE_TOKEN}, %rax
+        mov     ${CONTEXT_SWITCH_RESTORE_TOKEN}, %rax
         rstorssp (%rax)
         saveprevssp
         .endif
@@ -497,44 +502,50 @@ global_asm!(
         // Switch to the new task state
 
         // Switch to the new task page tables
-        mov     %rdx, %cr3
+        mov     %r15, %cr3
 
         .if CFG_SHADOW_STACKS
-	cmpb $0, {IS_CET_SUPPORTED}(%rip)
-        je 2f
+        cmpb    $0, {IS_CET_SUPPORTED}(%rip)
+        je      2f
         // Switch to the new task shadow stack and move the "shadow stack
         // restore token" back.
-        mov     {TASK_SSP_OFFSET}(%rdi), %rdx
+        mov     {TASK_SSP_OFFSET}(%r13), %rdx
         rstorssp (%rdx)
         saveprevssp
     2:
         .endif
 
         // Switch to the new task stack
-        movq    {TASK_RSP_OFFSET}(%rdi), %rsp
+        movq    {TASK_RSP_OFFSET}(%r13), %rsp
 
         // We've already restored rsp
-        addq        $8, %rsp
+        addq    $8, %rsp
 
         // Restore the task context
-        popq        %r15
-        popq        %r14
-        popq        %r13
-        popq        %r12
-        popq        %r11
-        popq        %r10
-        popq        %r9
-        popq        %r8
-        popq        %rbp
-        popq        %rdi
-        popq        %rsi
-        popq        %rdx
-        popq        %rcx
-        popq        %rbx
-        popq        %rax
+        popq    %r15
+        popq    %r14
+        popq    %r13
+        popq    %r12
+        popq    %r11
+        popq    %r10
+        popq    %r9
+        popq    %r8
+        popq    %rbp
+        popq    %rdi
+        popq    %rsi
+        popq    %rdx
+        popq    %rcx
+        popq    %rbx
+        popq    %rax
         popfq
 
         ret
+
+    3:
+        // Switch to a stack pointer that's valid after init stack is freed.
+        mov     %r14, %rsp
+        call    free_init_stack
+        jmp     1b
     "#,
     TASK_RSP_OFFSET = const offset_of!(Task, rsp),
     TASK_SSP_OFFSET = const offset_of!(Task, ssp),

commit 3fe27e3fa63660b940bc659c392265355f2ba739

kernel/cpu/percpu: Remove init stack
Commit 676e7412129119fc837cdf206c2138f17a41d804 introduced the side
effect of breaking the stack unwinder because init_stack no longer
exists after the first context switch in schedule_init(). This commit
fixes that by completely removing init_stack:

- BSP is not impacted because it never uses init_stack and uses
  bsp_stack instead
- AP now uses context_switch_stack as its initial stack because this
  stack was originally designed to be used only when switching between
  tasks

In both cases, IRQs requiring a stack can safely occur during context
switching because these stacks are accessible after switching to the
first task's page table.

Let the stack unwinder inspect context_switch_stack instead of
init_stack. This stack will always exist as long as its PerCpu is in
use.

Fixes: 676e7412 ("task/schedule: Free init stack during first context switch")
Signed-off-by: Peter Fang <peter.fang@intel.com>

kernel/src/cpu/percpu.rs
@@ -40,7 +40,7 @@ use crate::mm::{
     SVSM_PERCPU_BASE, SVSM_PERCPU_CAA_BASE, SVSM_PERCPU_END, SVSM_PERCPU_TEMP_BASE_2M,
     SVSM_PERCPU_TEMP_BASE_4K, SVSM_PERCPU_TEMP_END_2M, SVSM_PERCPU_TEMP_END_4K,
     SVSM_PERCPU_VMSA_BASE, SVSM_SHADOW_STACKS_INIT_TASK, SVSM_SHADOW_STACK_ISST_DF_BASE,
-    SVSM_STACKS_INIT_TASK, SVSM_STACK_IST_DF_BASE,
+    SVSM_STACK_IST_DF_BASE,
 };
 use crate::platform::{SvsmPlatform, SVSM_PLATFORM};
 use crate::sev::ghcb::{GhcbPage, GHCB};
@@ -382,7 +382,6 @@ pub struct PerCpu {
     /// `#HV` doorbell page for this CPU.
     hv_doorbell: Cell<Option<&'static HVDoorbell>>,
 
-    init_stack: Cell<Option<VirtAddr>>,
     init_shadow_stack: Cell<Option<VirtAddr>>,
     context_switch_stack: Cell<Option<VirtAddr>>,
     ist: IstStacks,
@@ -418,7 +417,6 @@ impl PerCpu {
             ghcb: OnceCell::new(),
             hypercall_pages: RefCell::new(None),
             hv_doorbell: Cell::new(None),
-            init_stack: Cell::new(None),
             init_shadow_stack: Cell::new(None),
             context_switch_stack: Cell::new(None),
             ist: IstStacks::new(),
@@ -618,10 +616,6 @@ impl PerCpu {
         self.hv_doorbell.as_ptr().cast()
     }
 
-    pub fn get_top_of_stack(&self) -> VirtAddr {
-        self.init_stack.get().unwrap()
-    }
-
     pub fn get_top_of_shadow_stack(&self) -> VirtAddr {
         self.init_shadow_stack.get().unwrap()
     }
@@ -680,18 +674,6 @@ impl PerCpu {
         self.vm_range
             .insert_at(base, Arc::new(Mapping::new(shadow_stack)))?;
         Ok(ssp)
-    }
-
-    fn allocate_init_stack(&self) -> Result<(), SvsmError> {
-        let init_stack = Some(self.allocate_stack(SVSM_STACKS_INIT_TASK)?);
-        self.init_stack.set(init_stack);
-        Ok(())
-    }
-
-    pub fn free_init_stack(&self) -> Result<(), SvsmError> {
-        let _ = self.vm_range.remove(SVSM_STACKS_INIT_TASK)?;
-        self.init_stack.set(None);
-        Ok(())
     }
 
     fn allocate_init_shadow_stack(&self) -> Result<(), SvsmError> {
@@ -839,9 +821,6 @@ impl PerCpu {
         // Reserve ranges for temporary mappings
         self.initialize_vm_ranges()?;
 
-        // Allocate per-cpu init stack
-        self.allocate_init_stack()?;
-
         if is_cet_ss_supported() {
             self.allocate_init_shadow_stack()?;
         }
@@ -925,7 +904,13 @@ impl PerCpu {
 
         hyperv::HvInitialVpContext {
             rip: start_rip,
-            rsp: self.get_top_of_stack().into(),
+            // Use the context switch stack as its initial stack. This stack
+            // will later serve as the transitory stack during context
+            // switching. It is accessible after switching to the first task's
+            // page table because it resides in the PerCpu virtual range.
+            //
+            // See switch_to() for details.
+            rsp: self.get_top_of_context_switch_stack().into(),
             rflags: 2,
 
             cs: svsm_code_segment(),

kernel/src/debug/stacktrace.rs
@@ -46,14 +46,14 @@ impl StackUnwinder {
         };
 
         let cpu = this_cpu();
-        let top_of_init_stack = cpu.get_top_of_stack();
-        let top_of_df_stack = cpu.get_top_of_df_stack();
         let current_stack = cpu.get_current_stack();
+        let top_of_cs_stack = cpu.get_top_of_context_switch_stack();
+        let top_of_df_stack = cpu.get_top_of_df_stack();
 
         let stacks: StacksBounds = [
-            MemoryRegion::from_addresses(top_of_init_stack - STACK_SIZE, top_of_init_stack),
-            MemoryRegion::from_addresses(top_of_df_stack - STACK_SIZE, top_of_df_stack),
             current_stack,
+            MemoryRegion::from_addresses(top_of_cs_stack - STACK_SIZE, top_of_cs_stack),
+            MemoryRegion::from_addresses(top_of_df_stack - STACK_SIZE, top_of_df_stack),
         ];
 
         Self::new(VirtAddr::from(rbp), stacks)
@@ -146,7 +146,7 @@ impl StackUnwinder {
         // A new task is launched with RBP = 0, which is pushed onto the stack
         // immediatly and can serve as a marker when the end of the stack has
         // been reached.
-        rbp == VirtAddr::new(0)
+        rbp == VirtAddr::null()
     }
 }
 

kernel/src/mm/address_space.rs
@@ -170,12 +170,8 @@ pub const SVSM_PERCPU_VMSA_BASE: VirtAddr = SVSM_PERCPU_BASE.const_add(4 * SIZE_
 /// Region for PerCPU Stacks
 pub const SVSM_PERCPU_STACKS_BASE: VirtAddr = SVSM_PERCPU_BASE.const_add(SIZE_LEVEL1);
 
-/// Stack address of the per-cpu init task
-pub const SVSM_STACKS_INIT_TASK: VirtAddr = SVSM_PERCPU_STACKS_BASE;
-
 /// Shadow stack address of the per-cpu init task
-pub const SVSM_SHADOW_STACKS_INIT_TASK: VirtAddr =
-    SVSM_STACKS_INIT_TASK.const_add(STACK_TOTAL_SIZE);
+pub const SVSM_SHADOW_STACKS_INIT_TASK: VirtAddr = SVSM_PERCPU_STACKS_BASE;
 
 /// Stack address to use during context switches
 pub const SVSM_CONTEXT_SWITCH_STACK: VirtAddr = SVSM_SHADOW_STACKS_INIT_TASK.const_add(PAGE_SIZE);

kernel/src/svsm.rs
@@ -78,7 +78,15 @@ global_asm!(
 
         .globl  startup_64
     startup_64:
-        /* Setup stack */
+        /*
+         * Setup stack.
+         *
+         * bsp_stack is always mapped across all page tables because it
+         * uses the shared PML4E, making it accessible after switching to
+         * the first task's page table.
+         *
+         * See switch_to() for details.
+         */
         leaq bsp_stack_end(%rip), %rsp
 
         /*

kernel/src/task/schedule.rs
@@ -438,11 +438,6 @@ pub fn schedule_task(task: TaskPointer) {
     schedule();
 }
 
-#[no_mangle]
-extern "C" fn free_init_stack() {
-    this_cpu().free_init_stack().unwrap();
-}
-
 global_asm!(
     // Make the value of the `shadow-stacks` feature usable in assembly.
     ".set const_false, 0",
@@ -476,7 +471,7 @@ global_asm!(
 
         // If `prev` is not null...
         testq   %r12, %r12
-        jz      3f
+        jz      1f
 
         // Save the current stack pointer
         movq    %rsp, {TASK_RSP_OFFSET}(%r12)
@@ -540,12 +535,6 @@ global_asm!(
         popfq
 
         ret
-
-    3:
-        // Switch to a stack pointer that's valid after init stack is freed.
-        mov     %r14, %rsp
-        call    free_init_stack
-        jmp     1b
     "#,
     TASK_RSP_OFFSET = const offset_of!(Task, rsp),
     TASK_SSP_OFFSET = const offset_of!(Task, ssp),