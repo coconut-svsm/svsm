f593022d8ea9ed5c25745386c77560f0fd0171d7
Peter Fang <peter.fang@intel.com>
2025-02-13 03:01:20 -0800

    platform: Add GlobalFeatureFlags capability
    This corresponds to index 2 in the syscall ABI. Currently, only bits
    [2:0] are defined as the platform type.
    
    Signed-off-by: Peter Fang <peter.fang@intel.com>

--- kernel/src/platform/native.rs
+++ kernel/src/platform/native.rs
@@ -22,6 +22,7 @@ use crate::io::{IOPort, DEFAULT_IO_DRIVER};
 use crate::mm::PerCPUPageMappingGuard;
 use crate::types::{PageSize, PAGE_SIZE};
 use crate::utils::MemoryRegion;
+use syscall::GlobalFeatureFlags;
 
 use bootlib::kernel_launch::{ApStartContext, SIPI_STUB_GPA};
 use core::{mem, ptr};
@@ -112,7 +113,8 @@ impl SvsmPlatform for NativePlatform {
     }
 
     fn capabilities(&self) -> Caps {
-        Caps::new(0, 0)
+        let features = GlobalFeatureFlags::PLATFORM_TYPE_NATIVE;
+        Caps::new(0, features)
     }
 
     fn cpuid(&self, eax: u32) -> Option<CpuidResult> {
