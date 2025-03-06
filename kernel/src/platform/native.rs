39ca9c5c0234f32ca80280ccbc818d98e3809593
Joerg Roedel <jroedel@suse.de>
2025-03-06 11:48:03 +0100

    kernel/cpu: Rename cpu/x86/{apic.rs -> x2apic.rs}
    The file only contains X2APIC functions, so rename it to reflect that.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- kernel/src/platform/native.rs
+++ kernel/src/platform/native.rs
@@ -13,7 +13,7 @@ use crate::cpu::control_regs::read_cr3;
 use crate::cpu::cpuid::CpuidResult;
 use crate::cpu::percpu::PerCpu;
 use crate::cpu::smp::create_ap_start_context;
-use crate::cpu::x86::apic::{x2apic_enable, x2apic_eoi, x2apic_icr_write};
+use crate::cpu::x86::x2apic::{x2apic_enable, x2apic_eoi, x2apic_icr_write};
 use crate::error::SvsmError;
 use crate::hyperv;
 use crate::hyperv::{hyperv_setup_hypercalls, hyperv_start_cpu, is_hyperv_hypervisor};
