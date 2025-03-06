c4465b73688709fa927298e2ab6ee15cc7de0e54
Joerg Roedel <jroedel@suse.de>
2025-03-06 13:00:56 +0100

    kernel/cpu/apic: Add X2APIC driver
    Implement a struct X2Apic which implements the new X86Apic trait and
    uses the RawX86Apic implementation.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- kernel/src/cpu/x86/x2apic.rs
+++ kernel/src/cpu/x86/x2apic.rs
@@ -6,7 +6,93 @@
 // Author: Jon Lange <jlange@microsoft.com>
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use super::apic::{ApicAccess, RawX86Apic, X86Apic};
 use crate::cpu::msr::{read_msr, write_msr};
+
+const MSR_X2APIC_BASE: u32 = 0x800;
+
+struct X2ApicAccessor {}
+
+impl ApicAccess for X2ApicAccessor {
+    #[inline(always)]
+    fn apic_write(offset: usize, value: u64) {
+        let msr = MSR_X2APIC_BASE + u32::try_from(offset).unwrap();
+        // SAFETY: Writes to X2APIC MSRs never harms memory safety.
+        unsafe { write_msr(msr, value) };
+    }
+
+    #[inline(always)]
+    fn apic_read(offset: usize) -> u64 {
+        let msr = MSR_X2APIC_BASE + u32::try_from(offset).unwrap();
+        read_msr(msr)
+    }
+}
+
+type RawX2Apic = RawX86Apic<X2ApicAccessor>;
+
+#[derive(Debug, Default)]
+pub struct X2Apic {
+    // Skip enablement via MSR_APIC_BASE
+    skip_msr_enable: bool,
+}
+
+impl X2Apic {
+    /// Creates new instance of [`X2Apic`] which needs to be enabled via
+    /// MSR_APIC_BASE.
+    pub fn new() -> Self {
+        Self {
+            skip_msr_enable: false,
+        }
+    }
+
+    /// Creates new instance of [`X2Apic`] which does not need to be enabled
+    /// via MSR_APIC_BASE.
+    pub fn new_skip_msr_enable() -> Self {
+        Self {
+            skip_msr_enable: true,
+        }
+    }
+}
+
+impl X86Apic for X2Apic {
+    fn enable(&self) {
+        // Enable X2APIC mode.
+        if !self.skip_msr_enable {
+            let apic_base = read_msr(MSR_APIC_BASE);
+            let apic_base_x2_enabled = apic_base | APIC_ENABLE_MASK | APIC_X2_ENABLE_MASK;
+            if apic_base != apic_base_x2_enabled {
+                // SAFETY: enabling X2APIC mode allows accessing APIC's control
+                // registers through MSR accesses, so enabling it doesn't break
+                // memory safety itself.
+                unsafe {
+                    write_msr(MSR_APIC_BASE, apic_base_x2_enabled);
+                }
+            }
+        }
+        // Set SW-enable in SPIV to enable IRQ delivery
+        self.sw_enable();
+    }
+
+    fn eoi(&self) {
+        RawX2Apic::eoi();
+    }
+
+    fn icr_write(&self, icr: u64) {
+        RawX2Apic::icr_write(icr);
+    }
+
+    fn check_isr(&self, vector: usize) -> bool {
+        RawX2Apic::check_isr(vector)
+    }
+
+    fn spiv_write(&self, vector: u8, enable: bool) {
+        RawX2Apic::spiv_write(vector, enable);
+    }
+
+    fn sw_enable(&self) {
+        RawX2Apic::sw_enable();
+    }
+}
 
 /// End-of-Interrupt register MSR offset
 pub const MSR_X2APIC_EOI: u32 = 0x80B;
@@ -32,7 +118,7 @@ const APIC_SPIV_SW_ENABLE_MASK: u64 = 1 << 8;
 ///
 /// A `(u32, u32)` tuple with the MSR offset as the first and the vector
 /// bitmask as the second value.
-pub fn apic_register_bit(vector: usize) -> (u32, u32) {
+fn apic_register_bit(vector: usize) -> (u32, u32) {
     let index: u8 = vector as u8;
     ((index >> 5) as u32, 1 << (index & 0x1F))
 }
