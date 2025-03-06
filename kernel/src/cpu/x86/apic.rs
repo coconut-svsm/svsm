a1cc2306cca9df6ec70741d65fdfa69588b3e4b3
Joerg Roedel <jroedel@suse.de>
2025-03-06 16:02:49 +0100

    kernel/cpu: Introduce struct LApic as the PerCpu APIC interface
    This struct will be the interface installed in PerCpu to access the
    local APIC driver. Internally it supports different backend drivers
    via enum X86ApicDriver.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- kernel/src/cpu/x86/apic.rs
+++ kernel/src/cpu/x86/apic.rs
@@ -4,6 +4,8 @@
 //
 // Author: Joerg Roedel <jroedel@suse.de>
 
+use super::{SnpGhcbApic, X2Apic};
+use core::cell::RefCell;
 use core::marker::PhantomData;
 
 pub trait ApicAccess {
@@ -152,4 +154,133 @@ pub trait X86Apic {
 
     /// Enable the APIC-Software-Enable bit.
     fn sw_enable(&self);
+}
+
+/// Wrapper structure for existing Local APIC drivers. All members (except
+/// `None`) must implement trait [`X86Apic`].
+#[derive(Debug, Default)]
+pub enum X86ApicDriver {
+    #[default]
+    None,
+    X2(X2Apic),
+    Snp(SnpGhcbApic),
+}
+
+impl X86ApicDriver {
+    /// Create a new instance for an X2APIC driver
+    ///
+    /// # Arguments
+    ///
+    /// - `apic` - Instance of struct [`X2Apic`].
+    ///
+    /// # Returns
+    ///
+    /// Instance of [`X86ApicDriver`] for using X2APIC.
+    pub fn new_x2apic(apic: X2Apic) -> Self {
+        Self::X2(apic)
+    }
+
+    pub fn new_snp_apic(apic: SnpGhcbApic) -> Self {
+        Self::Snp(apic)
+    }
+
+    /// Returns a reference to the contained APIC driver. The driver type
+    /// implements the [`X86Apic`] trait. The method panics when no APIC driver
+    /// is set.
+    ///
+    /// # Returns
+    ///
+    /// Reference to an APIC driver struct which implements the [`X86Apic`] trait.
+    #[inline(always)]
+    fn get(&self) -> &dyn X86Apic {
+        match self {
+            X86ApicDriver::None => panic!("APIC driver not set!"),
+            X86ApicDriver::X2(apic) => apic,
+            X86ApicDriver::Snp(apic) => apic,
+        }
+    }
+
+    fn enable(&self) {
+        self.get().enable();
+    }
+
+    #[inline(always)]
+    fn eoi(&self) {
+        self.get().eoi();
+    }
+
+    #[inline(always)]
+    fn post_irq(&self, icr: u64) {
+        self.get().icr_write(icr);
+    }
+
+    #[inline(always)]
+    fn irq_in_service(&self, vector: usize) -> bool {
+        self.get().check_isr(vector)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct LApic {
+    driver: RefCell<X86ApicDriver>,
+}
+
+impl LApic {
+    /// Creates new [`LApic`] instance with no [`X86ApicDriver`] installed.
+    ///
+    /// # Returns
+    ///
+    /// Empty instance of [`LApic`].
+    pub fn new() -> Self {
+        Self {
+            driver: RefCell::new(X86ApicDriver::None),
+        }
+    }
+
+    fn enable(&self) {
+        let apic = self.driver.borrow();
+        (*apic).enable();
+    }
+
+    /// Install new [`X86ApicDriver`] instance. This can be used to install or
+    /// remove an APIC driver.
+    ///
+    /// # Arguments
+    ///
+    /// - `driver` - [`X86ApicDriver`] to install.
+    pub fn set(&self, driver: X86ApicDriver) {
+        self.driver.replace(driver);
+        // Enable APIC only after its driver is successfully installed.
+        self.enable();
+    }
+
+    /// Send and EOI message
+    pub fn eoi(&self) {
+        let apic = self.driver.borrow();
+        (*apic).eoi();
+    }
+
+    /// Post an IRQ via the Interrupt Command Register
+    ///
+    /// # Arguments
+    ///
+    /// - `icr` - Command register value to write
+    pub fn post_irq(&self, icr: u64) {
+        let apic = self.driver.borrow();
+        (*apic).post_irq(icr);
+    }
+
+    /// Check for an IRQ vector in the ISR bitmap
+    ///
+    /// # Arguments
+    ///
+    /// - `vector` - Vector to check in ISR
+    ///
+    /// # Returns
+    ///
+    /// `True` when vector bit is set, `False` otherwise
+    pub fn irq_in_service(&self, vector: usize) -> bool {
+        let apic = self.driver.borrow();
+        (*apic).irq_in_service(vector)
+    }
 }
