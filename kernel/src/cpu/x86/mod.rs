598740b4b32fdcb81e57a324ab54dbd68f6f30c0
Joerg Roedel <jroedel@suse.de>
2025-03-06 16:02:49 +0100

    kernel/cpu: Introduce struct LApic as the PerCpu APIC interface
    This struct will be the interface installed in PerCpu to access the
    local APIC driver. Internally it supports different backend drivers
    via enum X86ApicDriver.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- kernel/src/cpu/x86/mod.rs
+++ kernel/src/cpu/x86/mod.rs
@@ -9,6 +9,6 @@ pub mod smap;
 pub mod snp_apic;
 pub mod x2apic;
 
-pub use apic::{ApicAccess, RawX86Apic, X86Apic};
+pub use apic::{ApicAccess, LApic, RawX86Apic, X86Apic, X86ApicDriver};
 pub use snp_apic::SnpGhcbApic;
 pub use x2apic::X2Apic;
