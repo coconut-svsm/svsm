coconut-svsm/svsm

commit be9c1fbdb6b7bf918bf0766e5cf1edc6d7abdfdd

sev: support CPUID over GHCB
For synthetic (hypervisor-owned) CPUID leaves, perform the CPUID access
via the GHCB instead of trying to look up the result in the CPUID table.

Signed-off-by: Jon Lange <jlange@microsoft.com>

kernel/src/platform/snp.rs
@@ -205,7 +205,14 @@ impl SvsmPlatform for SnpPlatform {
     }
 
     fn cpuid(&self, eax: u32) -> Option<CpuidResult> {
-        cpuid_table(eax)
+        // If this is an architectural CPUID leaf, then extract the result
+        // from the CPUID table.  Otherwise, request the value from the
+        // hypervisor.
+        if (eax >> 28) == 4 {
+            current_ghcb().cpuid(eax).ok()
+        } else {
+            cpuid_table(eax)
+        }
     }
 
     fn setup_guest_host_comm(&mut self, cpu: &PerCpu, is_bsp: bool) {

kernel/src/sev/ghcb.rs
@@ -5,6 +5,7 @@
 // Author: Joerg Roedel <jroedel@suse.de>
 
 use crate::address::{Address, PhysAddr, VirtAddr};
+use crate::cpu::cpuid::CpuidResult;
 use crate::cpu::msr::{write_msr, SEV_GHCB};
 use crate::cpu::percpu::this_cpu;
 use crate::cpu::{flush_tlb_global_sync, IrqGuard, X86GeneralRegs};
@@ -94,6 +95,7 @@ impl From<GhcbError> for SvsmError {
 #[expect(non_camel_case_types, clippy::upper_case_acronyms)]
 enum GHCBExitCode {
     RDTSC = 0x6e,
+    CPUID = 0x72,
     IOIO = 0x7b,
     MSR = 0x7c,
     RDTSCP = 0x87,
@@ -279,6 +281,27 @@ impl GHCB {
 
     ghcb_getter!(get_usage_valid, usage, u32);
     ghcb_setter!(set_usage_valid, usage, u32);
+
+    pub fn cpuid(&self, eax: u32) -> Result<CpuidResult, SvsmError> {
+        self.clear();
+        self.set_rax_valid(eax as u64);
+        self.vmgexit(GHCBExitCode::CPUID, 0, 0)?;
+        Ok(CpuidResult {
+            eax: self.get_rax_valid()? as u32,
+            ebx: self.get_rbx_valid()? as u32,
+            ecx: self.get_rcx_valid()? as u32,
+            edx: self.get_rdx_valid()? as u32,
+        })
+    }
+
+    pub fn cpuid_regs(&self, regs: &mut X86GeneralRegs) -> Result<(), SvsmError> {
+        let result = self.cpuid(regs.rax as u32)?;
+        regs.rax = result.eax as usize;
+        regs.rbx = result.ebx as usize;
+        regs.rcx = result.ecx as usize;
+        regs.rdx = result.edx as usize;
+        Ok(())
+    }
 
     pub fn rdtscp_regs(&self, regs: &mut X86GeneralRegs) -> Result<(), SvsmError> {
         self.clear();

commit 264c4a8103ce21e9caf438aff6511d0226eabfeb

kernel/sev/ghcb: Always supply RCX to cpuid() function
RCX must always be supplied when sending a GHCB CPUID NAE event to the
hypervisor. When RCX is not supplied, the GHCB call will (or should) fail.
The KVM hypervisor will log the following:

  kvm_amd: kvm [15381]: vcpu0, guest rIP: 0x0 vmgexit: exit code 0x72 input is not valid

Update the GHCB cpuid() function to require the CPUID subleaf value.

Fixes: be9c1fbdb6b7 ("sev: support CPUID over GHCB")
Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

kernel/src/cpu/cpuid.rs
@@ -127,8 +127,8 @@ pub fn cpuid_table_raw(eax: u32, ecx: u32, xcr0: u64, xss: u64) -> Option<CpuidR
     None
 }
 
-pub fn cpuid_table(eax: u32) -> Option<CpuidResult> {
-    cpuid_table_raw(eax, 0, 0, 0)
+pub fn cpuid_table(eax: u32, ecx: u32) -> Option<CpuidResult> {
+    cpuid_table_raw(eax, ecx, 0, 0)
 }
 
 pub fn dump_cpuid_table() {

kernel/src/cpu/features.rs
@@ -13,24 +13,24 @@ const X86_FEATURE_UMIP: u32 = 2;
 
 pub fn cpu_has_pge(platform: &dyn SvsmPlatform) -> bool {
     platform
-        .cpuid(0x0000_0001)
+        .cpuid(0x0000_0001, 0)
         .map_or_else(|| false, |c| (c.edx >> X86_FEATURE_PGE) & 1 == 1)
 }
 
 pub fn cpu_has_smep(platform: &dyn SvsmPlatform) -> bool {
     platform
-        .cpuid(0x0000_0007)
+        .cpuid(0x0000_0007, 0)
         .map_or_else(|| false, |c| (c.ebx >> X86_FEATURE_SMEP & 1) == 1)
 }
 
 pub fn cpu_has_smap(platform: &dyn SvsmPlatform) -> bool {
     platform
-        .cpuid(0x0000_0007)
+        .cpuid(0x0000_0007, 0)
         .map_or_else(|| false, |c| (c.ebx >> X86_FEATURE_SMAP & 1) == 1)
 }
 
 pub fn cpu_has_umip(platform: &dyn SvsmPlatform) -> bool {
     platform
-        .cpuid(0x0000_0007)
+        .cpuid(0x0000_0007, 0)
         .map_or_else(|| false, |c| (c.ecx >> X86_FEATURE_UMIP & 1) == 1)
 }

kernel/src/hyperv/hv.rs
@@ -272,7 +272,7 @@ pub static IS_HYPERV: ImmutAfterInitCell<bool> = ImmutAfterInitCell::uninit();
 
 fn is_hyperv_hypervisor() -> bool {
     // Get the hypervisor interface signature.
-    let result = SVSM_PLATFORM.cpuid(0x40000001);
+    let result = SVSM_PLATFORM.cpuid(0x40000001, 0);
     if let Some(cpuid_result) = result {
         cpuid_result.eax == 0x31237648
     } else {

kernel/src/platform/mod.rs
@@ -139,7 +139,7 @@ pub trait SvsmPlatform: Sync {
     ) -> hyperv::HvHypercallOutput;
 
     /// Obtain CPUID using platform-specific tables.
-    fn cpuid(&self, eax: u32) -> Option<CpuidResult>;
+    fn cpuid(&self, eax: u32, ecx: u32) -> Option<CpuidResult>;
 
     /// Write a host-owned MSR.
     /// # Safety

kernel/src/platform/native.rs
@@ -121,8 +121,8 @@ impl SvsmPlatform for NativePlatform {
         unsafe { hyperv::execute_hypercall(input_control, hypercall_pages) }
     }
 
-    fn cpuid(&self, eax: u32) -> Option<CpuidResult> {
-        Some(CpuidResult::get(eax, 0))
+    fn cpuid(&self, eax: u32, ecx: u32) -> Option<CpuidResult> {
+        Some(CpuidResult::get(eax, ecx))
     }
 
     unsafe fn write_host_msr(&self, msr: u32, value: u64) {

kernel/src/platform/snp.rs
@@ -166,7 +166,7 @@ impl SvsmPlatform for SnpPlatform {
     fn get_page_encryption_masks(&self) -> PageEncryptionMasks {
         // Find physical address size.
         let processor_capacity =
-            cpuid_table(0x80000008).expect("Can not get physical address size from CPUID table");
+            cpuid_table(0x80000008, 0).expect("Can not get physical address size from CPUID table");
         if vtom_enabled() {
             let vtom = *VTOM;
             PageEncryptionMasks {
@@ -178,7 +178,7 @@ impl SvsmPlatform for SnpPlatform {
         } else {
             // Find C-bit position.
             let sev_capabilities =
-                cpuid_table(0x8000001f).expect("Can not get C-Bit position from CPUID table");
+                cpuid_table(0x8000001f, 0).expect("Can not get C-Bit position from CPUID table");
             let c_bit = sev_capabilities.ebx & 0x3f;
             PageEncryptionMasks {
                 private_pte_mask: 1 << c_bit,
@@ -193,7 +193,7 @@ impl SvsmPlatform for SnpPlatform {
         // Examine CPUID information to see whether CET is supported by the
         // hypervisor.  If no CPUID information is present, then assume that
         // CET is supported.
-        if let Some(cpuid) = cpuid_table(7) {
+        if let Some(cpuid) = cpuid_table(7, 0) {
             (cpuid.ecx & 0x80) != 0
         } else {
             todo!()
@@ -223,14 +223,14 @@ impl SvsmPlatform for SnpPlatform {
         })
     }
 
-    fn cpuid(&self, eax: u32) -> Option<CpuidResult> {
+    fn cpuid(&self, eax: u32, ecx: u32) -> Option<CpuidResult> {
         // If this is an architectural CPUID leaf, then extract the result
         // from the CPUID table.  Otherwise, request the value from the
         // hypervisor.
         if (eax >> 28) == 4 {
-            current_ghcb().cpuid(eax).ok()
+            current_ghcb().cpuid(eax, ecx).ok()
         } else {
-            cpuid_table(eax)
+            cpuid_table(eax, ecx)
         }
     }
 

kernel/src/platform/tdp.rs
@@ -130,8 +130,8 @@ impl SvsmPlatform for TdpPlatform {
         })
     }
 
-    fn cpuid(&self, eax: u32) -> Option<CpuidResult> {
-        Some(CpuidResult::get(eax, 0))
+    fn cpuid(&self, eax: u32, ecx: u32) -> Option<CpuidResult> {
+        Some(CpuidResult::get(eax, ecx))
     }
 
     unsafe fn write_host_msr(&self, msr: u32, value: u64) {

kernel/src/sev/ghcb.rs
@@ -308,9 +308,10 @@ impl GHCB {
         Ok(())
     }
 
-    pub fn cpuid(&self, eax: u32) -> Result<CpuidResult, SvsmError> {
+    pub fn cpuid(&self, eax: u32, ecx: u32) -> Result<CpuidResult, SvsmError> {
         self.clear();
         self.set_rax_valid(eax as u64);
+        self.set_rcx_valid(ecx as u64);
         self.vmgexit(GHCBExitCode::CPUID, 0, 0)?;
         Ok(CpuidResult {
             eax: self.get_rax_valid()? as u32,
@@ -321,7 +322,7 @@ impl GHCB {
     }
 
     pub fn cpuid_regs(&self, regs: &mut X86GeneralRegs) -> Result<(), SvsmError> {
-        let result = self.cpuid(regs.rax as u32)?;
+        let result = self.cpuid(regs.rax as u32, regs.rcx as u32)?;
         regs.rax = result.eax as usize;
         regs.rbx = result.ebx as usize;
         regs.rcx = result.ecx as usize;