2cbc76b4fb45b1916016626a03217035748f11b6
Carlos López <carlos.lopezr4096@gmail.com>
2025-05-04 21:15:24 +0200

    build: port build tool to Rust
    Remove a dependency on Python by porting the build.py script to Rust. Do
    this by adding a new workspace member and adding an alias, based on the
    xtask pattern [0] for ease of use.
    
    [0]: https://github.com/matklad/cargo-xtask/
    
    Signed-off-by: Carlos López <carlos.lopezr4096@gmail.com>

---
+++ xbuild/src/igvm.rs
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Author: Carlos López <carlos.lopezr4096@gmail.com>
+
+use crate::{run_cmd_checked, Args, BuildResult, RecipeParts, HELPERS};
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::process::Command;
+
+/// Platform flags supported by `igvmbuilder`.
+#[derive(Debug, Deserialize, Clone, Copy)]
+#[serde(rename_all = "lowercase")]
+enum IgvmPlatform {
+    Native,
+    Vsm,
+    Snp,
+    Tdp,
+}
+
+impl IgvmPlatform {
+    /// Get the required string argument to pass to igvmbuilder.
+    fn as_arg(&self) -> &str {
+        match self {
+            Self::Vsm => "--vsm",
+            Self::Tdp => "--tdp",
+            Self::Snp => "--snp",
+            Self::Native => "--native",
+        }
+    }
+}
+
+/// IGVM measure types
+#[derive(Debug, Deserialize, Clone, Copy, Default)]
+#[serde(rename_all = "lowercase")]
+enum IgvmMeasure {
+    #[default]
+    Print,
+}
+
+impl IgvmMeasure {
+    /// Get the string command to pass to igvmmeasure.
+    fn as_arg(&self) -> &str {
+        match self {
+            Self::Print => "measure",
+        }
+    }
+}
+
+/// Possible IGVM targets.
+#[derive(Clone, Copy, Debug, Deserialize, Hash, PartialEq, Eq)]
+enum IgvmTarget {
+    #[serde(rename = "qemu")]
+    Qemu,
+    #[serde(rename = "hyper-v")]
+    HyperV,
+    #[serde(rename = "vanadium")]
+    Vanadium,
+}
+
+impl IgvmTarget {
+    fn as_arg(&self) -> &str {
+        match self {
+            Self::Qemu => "qemu",
+            Self::HyperV => "hyper-v",
+            Self::Vanadium => "vanadium",
+        }
+    }
+}
+
+/// Configuration for a single IGVM target
+#[derive(Clone, Debug, Deserialize, Default)]
+#[serde(rename_all = "kebab-case")]
+struct IgvmTargetConfig {
+    /// Path for output file
+    #[serde(default = "IgvmTargetConfig::default_output")]
+    output: PathBuf,
+    /// See help for `igvmbuilder --policy`
+    #[serde(default = "IgvmTargetConfig::default_policy")]
+    policy: String,
+    /// See help for `igvmbuilder --comport`.
+    comport: Option<String>,
+    /// Platform flags for igvmbuilder
+    #[serde(default = "IgvmTargetConfig::default_platforms")]
+    platforms: Vec<IgvmPlatform>,
+    /// Main command passed to `igvmmeasure`.
+    #[serde(default)]
+    measure: IgvmMeasure,
+    /// See help for `igvmmeasure --native-zero`.
+    #[serde(default)]
+    measure_native_zeroes: bool,
+    /// See help for `igvmmeasure --check_kvm`.
+    #[serde(default)]
+    check_kvm: bool,
+}
+
+impl IgvmTargetConfig {
+    fn default_policy() -> String {
+        "0x30000".into()
+    }
+
+    fn default_output() -> PathBuf {
+        "default.json".into()
+    }
+
+    fn default_platforms() -> Vec<IgvmPlatform> {
+        vec![IgvmPlatform::Snp, IgvmPlatform::Tdp, IgvmPlatform::Vsm]
+    }
+
+    fn igvmbuild(
+        &self,
+        args: &Args,
+        target: IgvmTarget,
+        parts: &RecipeParts,
+    ) -> BuildResult<PathBuf> {
+        let output = PathBuf::from_iter(["bin".as_ref(), self.output.as_os_str()]);
+        let mut cmd = Command::new(HELPERS.igvmbuilder(args));
+        cmd.arg("--sort")
+            .arg("--output")
+            .arg(&output)
+            .args(["--policy", &self.policy])
+            .arg("--stage2")
+            .arg(&parts.stage2)
+            .arg("--kernel")
+            .arg(&parts.kernel);
+        if let Some(s1) = parts.stage1.as_ref() {
+            cmd.arg("--tdx-stage1").arg(s1);
+        }
+        if let Some(fw) = parts.firmware.as_ref() {
+            cmd.arg("--firmware").arg(fw);
+        }
+        if let Some(fs) = parts.fs.as_ref() {
+            cmd.arg("--filesystem").arg(fs);
+        }
+        if let Some(comport) = self.comport.as_ref() {
+            cmd.arg("--comport").arg(comport);
+        }
+        if args.verbose {
+            cmd.arg("--verbose");
+        }
+        for plat in self.platforms.iter() {
+            cmd.arg(plat.as_arg());
+        }
+        cmd.arg(target.as_arg());
+        run_cmd_checked(cmd, args)?;
+        Ok(output)
+    }
+
+    fn igvmmeasure(&self, args: &Args, bin: PathBuf) -> BuildResult<()> {
+        let mut cmd = Command::new(HELPERS.igvmmeasure(args));
+        if self.check_kvm {
+            cmd.arg("--check-kvm");
+        }
+        if self.measure_native_zeroes {
+            cmd.arg("--native-zero");
+        }
+        cmd.arg(bin).arg(self.measure.as_arg());
+        run_cmd_checked(cmd, args)
+    }
+
+    fn build(&self, args: &Args, target: IgvmTarget, parts: &RecipeParts) -> BuildResult<()> {
+        let bin = self.igvmbuild(args, target, parts)?;
+        self.igvmmeasure(args, bin)?;
+        Ok(())
+    }
+}
+
+/// IGVM configuration for a recipe. It consists of a list of
+/// hypervisor targets and a configuration for each of them.
+#[derive(Debug, Deserialize, Clone)]
+pub struct IgvmConfig {
+    #[serde(flatten, default)]
+    targets: HashMap<IgvmTarget, IgvmTargetConfig>,
+}
+
+impl IgvmConfig {
+    pub fn build(&self, args: &Args, parts: &RecipeParts) -> BuildResult<()> {
+        for (target, config) in self.targets.iter() {
+            config.build(args, *target, parts)?;
+        }
+        Ok(())
+    }
+}
