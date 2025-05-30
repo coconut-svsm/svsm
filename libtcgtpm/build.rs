coconut-svsm/svsm

commit c2e95ca954f0e0dd7249e11fcf50742854f7b527

libtcgtpm/build.rs: Use include files of libcrt for bindgen
Explicitly supply the include directory of libcrt to the bindgen call in
build.rs to make sure it always finds `stddef.h`.

Signed-off-by: Joerg Roedel <jroedel@suse.de>

libtcgtpm/build.rs
@@ -26,6 +26,7 @@ fn main() {
         .allowlist_file("deps/libtcgtpm.h")
         .use_core()
         .clang_arg("-Wno-incompatible-library-redeclaration")
+        .clang_arg("-isystemdeps/libcrt/include/")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .unwrap_or_else(|_| panic!("Unable to generate bindings for deps/libtcgtpm.h"));

commit 3ffb72dc962d893b2edc774c9d07721b2cf48596

libtcgtpm/build.rs: set -fno-pie clang arg
Commit c2e95ca9 ("libtcgtpm/build.rs: Use include files of libcrt for
bindgen") added `-isystemdeps/libcrt/include/` clang arg to the bindgen
call. This though produces an empty `bindings.rs` in our CI
(ubuntu-latest), thus leading `make test` to fail by not finding symbols
as `TPM_Manufacture`, etc.

This happens because in libcrt.h we have this on top:
    #if defined(__pie__)
    #pragma GCC visibility push (hidden)
    #endif

and probably on Ubuntu `-fno-pie` is not enabled by default.

So let's invoke clang always with `-fno-pie` to find all the symbols to
generate the bindings.

Fixes: c2e95ca9 ("libtcgtpm/build.rs: Use include files of libcrt for bindgen")
Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

libtcgtpm/build.rs
@@ -27,6 +27,7 @@ fn main() {
         .use_core()
         .clang_arg("-Wno-incompatible-library-redeclaration")
         .clang_arg("-isystemdeps/libcrt/include/")
+        .clang_arg("-fno-pie") // libcrt.h hides symbols if pie is enabled
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .unwrap_or_else(|_| panic!("Unable to generate bindings for deps/libtcgtpm.h"));