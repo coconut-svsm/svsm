4bbfe2c397a3fa02c9321c2a83b0a8787b09dbd7
Luigi Leonardi <leonardi@redhat.com>
2025-06-18 06:44:09 -0400

    scripts/launch_guest: add vsock to launch_guest
    Add an optional --vsock parameter. This will attach a vsock-device
    to the VM.
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

--- scripts/launch_guest.sh
+++ scripts/launch_guest.sh
@@ -34,7 +34,9 @@ ACCEL=kvm
 IGVM_OBJ=""
 
 STATE_DEVICE=""
-STATE_ENABLE=""
+VSOCK_DEVICE=""
+VIRTIO_ENABLE=""
+VIRTIO_CONFIG=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -54,8 +56,8 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
     --state)
-      STATE_ENABLE="x-svsm-virtio-mmio=on"
-      STATE_DEVICE+="-global virtio-mmio.force-legacy=false "
+      VIRTIO_ENABLE="x-svsm-virtio-mmio=on"
+      VIRTIO_CONFIG="-global virtio-mmio.force-legacy=false "
       STATE_DEVICE+="-drive file=$2,format=raw,if=none,id=svsm_storage,cache=none "
       STATE_DEVICE+="-device virtio-blk-device,drive=svsm_storage "
       shift
@@ -78,6 +80,13 @@ while [[ $# -gt 0 ]]; do
       ;;
     --aproxy)
       COM3_SERIAL="-serial unix:$2"
+      shift
+      shift
+      ;;
+    --vsock)
+      VIRTIO_ENABLE="x-svsm-virtio-mmio=on"
+      VIRTIO_CONFIG="-global virtio-mmio.force-legacy=false "
+      VSOCK_DEVICE+="-device vhost-vsock-device,guest-cid=$2 "
       shift
       shift
       ;;
@@ -176,7 +185,7 @@ fi
 $SUDO_CMD \
   $QEMU \
     -cpu $CPU \
-    -machine $MACHINE,$STATE_ENABLE \
+    -machine $MACHINE,$VIRTIO_ENABLE \
     -object $MEMORY \
     $IGVM_OBJ \
     $SNP_GUEST \
@@ -193,5 +202,7 @@ $SUDO_CMD \
     $COM4_SERIAL \
     $QEMU_EXIT_DEVICE \
     $QEMU_TEST_IO_DEVICE \
+    $VIRTIO_CONFIG \
     $STATE_DEVICE \
+    $VSOCK_DEVICE \
     "$@"
