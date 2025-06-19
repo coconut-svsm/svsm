a761a6643b65cf609286dfbeadfedb3d5cb55252
Joerg Roedel <joerg.roedel@amd.com>
2025-06-19 07:44:31 +0000

    kernel/cpu/shadow-stack: Move initializing to CPU code
    Move the initialization of the shadow stack contents to the code in
    the cpu crate. It is not well placed in the memory management code
    because it forces initialization to be done before the virtual address
    of the shadow stack can be known.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- kernel/src/cpu/mod.rs
+++ kernel/src/cpu/mod.rs
@@ -34,4 +34,5 @@ pub use idt::common::X86ExceptionContext;
 pub use irq_state::{irqs_disabled, irqs_enabled, IrqGuard, IrqState, TprGuard};
 pub use percpu::{irq_nesting_count, irqs_disable, irqs_enable, lower_tpr, raise_tpr};
 pub use registers::{X86GeneralRegs, X86InterruptFrame, X86SegmentRegs};
+pub use shadow_stack::ShadowStackInit;
 pub use tlb::*;
