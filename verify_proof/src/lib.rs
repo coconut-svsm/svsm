6cebdd3cf2357ad38609130fbc542651dc3a3f2c
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-07-24 17:23:46 -0700

    verify_proof: Add nonlinear and set proofs for computation related to allocator.
    Used by alloc.rs and alloc.verus.rs
    
    Signed-off-by: Ziqiao Zhou <ziqiaozhou@microsoft.com>

--- verify_proof/src/lib.rs
+++ verify_proof/src/lib.rs
@@ -16,6 +16,10 @@ pub mod frac_perm;
 #[cfg(verus_keep_ghost)]
 pub mod frac_ptr;
 #[cfg(verus_keep_ghost)]
+pub mod nonlinear;
+#[cfg(verus_keep_ghost)]
+pub mod set;
+#[cfg(verus_keep_ghost)]
 pub mod sum;
 
 verus! {
@@ -24,6 +28,7 @@ global size_of usize == 8;
 
 #[cfg_attr(verus_keep_ghost, verifier::broadcast_use_by_default_when_this_crate_is_imported)]
 pub broadcast group group_axioms {
+    set::lemma_set_usize_finite,
 }
 
 } // verus!
