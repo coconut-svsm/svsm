coconut-svsm/svsm

commit 56c43b2f6ef340348894a7db7d8a43706a8bf66f

testing: Split qemu_test_env into 2 booleans
The qemu testing environment adds the qemu testdev device, which is a
standard qemu device replicated in some other environments, plus
svsm-specific "IORequests" implemented only in the test-in-svsm.sh
script. Split this boolean into 2 so enviornments that have one but not
both can support as many tests as possible.

Signed-off-by: Adam Dunlap <acdunlap@google.com>

kernel/src/svsm.rs
@@ -353,7 +353,7 @@ pub extern "C" fn svsm_main(cpu_index: usize) {
     #[cfg(test)]
     {
         if config.has_qemu_testdev() {
-            crate::testutils::set_qemu_test_env();
+            crate::testutils::set_has_qemu_testdev();
         }
         crate::test_main();
     }

bootlib/src/igvm_params.rs
@@ -128,8 +128,11 @@ pub struct IgvmParamBlock {
     /// Indicates whether SVSM should use an IO port to read the qemu FwCfg.
     pub has_fw_cfg_port: u8,
 
+    /// Indicates whether SVSM can use "IORequest"s to assist with testing.
+    pub has_test_iorequests: u8,
+
     #[doc(hidden)]
-    pub _reserved: [u8; 2],
+    pub _reserved: [u8; 1],
 
     /// Metadata containing information about the firmware image embedded in the
     /// IGVM file.

igvmbuilder/src/igvm_builder.rs
@@ -240,6 +240,11 @@ impl IgvmBuilder {
             _ => 0,
         };
 
+        let has_test_iorequests = match self.options.hypervisor {
+            Hypervisor::Qemu => 1,
+            _ => 0,
+        };
+
         // Most of the parameter block can be initialised with constants.
         Ok(IgvmParamBlock {
             param_area_size,
@@ -261,6 +266,7 @@ impl IgvmBuilder {
             suppress_svsm_interrupts_on_snp,
             has_qemu_testdev,
             has_fw_cfg_port,
+            has_test_iorequests,
             ..Default::default()
         })
     }

kernel/src/block/virtio_blk.rs
@@ -71,7 +71,7 @@ impl BlockDriver for VirtIOBlkDriver {
 #[cfg(all(test, test_in_svsm))]
 mod tests {
     use crate::{
-        address::PhysAddr, fw_cfg::FwCfg, platform::SVSM_PLATFORM, testutils::is_qemu_test_env,
+        address::PhysAddr, fw_cfg::FwCfg, platform::SVSM_PLATFORM, testutils::has_test_iorequests,
     };
     use core::cmp::min;
     extern crate alloc;
@@ -111,7 +111,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_read_4sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_read(4);
         }
     }
@@ -119,7 +119,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_read_8sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_read(8);
         }
     }
@@ -127,7 +127,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_read_9sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_read(9);
         }
     }
@@ -135,7 +135,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_read_17sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_read(17);
         }
     }
@@ -172,7 +172,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_write_4sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_write(4);
         }
     }
@@ -180,7 +180,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_write_8sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_write(8);
         }
     }
@@ -188,7 +188,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_write_9sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_write(9);
         }
     }
@@ -196,7 +196,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_virtio_write_17sectors() {
-        if is_qemu_test_env() {
+        if has_test_iorequests() {
             virtio_write(17);
         }
     }

kernel/src/config.rs
@@ -150,4 +150,8 @@ impl<'a> SvsmConfig<'a> {
     pub fn has_qemu_testdev(&self) -> bool {
         self.igvm_params.has_qemu_testdev()
     }
+
+    pub fn has_test_iorequests(&self) -> bool {
+        self.igvm_params.has_test_iorequests()
+    }
 }

kernel/src/cpu/vc.rs
@@ -21,7 +21,7 @@ use crate::sev::ghcb::GHCB;
 use core::fmt;
 
 #[cfg(test)]
-use crate::testutils::{is_qemu_test_env, is_test_platform_type};
+use crate::testutils::{has_qemu_testdev, is_test_platform_type};
 
 pub const SVM_EXIT_EXCP_BASE: usize = 0x40;
 pub const SVM_EXIT_LAST_EXCP: usize = 0x5f;
@@ -471,7 +471,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_8() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_VAL: u8 = 0x12;
             verify_ghcb_gets_altered(|| outb(TESTDEV_ECHO_LAST_PORT, TEST_VAL));
             assert_eq!(
@@ -484,7 +484,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_16() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_VAL: u16 = 0x4321;
             verify_ghcb_gets_altered(|| outw(TESTDEV_ECHO_LAST_PORT, TEST_VAL));
             assert_eq!(
@@ -497,7 +497,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_32() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_VAL: u32 = 0xabcd1234;
             verify_ghcb_gets_altered(|| outl(TESTDEV_ECHO_LAST_PORT, TEST_VAL));
             assert_eq!(
@@ -510,7 +510,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_8_hardcoded() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_VAL: u8 = 0x12;
             verify_ghcb_gets_altered(|| outb_to_testdev_echo(TEST_VAL));
             assert_eq!(TEST_VAL, verify_ghcb_gets_altered(inb_from_testdev_echo));
@@ -520,7 +520,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_16_hardcoded() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_VAL: u16 = 0x4321;
             verify_ghcb_gets_altered(|| outw_to_testdev_echo(TEST_VAL));
             assert_eq!(TEST_VAL, verify_ghcb_gets_altered(inw_from_testdev_echo));
@@ -530,7 +530,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_32_hardcoded() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_VAL: u32 = 0xabcd1234;
             verify_ghcb_gets_altered(|| outl_to_testdev_echo(TEST_VAL));
             assert_eq!(TEST_VAL, verify_ghcb_gets_altered(inl_from_testdev_echo));
@@ -540,7 +540,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_port_io_string_16_get_last() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const TEST_DATA: &[u16] = &[0x1234, 0x5678, 0x9abc, 0xdef0];
             verify_ghcb_gets_altered(|| rep_outsw(TESTDEV_ECHO_LAST_PORT, TEST_DATA));
             assert_eq!(
@@ -576,7 +576,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_rdmsr_apic() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             let apic_base = verify_ghcb_gets_altered(|| read_msr(MSR_APIC_BASE));
             assert_eq!(apic_base & APIC_BASE_PHYS_ADDR_MASK, APIC_DEFAULT_PHYS_BASE);
         }
@@ -586,7 +586,7 @@ mod tests {
     //#[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     #[ignore = "DBG_CTL access no longer intercepted"]
     fn test_rdmsr_debug_ctl() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const MSR_DEBUG_CTL: u32 = 0x1d9;
             let apic_base = verify_ghcb_gets_altered(|| read_msr(MSR_DEBUG_CTL));
             assert_eq!(apic_base, 0);
@@ -598,7 +598,7 @@ mod tests {
     #[test]
     #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     fn test_wrmsr_tsc_aux() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             let test_val = 0x1234;
             verify_ghcb_gets_altered(||
                 // SAFETY: writing to TSC_AUX MSR doesn't break memory safety.
@@ -620,7 +620,7 @@ mod tests {
     // #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     #[ignore = "Currently unhandled by #VC handler"]
     fn test_vmmcall_vapic_poll_irq() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const VMMCALL_HC_VAPIC_POLL_IRQ: u32 = 1;
 
             let res = verify_ghcb_gets_altered(|| unsafe {
@@ -634,7 +634,7 @@ mod tests {
     // #[cfg_attr(not(test_in_svsm), ignore = "Can only be run inside guest")]
     #[ignore = "Currently unhandled by #VC handler"]
     fn test_read_write_dr7() {
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_qemu_testdev() && is_test_platform_type(SvsmPlatformType::Snp) {
             const DR7_DEFAULT: u64 = 0x400;
             const DR7_TEST: u64 = 0x401;
 

kernel/src/greq/services.rs
@@ -121,12 +121,12 @@ mod tests {
 
         use crate::serial::Terminal;
         use crate::testing::{assert_eq_warn, svsm_test_io, IORequest};
-        use crate::testutils::{is_qemu_test_env, is_test_platform_type};
+        use crate::testutils::{has_test_iorequests, is_test_platform_type};
 
         use alloc::vec;
         use bootlib::platform::SvsmPlatformType;
 
-        if is_qemu_test_env() && is_test_platform_type(SvsmPlatformType::Snp) {
+        if has_test_iorequests() && is_test_platform_type(SvsmPlatformType::Snp) {
             let sp = svsm_test_io().unwrap();
 
             sp.put_byte(IORequest::GetLaunchMeasurement as u8);

kernel/src/igvm_params.rs
@@ -446,4 +446,8 @@ impl IgvmParams<'_> {
     pub fn has_fw_cfg_port(&self) -> bool {
         self.igvm_param_block.has_fw_cfg_port != 0
     }
+
+    pub fn has_test_iorequests(&self) -> bool {
+        self.igvm_param_block.has_test_iorequests != 0
+    }
 }

commit adef0c43d3d10023c53659db2fc3b933e57321c5

testing: initialize HAS_TEST_IOREQUESTS variable
Commit 56c43b2 introduced the variable `HAS_TEST_IOREQUESTS`, but it was never set.
This caused all the tests calling `has_test_iorequests()` to never run.

Initialize it according to the IGVM parameter.

Fixes: 56c43b2 ("testing: Split qemu_test_env into 2 booleans")
Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

kernel/src/svsm.rs
@@ -356,6 +356,9 @@ pub extern "C" fn svsm_main(cpu_index: usize) {
         if config.has_qemu_testdev() {
             crate::testutils::set_has_qemu_testdev();
         }
+        if config.has_test_iorequests() {
+            crate::testutils::set_has_test_iorequests();
+        }
         crate::test_main();
     }
 