71083d093ab9020083bcf051954a55603ecce140
Joerg Roedel <joerg.roedel@amd.com>
2025-08-06 10:15:56 +0200

    COCONUT-SVSM Release 2024.09-devel
    Development release for September 2025.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- release/src/lib.rs
+++ release/src/lib.rs
@@ -51,7 +51,7 @@ impl fmt::Display for SvsmVersion {
 }
 
 const VERSION_YEAR: u32 = 2025;
-const VERSION_MONTH: u32 = 8;
+const VERSION_MONTH: u32 = 9;
 #[allow(dead_code)]
 const VERSION_COUNTER: u32 = 0;
 
