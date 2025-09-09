3b8f050a64fd2eebc52ed6f32acc0e4e6224f348
Nicolai Stange <nstange@suse.de>
2025-09-09 16:42:45 +0000

    Add cocoon-tpm-storage dep and wire it up to SVSM's BlockDriver
    In working further towards supporting persistence built on CocoonFs,
    
    - introduce a "persistence" Cargo feature for gating the functionality,
    - add an optional dependency to the cocoon-tpm-storage crate,
      which provides the CocoonFs implementation,
    - and wire it up to the SVSM's BlockDriver trait. More specifically,
      introduce a SvsmNvBlkDev struct wrapper around a generic D dereffing
      to BlockDriver, and implement the cocoon-tpm-storage's NvBlkDev trait
      for it.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -42,6 +42,8 @@ pub mod kernel_region;
 pub mod locking;
 pub mod log_buffer;
 pub mod mm;
+#[cfg(feature = "persistence")]
+pub mod persistence;
 pub mod platform;
 pub mod protocols;
 pub mod requests;
