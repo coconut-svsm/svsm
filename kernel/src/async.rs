2f4396dbc2bee407ae1993e92976b793f7b66082
Nicolai Stange <nstange@suse.de>
2025-09-09 12:52:02 +0000

    async: implement busy-polling Rust async task executor
    Implement the rather trivial task_busypoll_to_completion() for
    busy-polling Rust async tasks, typically represented by types
    implementing the Future trait or similar, to completion.
    
    For more flexibility, task_busypoll_to_completion() does not operate
    on Rust Futures directly, but invokes a user provided closure that
    takes only a Context. That closure might wrap an actual Future or,
    alternatively, something else which is is only "Future-like".
    
    Example:
      let result = task_busypoll_to_completion(
        |cx| { Future::poll(my_fut, cx) }
      );
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- kernel/src/async.rs
+++ kernel/src/async.rs
@@ -6,6 +6,7 @@
 
 use cocoon_tpm_utils_async as utils_async;
 
+use core::task;
 use utils_async::sync_types;
 
 use crate::locking::{LockGuard, RWLock, ReadLockGuard, SpinLock, WriteLockGuard};
@@ -59,4 +60,24 @@ impl sync_types::SyncTypes for SvsmSyncTypes {
     type Lock<T: Send> = SpinLock<T>;
     type RwLock<T: Send + Sync> = RWLock<T>;
     type SyncRcPtrFactory = sync_types::GenericArcFactory;
+}
+
+/// Busypoll an asynchronous task to completion.
+///
+/// The task to poll on is represented as a closure, `task`. Note that this scheme enables
+/// convenient polling on standard Rust [`Future`]s, as well as on `Future`-like types taking
+/// additional arguments for their `poll()`.
+///
+/// # Arguments:
+///
+/// * `task` -> The task to poll to completion.
+pub fn task_busypoll_to_completion<O, T: FnMut(&mut task::Context<'_>) -> task::Poll<O>>(
+    mut task: T,
+) -> O {
+    let mut ctx = task::Context::from_waker(task::Waker::noop());
+    loop {
+        if let task::Poll::Ready(result) = task(&mut ctx) {
+            return result;
+        }
+    }
 }
