24b4179e1ffc8cffb0eaa7ec7cf3d87d6c3e7886
Peter Fang <peter.fang@intel.com>
2025-10-03 00:13:13 -0700

    igvmbuilder/ovmf: Add TDX metadata
    Add support for OVMF's TDX metadata, identified using GUID
    e47a6535-984a-4798-865e-4685a7bf8ec2.
    
    This is mostly just a placeholder for now. It only verifies the
    signature and ensures that all of the metadata entries can be
    successfully parsed.
    
    Signed-off-by: Peter Fang <peter.fang@intel.com>

--- bootlib/src/firmware.rs
+++ bootlib/src/firmware.rs
@@ -6,6 +6,7 @@ use uuid::{uuid, Uuid};
 
 pub const OVMF_RESET_VECTOR_GUID: Uuid = uuid!("813d7b2c-9558-4d88-8b31-1427a85abe69");
 pub const OVMF_SEV_METADATA_GUID: Uuid = uuid!("dc886566-984a-4798-a75e-5585a7bf67cc");
+pub const OVMF_TDX_METADATA_GUID: Uuid = uuid!("e47a6535-984a-4798-865e-4685a7bf8ec2");
 pub const OVMF_TABLE_FOOTER_GUID: Uuid = uuid!("96b582de-1fb2-45f7-baea-a366c55a082d");
 pub const SEV_INFO_BLOCK_GUID: Uuid = uuid!("00f771de-1a7e-4fcb-890e-68c77e2fb44e");
 pub const SVSM_INFO_GUID: Uuid = uuid!("a789a612-0597-4c4b-a49f-cbb1fe9d1ddd");
