f356ad876eb601db75170bb28dc3ccb27f0cf18c
Luigi Leonardi <leonardi@redhat.com>
2025-11-13 08:09:08 -0500

    kernel: introduce VsockDriver trait for driver abstraction
    Define a VsockDriver trait to abstract vsock operations and enable
    multiple backend implementations.
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

---
+++ kernel/src/vsock/mod.rs
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2025 Red Hat, Inc.
+//
+// Author: Luigi Leonardi <leonardi@redhat.com>
+
+pub mod api;
+pub mod error;
+
+pub use error::VsockError;
+
+/// Well-known CID for the host.
+pub const VMADDR_CID_HOST: u64 = 2;
+
+extern crate alloc;
+use crate::{utils::immut_after_init::ImmutAfterInitCell, vsock::api::VsockDriver};
+use alloc::boxed::Box;
+
+// Currently only one vsock device is supported.
+static VSOCK_DEVICE: ImmutAfterInitCell<Box<dyn VsockDriver>> = ImmutAfterInitCell::uninit();
