78dbec39846f36f487c772b02a09278438496be3
ziqiaozhou <ziqiaozhou@microsoft.com>
2025-11-21 18:46:12 +0000

    scripts/vinstall: install rust toolchain needed by verus
    Add verus-compatible rust toolchain 1.91.0 in vinstall.sh, because it
    might be different from the svsm's default rust toolchain version.
    
    If the verus-compatible rust toolchain is not installed, the udpated
    cargo-v tool will an error like following:
    
    Failed to get verus version(exit status: 1): verus: required rust toolchain 1.91.0-x86_64-unknown-linux-gnu not found
    run the following command (in a bash-compatible shell) to install the necessary toolchain:
    rustup install 1.91.0-x86_64-unknown-linux-gnu
    
    Signed-off-by: ziqiaozhou <ziqiaozhou@microsoft.com>

--- scripts/vinstall.sh
+++ scripts/vinstall.sh
@@ -5,12 +5,12 @@
 #
 # Author: Ziqiao Zhou <ziqiaozhou@microsoft.com>
 # A script to install verus tools
-VERISMO_REV=ffee0c9
-VERUS_RUST_VERSION=1.91
+VERISMO_REV=4f504b7
+VERUS_RUST_VERSION=1.91.0
 
 # Install x86_64-unknown-none target for verus-compatible Rust version
 export RUSTUP_TOOLCHAIN=$VERUS_RUST_VERSION
-rustup target add x86_64-unknown-none
+rustup target add x86_64-unknown-none --toolchain $RUSTUP_TOOLCHAIN
 # Install the verus toolchain
 cargo install --git https://github.com/microsoft/verismo/ --rev $VERISMO_REV cargo-v
 # verus-rustc as a wrapper to call verus with proper rustc flags.
