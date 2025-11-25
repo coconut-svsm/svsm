62dd355d2ade19c38a1b2c1225e30a3a9f79a042
Nicola Ramacciotti <niko.ramak@gmail.com>
2025-11-25 17:06:48 +0100

    user/lib: Implement utilities to extract Layout from size or pointer
    Implement utilities to extract `Layout` from a size ora a pointer
    and test them.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- user/lib/src/alloc.rs
+++ user/lib/src/alloc.rs
@@ -1,8 +1,16 @@
 use coconut_alloc::AllocBlock;
-use core::alloc::{GlobalAlloc, Layout};
+#[cfg(any(test, all(not(test), target_os = "none")))]
+use core::alloc::GlobalAlloc;
+use core::alloc::Layout;
+#[cfg(any(test, all(not(test), target_os = "none")))]
 use core::ptr;
 use core::ptr::{addr_of, addr_of_mut};
 use core::sync::atomic::{AtomicUsize, Ordering};
+
+#[derive(Debug)]
+pub enum AllocError {
+    Layout,
+}
 
 const MAX_ALLOC_SIZE: usize = 32 * 1024; // 32 KiB
 const UNINITIALIZED: usize = 0;
@@ -44,8 +52,10 @@ fn get_global_heap() -> &'static AllocBlock {
     unsafe { &*addr_of!(HEAP) }
 }
 
+#[cfg(any(test, all(not(test), target_os = "none")))]
 struct SvsmUserAllocator;
 
+#[cfg(any(test, all(not(test), target_os = "none")))]
 // SAFETY: AllockBlock is lockless for allocations up to 32KB.
 // HEAP is write-once and only read via immutable references after initialization.
 // AllocBlock uses atomic operations internally to handle concurrent alloc/free safely.
@@ -82,3 +92,67 @@ unsafe impl GlobalAlloc for SvsmUserAllocator {
 #[cfg(all(not(test), target_os = "none"))]
 #[global_allocator]
 static GLOBAL_ALLOC: SvsmUserAllocator = SvsmUserAllocator;
+
+/// Build a Layout suitable for requests to the coconut allocator.
+pub fn layout_from_size(size: usize) -> Result<Layout, AllocError> {
+    if size == 0 || size > MAX_ALLOC_SIZE {
+        return Err(AllocError::Layout);
+    }
+    let next = size.next_power_of_two();
+
+    Layout::from_size_align(next, next).map_err(|_| AllocError::Layout)
+}
+
+/// Try to reconstruct the Layout for a pointer previously returned by
+/// an allocation.
+/// # Safety
+/// The pointer must have been allocated from the global heap.
+pub unsafe fn layout_from_ptr(ptr: *mut u8) -> Option<Layout> {
+    let heap = get_global_heap();
+    let base = heap as *const AllocBlock as *const u8;
+
+    // SAFETY: ptr must have been allocated from this heap
+    let off = unsafe { ptr.offset_from(base) };
+
+    if off < 0 {
+        // Should I check upper bound too? Importing BLOCK_SIZE from coconut_alloc
+        return None;
+    }
+
+    heap.layout_from_offset(off as usize)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_ALLOCATOR: SvsmUserAllocator = SvsmUserAllocator;
+
+    #[test]
+    fn test_layout_from_size() {
+        let layout = layout_from_size(100).unwrap();
+        assert_eq!(layout.size(), 128);
+        assert_eq!(layout.align(), 128);
+
+        let layout = layout_from_size(0);
+        assert!(layout.is_err());
+
+        let layout = layout_from_size(MAX_ALLOC_SIZE + 1);
+        assert!(layout.is_err());
+    }
+
+    #[test]
+    fn test_allocation_and_deallocation() {
+        set_global_heap();
+        let layout = layout_from_size(200).unwrap();
+        // SAFETY: Using the global allocator
+        unsafe {
+            let ptr = TEST_ALLOCATOR.alloc(layout);
+            assert!(!ptr.is_null());
+            let layout_reconstructed = layout_from_ptr(ptr).unwrap();
+            assert_eq!(layout_reconstructed.size(), 256);
+            assert_eq!(layout_reconstructed.align(), 256);
+            TEST_ALLOCATOR.dealloc(ptr, layout);
+        }
+    }
+}
