coconut-svsm/svsm

commit f1bcf0f0224c17e4b3892575be2a46ecd36ec003

kernel/mm/alloc: Do not use 0 as no-page marker
The page allocator uses zero as a marker when no page can be
allocated. This is problematic because PFN zero can be a valid page.
Change the NO_PAGE marker to a different value which will never appear
as a PFN.

Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

kernel/src/mm/alloc.rs
@@ -56,6 +56,9 @@ impl From<AllocError> for SvsmError {
 #[verus_spec]
 pub const MAX_ORDER: usize = 6;
 
+/// Special marker for no-page
+const NO_PAGE: usize = usize::MAX;
+
 /// Calculates the order of a given size for page allocation.
 ///
 /// # Arguments
@@ -118,6 +121,8 @@ impl PageStorageType {
     const TYPE_SHIFT: u64 = 4;
     const TYPE_MASK: u64 = (1u64 << Self::TYPE_SHIFT) - 1;
     const NEXT_SHIFT: u64 = 12;
+    const NEXT_MASK: u64 = !((1u64 << Self::NEXT_SHIFT) - 1);
+    const NO_NEXT: usize = NO_PAGE & Self::NEXT_MASK as usize;
     const ORDER_MASK: u64 = (1u64 << (Self::NEXT_SHIFT - Self::TYPE_SHIFT)) - 1;
     // Slab item sizes are encoded in a u16
     const SLAB_MASK: u64 = 0xffff;
@@ -164,7 +169,13 @@ impl PageStorageType {
     #[verus_verify(dual_spec(spec_encode_next))]
     #[verus_spec(returns self.spec_encode_next(next_page))]
     fn encode_next(self, next_page: usize) -> Self {
-        Self(self.0 | (next_page as u64) << Self::NEXT_SHIFT)
+        let page: u64 = if next_page == NO_PAGE {
+            Self::NO_NEXT as u64
+        } else {
+            ((next_page as u64) << Self::NEXT_SHIFT) & Self::NEXT_MASK
+        };
+
+        Self(self.0 | page)
     }
 
     /// Encodes the virtual address of the slab
@@ -221,7 +232,12 @@ impl PageStorageType {
     )]
     fn decode_next(&self) -> usize {
         proof! {broadcast use lemma_bit_u64_shr_bound;}
-        (self.0 >> Self::NEXT_SHIFT) as usize
+        let next: usize = (self.0 & Self::NEXT_MASK) as usize;
+        if next == Self::NO_NEXT {
+            NO_PAGE
+        } else {
+            next >> Self::NEXT_SHIFT
+        }
     }
 
     /// Decodes the slab
@@ -534,7 +550,7 @@ impl MemoryRegion {
             start_virt: VirtAddr::null(),
             page_count: 0,
             nr_pages: [0; MAX_ORDER],
-            next_page: [0; MAX_ORDER],
+            next_page: [NO_PAGE; MAX_ORDER],
             free_pages: [0; MAX_ORDER],
             #[cfg(verus_keep_ghost_body)]
             perms: Tracked::assume_new(),
@@ -763,7 +779,7 @@ impl MemoryRegion {
     fn get_next_page(&mut self, order: usize) -> Result<usize, AllocError> {
         let pfn = self.next_page[order];
 
-        if pfn == 0 {
+        if pfn == NO_PAGE {
             return Err(AllocError::OutOfMemory);
         }
 
@@ -920,7 +936,7 @@ impl MemoryRegion {
             .next_page
             .get(order)
             .ok_or(AllocError::InvalidPageOrder(order))?;
-        if next_page != 0 {
+        if next_page != NO_PAGE {
             return Ok(());
         }
 
@@ -1194,7 +1210,7 @@ impl MemoryRegion {
         let first_pfn = self.next_page[order];
 
         // Handle special cases first
-        if first_pfn == 0 {
+        if first_pfn == NO_PAGE {
             // No pages for that order
             return Err(AllocError::OutOfMemory);
         } else if first_pfn == pfn {

commit 63d7a89db033d3ba8ad6cf6dfcdafa80768af901

kernel/mm/alloc: Fix incomplete NO_PAGE pfn checks in allocate_pfn
This is to fix the incomplete change in f1bcf0f0.

Without this fix, the allocator will try to read page info at NO_PAGE
when it is out of mem at an order and then panic on an invalid page
number in check_pfn.

Fixes: f1bcf0f0 ("kernel/mm/alloc: Do not use 0 as no-page marker")

Signed-off-by: Ziqiao Zhou <ziqiaozhou@microsoft.com>

kernel/src/mm/alloc.rs
@@ -1253,7 +1253,7 @@ impl HeapMemoryRegion {
             }
             proof_with!(Tracked(&current_perm));
             let current_pfn = self.next_free_pfn(old_pfn, order);
-            if current_pfn == 0 {
+            if current_pfn == NO_PAGE {
                 return Err(AllocError::OutOfMemory);
             }
 