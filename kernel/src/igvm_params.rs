coconut-svsm/svsm

commit dd438cacf8ae81d4738a3d6b7701af13175433c1

bootlib/igvm_params: Use byte-level IGVM memory map
OVMF defines the IGVM memory map as a sub-parameter area within an IGVM
parameter area [1]. This area can be located anywhere in the parameter
area and doesn't have any alignment or size constraints. Change `struct
IgvmParamBlockFwInfo` accordingly to accomodate this, as the current
definition assumes that the map address is always page-aligned and the
map area is always page-sized.

Since now there is an increased likelihood that a misconfigured IGVM
file or guest firmware image could cause SVSM to overflow the memory
map, print a warning when such a case occurs.

[1] https://github.com/tianocore/edk2/pull/11487

Signed-off-by: Peter Fang <peter.fang@intel.com>

kernel/src/igvm_params.rs
@@ -179,16 +179,13 @@ impl IgvmParams<'_> {
         // If the parameters do not include a guest memory map area, then no
         // work is required.
         let fw_info = &self.igvm_param_block.firmware;
-        if fw_info.memory_map_page_count == 0 {
+        if fw_info.memory_map_size == 0 {
             return Ok(());
         }
 
         // Map the guest memory map area into the address space.
-        let mem_map_gpa = PhysAddr::from(fw_info.memory_map_page as u64 * PAGE_SIZE as u64);
-        let mem_map_region = MemoryRegion::<PhysAddr>::new(
-            mem_map_gpa,
-            fw_info.memory_map_page_count as usize * PAGE_SIZE,
-        );
+        let mem_map_gpa = PhysAddr::from(fw_info.memory_map_address as u64);
+        let mem_map_region = MemoryRegion::new(mem_map_gpa, fw_info.memory_map_size as usize);
         log::info!(
             "Filling guest IGVM memory map at {:#018x} size {:#018x}",
             mem_map_region.start(),
@@ -220,18 +217,21 @@ impl IgvmParams<'_> {
         }
 
         // Calculate the maximum number of entries that can be inserted.
-        let max_entries = fw_info.memory_map_page_count as usize * PAGE_SIZE
-            / size_of::<IGVM_VHS_MEMORY_MAP_ENTRY>();
+        let max_entries = fw_info.memory_map_size as usize / size_of::<IGVM_VHS_MEMORY_MAP_ENTRY>();
+        // Return an error if an overflow occurs.
+        if map.len() > max_entries {
+            log::warn!(
+                "Too many IGVM memory map entries ({}), max is {}",
+                map.len(),
+                max_entries
+            );
+            return Err(SvsmError::Firmware);
+        }
 
         // Generate a guest pointer range to hold the memory map.
         let mem_map = GuestPtr::new(mem_map_va);
 
         for (i, entry) in map.iter().enumerate() {
-            // Return an error if an overflow occurs.
-            if i >= max_entries {
-                return Err(SvsmError::Firmware);
-            }
-
             // SAFETY: mem_map_va points to newly mapped memory, whose physical
             // address is defined in the IGVM config.
             unsafe {
@@ -239,7 +239,7 @@ impl IgvmParams<'_> {
                     .offset(i as isize)
                     .write(IGVM_VHS_MEMORY_MAP_ENTRY {
                         starting_gpa_page_number: u64::from(entry.start()) / PAGE_SIZE as u64,
-                        number_of_pages: entry.len() as u64 / PAGE_SIZE as u64,
+                        number_of_pages: (entry.len() / PAGE_SIZE) as u64,
                         entry_type: MemoryMapEntryType::default(),
                         flags: 0,
                         reserved: 0,
@@ -356,10 +356,10 @@ impl IgvmParams<'_> {
         // If this firmware expects an IGVM memory map but the IGVM memory
         // map is not within any of the firmware GPA ranges, then add the IGVM
         // memory map to the set of firmware regions.
-        if fw_info.memory_map_page_count != 0 {
+        if fw_info.memory_map_size != 0 {
             let map_region = MemoryRegion::new(
-                PhysAddr::from(fw_info.memory_map_page as u64 * PAGE_SIZE as u64),
-                fw_info.memory_map_page_count as usize * PAGE_SIZE,
+                PhysAddr::from(fw_info.memory_map_address as u64),
+                fw_info.memory_map_size as usize,
             );
             // Scan all the firmware regions to determine whether any of them
             // contain the memory map.  If not, the memory map should be added

bootlib/src/igvm_params.rs
@@ -71,11 +71,12 @@ pub struct IgvmParamBlockFwInfo {
 
     /// The guest physical address of the IGVM memory map consumed by the
     /// guest firmware.
-    pub memory_map_page: u32,
+    pub memory_map_address: u32,
 
-    /// The number of pages reserved for the IGVM memory map consumed by the
-    /// guest firmware.
-    pub memory_map_page_count: u32,
+    /// The number of bytes reserved for the IGVM memory map consumed by the
+    /// guest firmware.  Use byte-level size here because the firmware is
+    /// allowed to define a memory map area that isn't page-sized.
+    pub memory_map_size: u32,
 
     /// The number of prevalidated memory regions defined by the firmware.
     pub prevalidated_count: u32,

igvmbuilder/src/igvm_firmware.rs
@@ -308,23 +308,13 @@ impl IgvmFirmware {
         if let Some(gpa) = parameter_area.gpa {
             if parameter_area.memory_map {
                 // Capture the memory map in the firmware information.
-                let memory_map_page = gpa / PAGE_SIZE_4K;
-                self.fw_info.memory_map_page = memory_map_page as u32;
-                if self.fw_info.memory_map_page as u64 != memory_map_page {
-                    let e = format!("Memory map address {:#018x} is larger than 32 bits", gpa);
-                    return Err(e.into());
-                }
-                let page_count = parameter_area.number_of_bytes.div_ceil(PAGE_SIZE_4K);
-                // Truncate the page count if it is too large to fit into a
+                self.fw_info.memory_map_address = u32::try_from(gpa)?;
+                // Truncate the byte count if it is too large to fit into a
                 // 32-bit number.  It is acceptable for the SVSM to provide a
                 // smaller set of data than the firmware is capable of
                 // handling.
-                self.fw_info.memory_map_page_count = if page_count > 0xFFFF_FFFF {
-                    0xFFFF_FFFF
-                } else {
-                    page_count as u32
-                };
-
+                self.fw_info.memory_map_size =
+                    u32::try_from(parameter_area.number_of_bytes).unwrap_or(u32::MAX);
                 Ok(false)
             } else if parameter_area.parameter_list.is_empty() {
                 Ok(false)

commit 5c5af8ac98a43e5dd776c87fef3f2a7d6a9527cc

kernel/igvm_params: Allow unaligned guest memory map
The IGVM memory map does not need to be page-aligned or page-sized.
Don't make assumptions about alignment when writing to the memory map.
Pre-validation of the memory map is still done on page boundaries.

Fixes: dd438cacf ("bootlib/igvm_params: Use byte-level IGVM memory map")
Reported-by: Luigi Leonardi <leonardi@redhat.com>
Tested-by: Luigi Leonardi <leonardi@redhat.com>
Signed-off-by: Peter Fang <peter.fang@intel.com>

kernel/src/igvm_params.rs
@@ -14,7 +14,7 @@ use crate::mm::alloc::free_multiple_pages;
 use crate::mm::{GuestPtr, PerCPUPageMappingGuard, PAGE_SIZE};
 use crate::platform::{PageStateChangeOp, PageValidateOp, SevFWMetaData, SVSM_PLATFORM};
 use crate::types::PageSize;
-use crate::utils::{round_to_pages, MemoryRegion};
+use crate::utils::{page_align_up, round_to_pages, MemoryRegion};
 use alloc::vec::Vec;
 use cpuarch::vmsa::VMSA;
 
@@ -177,17 +177,21 @@ impl IgvmParams<'_> {
         // If the parameters do not include a guest memory map area, then no
         // work is required.
         let fw_info = &self.igvm_param_block.firmware;
-        if fw_info.memory_map_size == 0 {
+        let mem_map_size = if fw_info.memory_map_size == 0 {
             return Ok(());
-        }
+        } else {
+            fw_info.memory_map_size as usize
+        };
 
         // Map the guest memory map area into the address space.
         let mem_map_gpa = PhysAddr::from(fw_info.memory_map_address as u64);
-        let mem_map_region = MemoryRegion::new(mem_map_gpa, fw_info.memory_map_size as usize);
+        // Guest memory map may not be page-aligned or page-sized.
+        let mem_map_region =
+            MemoryRegion::new(mem_map_gpa.page_align(), page_align_up(mem_map_size));
         log::info!(
             "Filling guest IGVM memory map at {:#018x} size {:#018x}",
-            mem_map_region.start(),
-            mem_map_region.len(),
+            mem_map_gpa,
+            mem_map_size,
         );
 
         let mem_map_mapping =
@@ -217,7 +221,7 @@ impl IgvmParams<'_> {
         }
 
         // Calculate the maximum number of entries that can be inserted.
-        let max_entries = fw_info.memory_map_size as usize / size_of::<IGVM_VHS_MEMORY_MAP_ENTRY>();
+        let max_entries = mem_map_size / size_of::<IGVM_VHS_MEMORY_MAP_ENTRY>();
         // Return an error if an overflow occurs.
         if map.len() > max_entries {
             log::warn!(
@@ -229,7 +233,7 @@ impl IgvmParams<'_> {
         }
 
         // Generate a guest pointer range to hold the memory map.
-        let mem_map = GuestPtr::new(mem_map_va);
+        let mem_map = GuestPtr::new(mem_map_va + mem_map_gpa.page_offset());
 
         for (i, entry) in map.iter().enumerate() {
             // SAFETY: mem_map_va points to newly mapped memory, whose physical