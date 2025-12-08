13753a9f2c969ae75eb65016569e06ac85b1155d
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-12-08 21:43:57 +0000

    verify_external: Remove spec for From, Into, and Integer in verify_external
    Their spec are defined in vstd since 11/24/2025;
    Thus we do not need to maintain the spec in verify_external
    
    Signed-off-by: Ziqiao Zhou <ziqiaozhou@microsoft.com>

--- verify_external/src/convert.verus.rs
+++ verify_external/src/convert.verus.rs
@@ -4,73 +4,8 @@
 //
 // Author: Ziqiao Zhou <ziqiaozhou@microsoft.com>
 use vstd::prelude::*;
+use vstd::std_specs::convert::{FromSpec, IntoSpec};
 verus! {
-
-pub trait FromSpec<T>: Sized {
-    spec fn from_spec(v: T) -> Self;
-}
-
-pub trait IntoSpec<T>: Sized {
-    spec fn into_spec(self) -> T;
-}
-
-impl<T, U> IntoSpec<U> for T where U: FromSpec<T> {
-    open spec fn into_spec(self) -> U {
-        U::from_spec(self)
-    }
-}
-
-pub trait FromIntoInteger: FromSpec<int> + IntoSpec<int> {
-
-}
-
-macro_rules! def_primitive_from{
-    ($toty: ty; $($fromty: ty),*) => {verus!{
-        $(
-            impl FromSpec<$fromty> for $toty {
-                open spec fn from_spec(v: $fromty) -> Self {
-                    v as $toty
-                }
-            }
-        )*
-    }}
-}
-
-def_primitive_from!{u8; u8, int}
-
-def_primitive_from!{u16; u8, u16, int}
-
-def_primitive_from!{u32; u8, u16, u32, int}
-
-def_primitive_from!{u64; u8, u16, u32, u64, usize, int}
-
-def_primitive_from!{usize; u8, u16, u32, usize, int}
-
-def_primitive_from!{u128; u8, u16, u32, u64, usize, u128, int}
-
-def_primitive_from!{int; u8, u16, u32, u64, usize, u128, int, nat}
-
-def_primitive_from!{nat; u8, u16, u32, u64, usize, u128, nat}
-
-impl FromIntoInteger for u8 {
-
-}
-
-impl FromIntoInteger for u16 {
-
-}
-
-impl FromIntoInteger for u32 {
-
-}
-
-impl FromIntoInteger for u64 {
-
-}
-
-impl FromIntoInteger for usize {
-
-}
 
 #[verifier(inline)]
 pub open spec fn exists_into<T, U>(v: T, r: spec_fn(v: U) -> bool) -> bool where T: Into<U> {
