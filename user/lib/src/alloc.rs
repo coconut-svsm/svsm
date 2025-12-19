a6cbe6d4a8df3dd7d48191106c1226c6e22e17c4
Nicola Ramacciotti <niko.ramak@gmail.com>
2025-12-19 14:04:29 +0100

    user/lib: Implement GlobalAlloc trait to use the alloc crate
    Implement the `GlobalAlloc` trait by wrapping the lockless `alloc`
    and `free` operations of `AllocBlock`, which use atomic operations
    internally. Since those operation return offset from the start of
    the block, convert them to pointers relative to the heap base.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- user/lib/src/alloc.rs
+++ user/lib/src/alloc.rs
@@ -2,6 +2,8 @@
 
 use crate::{console_print, print, println};
 use coconut_alloc::AllocBlock;
+use core::alloc::{GlobalAlloc, Layout};
+use core::ptr;
 use core::ptr::{addr_of, addr_of_mut};
 use core::sync::atomic::{AtomicUsize, Ordering};
 
@@ -11,6 +13,7 @@ pub enum AllocError {
     NotInitialized,
 }
 
+const MAX_ALLOC_SIZE: usize = 32 * 1024; // 32 KiB
 const UNINITIALIZED: usize = 0;
 const INITIALIZING: usize = 1;
 const INITIALIZED: usize = 2;
@@ -54,3 +57,45 @@ fn get_global_heap() -> Result<&'static AllocBlock, AllocError> {
     // STATE_INITIALIZED ensures single initialization.
     unsafe { Ok(&*addr_of!(HEAP)) }
 }
+
+struct SvsmUserAllocator;
+
+// SAFETY: AllockBlock is lockless for allocations up to 32KB.
+// HEAP is write-once and only read via immutable references after initialization.
+// AllocBlock uses atomic operations internally to handle concurrent alloc/free safely.
+unsafe impl GlobalAlloc for SvsmUserAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        if layout.size() == 0 || layout.size() > MAX_ALLOC_SIZE {
+            return ptr::null_mut();
+        }
+
+        let Ok(heap) = get_global_heap() else {
+            return ptr::null_mut();
+        };
+
+        match heap.alloc(layout.size()) {
+            Ok(off) => {
+                let base = heap as *const AllocBlock as *const u8;
+                // SAFETY: Atomic allocation just performed
+                // The offset is valid
+                unsafe { base.add(off) as *mut u8 }
+            }
+            Err(_) => ptr::null_mut(),
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        let Ok(heap) = get_global_heap() else {
+            return;
+        };
+
+        let base = heap as *const AllocBlock as *const u8;
+        // SAFETY: ptr must have been allocated from this heap
+        let off = unsafe { ptr.offset_from(base) as usize };
+
+        heap.free(off);
+    }
+}
+
+#[global_allocator]
+static GLOBAL_ALLOC: SvsmUserAllocator = SvsmUserAllocator;
