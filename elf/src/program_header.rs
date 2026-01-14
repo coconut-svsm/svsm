8cfae7d8dcdda96666aead3cd59a2eab71574c39
Carlos López <clopez@suse.de>
2026-01-14 13:25:55 +0100

    elf: update to Rust 2024 edition (format changes)
    Rust 2024 edition was released with version 1.85 of the language,
    which means it is available to us. So as to not fall behind, update the
    edition of different components in the repo to the latest version.
    
    This only contains formatting changes.
    
    Signed-off-by: Carlos López <clopez@suse.de>

--- elf/src/program_header.rs
+++ elf/src/program_header.rs
@@ -6,10 +6,10 @@
 //
 // vim: ts=4 sw=4 et
 
-use super::types::*;
 use super::Elf64AddrRange;
 use super::Elf64FileRange;
 use super::ElfError;
+use super::types::*;
 use bitflags::bitflags;
 
 bitflags! {
--- elf/src/syms.rs
+++ elf/src/syms.rs
@@ -6,8 +6,8 @@
 //
 // vim: ts=4 sw=4 et
 
-use super::types::*;
 use super::ElfError;
+use super::types::*;
 
 use core::ffi;
 
