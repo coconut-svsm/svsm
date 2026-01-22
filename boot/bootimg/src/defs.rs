1cc4c531194abf03558cab28f9a8e751fe0bd394
Jon Lange <jlange@microsoft.com>
2026-01-22 14:35:18 -0800

    bootimg: implement `Error` for boot image errors
    The boot image error is compact and useful for small environments like
    stage2, but it is more useful to implement `Error` to provide better
    error messages in other environments.
    
    Signed-off-by: Jon Lange <jlange@microsoft.com>

--- boot/bootimg/src/defs.rs
+++ boot/bootimg/src/defs.rs
@@ -4,27 +4,12 @@
 //
 // Author: Jon Lange <jlange@microsoft.com>
 
+use crate::error::BootImageError;
 use crate::page_tables::page_align_up;
 
 use bootdefs::boot_params::BootParamBlock;
 
 pub const PAGE_SIZE_2M: u64 = 0x00200000;
-
-/// Define a simple error type to describe the error results from boot image
-/// operations.
-#[derive(Clone, Copy, Debug)]
-pub enum BootImageError {
-    Elf,
-    ElfRelocs,
-    ElfAlignment,
-    ElfSymbols,
-    SelfMapConflict,
-    KernelRangeTooLarge,
-    BadKernelAddress,
-    KernelTooBig,
-    HeapTooSmall,
-    Host,
-}
 
 /// Page table self-map level 3 index
 pub const PGTABLE_LVL3_IDX_PTE_SELFMAP: usize = 493;
