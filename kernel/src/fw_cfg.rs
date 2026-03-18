6b2610e44c16a8c5b3192ce3ea83aecdf2637ab2
Luigi Leonardi <leonardi@redhat.com>
2026-03-18 12:54:17 +0100

    fw_cfg: remove virtio-MMIO device discovery
    Now that virtio-MMIO device enumeration uses the device tree
    ("virtio/mmio: switch device discovery from fw_cfg to device tree"),
    remove the now-unused `get_virtio_mmio_addresses()` function along with
    its supporting types (hardware_info module, FwCfgFileIterator,
    read_from_it helper).
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

--- kernel/src/fw_cfg.rs
+++ kernel/src/fw_cfg.rs
@@ -7,11 +7,9 @@
 extern crate alloc;
 
 use crate::error::SvsmError;
-use zerocopy::FromBytes;
 
 use super::io::IOPort;
 use alloc::string::String;
-use alloc::vec::Vec;
 use core::mem::size_of;
 
 const FW_CFG_CTL: u16 = 0x510;
@@ -21,27 +19,6 @@ const _FW_CFG_ID: u16 = 0x01;
 const FW_CFG_FILE_DIR: u16 = 0x19;
 
 const MAX_FW_CFG_FILES: u32 = 0x1000;
-
-mod hardware_info {
-    use zerocopy::{FromBytes, Immutable, KnownLayout};
-
-    pub const HW_INFO_FILE: &str = "etc/hardware-info";
-
-    pub const TYPE_SVSM_VIRTIO_MMIO: u64 = 0x1000;
-
-    #[derive(FromBytes, Debug, Immutable, KnownLayout)]
-    #[repr(C)]
-    pub struct Header {
-        pub hw_type: u64,
-        pub size: u64,
-    }
-
-    #[derive(FromBytes, Debug, Immutable, KnownLayout)]
-    #[repr(C)]
-    pub struct SimpleDevice {
-        pub mmio_address: u64,
-    }
-}
 
 #[non_exhaustive]
 #[derive(Debug)]
@@ -81,39 +58,6 @@ impl FwCfgFile {
     }
     pub fn selector(&self) -> u16 {
         self.selector
-    }
-}
-
-#[derive(Debug)]
-struct FwCfgFileIterator<'a> {
-    fw_cfg: &'a FwCfg<'a>,
-    file_size: usize,
-}
-
-impl<'a> FwCfgFileIterator<'a> {
-    pub fn new(fw_cfg: &'a FwCfg<'a>, file_name: &str) -> Result<Self, SvsmError> {
-        let file = fw_cfg.file_selector(file_name)?;
-        fw_cfg.select(file.selector);
-        Ok(Self {
-            fw_cfg,
-            file_size: file.size as usize,
-        })
-    }
-}
-
-impl Iterator for FwCfgFileIterator<'_> {
-    type Item = u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.file_size == 0 {
-            return None;
-        }
-        self.file_size -= 1;
-        Some(self.fw_cfg.read_u8())
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.file_size, Some(self.file_size))
     }
 }
 
@@ -178,30 +122,5 @@ impl<'a> FwCfg<'a> {
         }
 
         Err(SvsmError::FwCfg(FwCfgError::FileNotFound))
-    }
-
-    /// Try reading an instance of T from the iterator.
-    fn read_from_it<T: FromBytes>(i: &mut impl Iterator<Item = u8>) -> Result<T, FwCfgError> {
-        let buffer: Vec<u8> = i.take(size_of::<T>()).collect();
-        T::read_from_bytes(buffer.as_slice()).map_err(|_| FwCfgError::InvalidFormat)
-    }
-
-    pub fn get_virtio_mmio_addresses(&self) -> Result<Vec<u64>, SvsmError> {
-        use hardware_info::*;
-
-        let mut it = FwCfgFileIterator::new(self, HW_INFO_FILE)?;
-
-        let mut addresses: Vec<u64> = Vec::<u64>::new();
-
-        while let Ok(header) = Self::read_from_it::<Header>(&mut it) {
-            if header.hw_type == TYPE_SVSM_VIRTIO_MMIO {
-                addresses.push(Self::read_from_it::<SimpleDevice>(&mut it)?.mmio_address);
-            } else {
-                for _ in 0..header.size as usize {
-                    it.next();
-                }
-            }
-        }
-        Ok(addresses)
     }
 }
