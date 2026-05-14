31aebdd34b317a95284daa9dc76fe77960532f1a
Luigi Leonardi <leonardi@redhat.com>
2026-05-14 14:37:59 +0200

    scripts/launch_guest: remove sudo from QEMU invocation
    Always running QEMU as root via sudo grants unnecessary privileges.
    
    Remove quotes around the QEMU variable so that it can include a prefix
    like sudo (e.g. QEMU="sudo /path/to/qemu").
    
    If needed, the user can still prefix the QEMU variable or parameter
    with sudo, or set the permissions correctly.
    
    Suggested-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

--- scripts/launch_guest.sh
+++ scripts/launch_guest.sh
@@ -161,12 +161,6 @@ if [ ! -z "$IMAGE" ]; then
     -device scsi-hd,drive=disk0"
 fi
 
-if [ "$EUID" -ne 0 ] && [ "$ACCEL" != "tcg" ]; then
-	SUDO_CMD="sudo"
-else
-	SUDO_CMD=""
-fi
-
 echo "============================="
 echo "Launching SVSM guest"
 echo "============================="
@@ -190,8 +184,7 @@ if [ -t 0 ]; then
 fi
 
 # Temporarily use -vga none to avoid IGVM VGA init failure in QEMU 10.1
-$SUDO_CMD \
-  "$QEMU" \
+$QEMU \
     -cpu $CPU \
     -machine $MACHINE \
     -object $MEMORY \
