8944a6e30d057b8fb0dab1d6c0e665d1a3f51133
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-06-10 12:29:42 +0200

    kernel/protocols: Add `r9` to the request parameters
    The new observability and configuration protocol requires an additional
    register `r9` to exchange data between svsm and the guest os.
    
    Add it to the `RequestParams` structure to fulfill this requirement.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/mod.rs
+++ kernel/src/protocols/mod.rs
@@ -29,6 +29,7 @@ pub struct RequestParams {
     rcx: u64,
     rdx: u64,
     r8: u64,
+    r9: u64,
 }
 
 impl RequestParams {
@@ -38,6 +39,7 @@ impl RequestParams {
             rcx: vmsa.rcx,
             rdx: vmsa.rdx,
             r8: vmsa.r8,
+            r9: vmsa.r9,
         }
     }
 
@@ -45,6 +47,7 @@ impl RequestParams {
         res.rcx = Some(self.rcx);
         res.rdx = Some(self.rdx);
         res.r8 = Some(self.r8);
+        res.r9 = Some(self.r9);
     }
 }
 
@@ -55,6 +58,7 @@ pub struct RequestOutput {
     rcx: Option<u64>,
     rdx: Option<u64>,
     r8: Option<u64>,
+    r9: Option<u64>,
 }
 
 impl RequestOutput {
@@ -64,6 +68,7 @@ impl RequestOutput {
             rcx: None,
             rdx: None,
             r8: None,
+            r9: None,
         }
     }
 
@@ -87,6 +92,9 @@ impl RequestOutput {
         }
         if let Some(val) = self.r8 {
             vmsa.r8 = val;
+        }
+        if let Some(val) = self.r9 {
+            vmsa.r9 = val;
         }
     }
 }
