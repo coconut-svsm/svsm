289aabf2dd6798a1c94c2e91d116821d5ce534d4
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-06-10 13:00:35 +0200

    kernel/protocols: Add OCP source details
    Add a new structure, based on OCP protocol specifications.
    
    A source entry has a 4-byte bitfield flag field, where the least
    significant bit specifies if a sources is writable or not. The other
    bits are reserved.
    
    Also include a 2-byte `kind` field to store information on the source
    type. This will help the guest understand which data is transmitted
    and the dimension required for the buffer to store these values.
    
    A 2-byte padding is added and each entry should be exactly 128-bytes,
    so the remaining space is left for a human readable name (utf-8 encoded).
    
    Introduce a new const constructor which can be used for initializing
    a source. This function will fail if the name is greater than or equal
    to all its reserved bytes, as we want the name to be null terminated.
    If the name is less than that value, it will be padded with zeros. It
    will also fail if the name contains a '/', as this is used in the protocol
    calls to group objects and sources.
    
    As these sources will be passed to the linux guest, it is useful to
    derive `IntoBytes` and `Immutable` for the transfer.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

---
+++ kernel/src/protocols/ocp/mod.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (C) 2026 Nicola Ramacciotti
+//
+// Author: Nicola Ramacciotti <niko.ramak@gmail.com>
+
+//! OCP protocol implementation (SVSM draft spec).
+
+pub mod source;
