0d25c0f6cc068e5e85337382d812b23cbf34d611
Carlos López <clopez@suse.de>
2026-06-12 19:29:21 +0200

    kernel: virtio/hal: support multi-page DMA
    Now that SharedBox supports allocating dynamically-sized slices, the
    virtio HAL is able to allocate a dynamic amount of pages for DMA. Make
    use of SharedBox::try_new_slice() and remove the TODOs.
    
    Signed-off-by: Carlos López <clopez@suse.de>

--- kernel/src/virtio/hal.rs
+++ kernel/src/virtio/hal.rs
@@ -5,10 +5,11 @@
 // Author: Oliver Steffen <osteffen@redhat.com>
 
 extern crate alloc;
-use crate::{locking::SpinLock, platform::SVSM_PLATFORM};
+use crate::{locking::SpinLock, platform::SVSM_PLATFORM, types::PAGE_SHIFT};
 use alloc::vec::Vec;
 use core::{
     mem::{MaybeUninit, size_of},
+    num::NonZeroUsize,
     ptr::{NonNull, addr_of},
 };
 use zerocopy::{FromBytes, Immutable, IntoBytes};
@@ -19,7 +20,7 @@ use crate::{
 };
 
 struct PageStore {
-    pages: Vec<(PhysAddr, SharedBox<[u8; PAGE_SIZE]>)>,
+    pages: Vec<(PhysAddr, SharedBox<[u8]>)>,
 }
 
 impl PageStore {
@@ -27,11 +28,11 @@ impl PageStore {
         PageStore { pages: Vec::new() }
     }
 
-    pub fn push(&mut self, pa: PhysAddr, shared_page: SharedBox<[u8; PAGE_SIZE]>) {
-        self.pages.push((pa, shared_page));
+    pub fn push(&mut self, pa: PhysAddr, shared_pages: SharedBox<[u8]>) {
+        self.pages.push((pa, shared_pages));
     }
 
-    pub fn pop(&mut self, pa: PhysAddr) -> Option<SharedBox<[u8; PAGE_SIZE]>> {
+    pub fn pop(&mut self, pa: PhysAddr) -> Option<SharedBox<[u8]>> {
         if let Some(p) = self.pages.iter().position(|e| e.0 == pa) {
             Some(self.pages.remove(p).1)
         } else {
@@ -53,22 +54,17 @@ pub struct SvsmHal;
 unsafe impl virtio_drivers::Hal for SvsmHal {
     /// Allocates and zeroes the given number of contiguous physical pages of DMA memory for VirtIO
     /// use.
-    ///
-    /// Limitation: `pages == 1` required.
     fn dma_alloc(
         pages: usize,
         _direction: virtio_drivers::BufferDirection,
     ) -> (virtio_drivers::PhysAddr, NonNull<u8>) {
-        // TODO: allow more than one page.
-        //       This currently works, because in "modern" virtio mode the crate only allocates
-        //       one page at a time.
-        assert!(pages == 1);
+        let size = NonZeroUsize::new(pages << PAGE_SHIFT).unwrap();
+        let shared_pages = SharedBox::try_new_slice(0u8, size).unwrap();
 
-        let shared_page = SharedBox::<[u8; PAGE_SIZE]>::try_new_zeroed().unwrap();
-        let pa = virt_to_phys(shared_page.addr());
-        let p = NonNull::<u8>::new(shared_page.addr().as_mut_ptr()).unwrap();
+        let pa = virt_to_phys(shared_pages.addr());
+        let p = NonNull::<u8>::new(shared_pages.addr().as_mut_ptr()).unwrap();
 
-        SHARED_MEM.lock().push(pa, shared_page);
+        SHARED_MEM.lock().push(pa, shared_pages);
 
         (pa.into(), p)
     }
@@ -80,18 +76,13 @@ unsafe impl virtio_drivers::Hal for SvsmHal {
     /// The memory must have been allocated by `dma_alloc` on the same `Hal` implementation, and not
     /// yet deallocated. `pages` must be the same number passed to `dma_alloc` originally, and both
     /// `paddr` and `vaddr` must be the values returned by `dma_alloc`.
-    ///
-    /// Limitation: `pages == 1` required.
     unsafe fn dma_dealloc(
         paddr: virtio_drivers::PhysAddr,
         _vaddr: NonNull<u8>,
         pages: usize,
     ) -> i32 {
-        //TODO: allow more than one page
-        assert!(pages == 1);
-
-        SHARED_MEM.lock().pop(paddr.into()).unwrap();
-
+        let shared_pages = SHARED_MEM.lock().pop(paddr.into()).unwrap();
+        debug_assert_eq!(shared_pages.len() >> PAGE_SHIFT, pages);
         0
     }
 
@@ -108,20 +99,16 @@ unsafe impl virtio_drivers::Hal for SvsmHal {
     ///
     /// The buffer must be a valid pointer to a non-empty memory range which will not be accessed by
     /// any other thread for the duration of this method call.
-    ///
-    /// Limitation: `buffer.len() <= PAGE_SIZE`
     unsafe fn share(
         buffer: NonNull<[u8]>,
         direction: virtio_drivers::BufferDirection,
     ) -> virtio_drivers::PhysAddr {
-        // TODO: allow more than one page
-        assert!(buffer.len() <= PAGE_SIZE);
-
-        let shared_page = SharedBox::<[u8; PAGE_SIZE]>::try_new_zeroed().unwrap();
+        let size = NonZeroUsize::new(buffer.len()).unwrap();
+        let shared_pages = SharedBox::try_new_slice(0u8, size).unwrap();
 
         if direction == virtio_drivers::BufferDirection::DriverToDevice {
             let src = buffer.as_ptr().cast::<u8>();
-            let dst = shared_page.addr().as_mut_ptr::<u8>();
+            let dst = shared_pages.addr().as_mut_ptr::<u8>();
 
             // SAFETY: We demand a valid `buffer` from the caller (virtio-drivers crate).
             //         We assterted that `dst` can hold at least `buffer.len()`.
@@ -130,8 +117,8 @@ unsafe impl virtio_drivers::Hal for SvsmHal {
             }
         }
 
-        let pa = virt_to_phys(shared_page.addr());
-        SHARED_MEM.lock().push(pa, shared_page);
+        let pa = virt_to_phys(shared_pages.addr());
+        SHARED_MEM.lock().push(pa, shared_pages);
 
         // return pa of shared page
         pa.into()
@@ -145,15 +132,11 @@ unsafe impl virtio_drivers::Hal for SvsmHal {
     /// The buffer must be a valid pointer to a non-empty memory range which will not be accessed by
     /// any other thread for the duration of this method call. The `paddr` must be the value
     /// previously returned by the corresponding `share` call.
-    ///
-    /// Limitation: `buffer.len() <= PAGE_SIZE`
     unsafe fn unshare(
         paddr: virtio_drivers::PhysAddr,
         buffer: NonNull<[u8]>,
         direction: virtio_drivers::BufferDirection,
     ) {
-        assert!(buffer.len() <= PAGE_SIZE);
-
         match SHARED_MEM.lock().pop(paddr.into()) {
             Some(shared_page) => {
                 let vaddr = phys_to_virt(paddr.into());
