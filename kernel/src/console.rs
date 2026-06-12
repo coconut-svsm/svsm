c66660b8be4fed55db03c70fa8a05e1fcdf72970
Vasant Karasulli <vasant.karasulli@suse.com>
2026-06-12 09:57:40 +0200

    console: remove unused symbols
    Many variables are not used since we switched to log buffer mechanism.
    Remove them as suggested by Nicola Ramacciotti.
    
    Signed-off-by: Vasant Karasulli <vasant.karasulli@suse.com>

--- kernel/src/console.rs
+++ kernel/src/console.rs
@@ -8,7 +8,7 @@ use crate::error::SvsmError;
 use crate::io::{IOPort, Write};
 use crate::locking::SpinLock;
 use crate::serial::SerialPort;
-use crate::utils::immut_after_init::{ImmutAfterInitCell, ImmutAfterInitResult};
+use crate::utils::immut_after_init::ImmutAfterInitCell;
 use core::fmt;
 
 /// A console device to output data
@@ -64,82 +64,4 @@ pub fn console_write(buffer: &[u8]) {
     if let Ok(writer) = WRITER.try_get_inner() {
         writer.lock().write_bytes(buffer);
     }
-}
-
-#[derive(Clone, Copy, Debug)]
-struct ConsoleLogger {
-    name: &'static str,
-}
-
-impl ConsoleLogger {
-    const fn new(name: &'static str) -> Self {
-        Self { name }
-    }
-}
-
-impl log::Log for ConsoleLogger {
-    fn enabled(&self, _metadata: &log::Metadata<'_>) -> bool {
-        true
-    }
-
-    fn log(&self, record: &log::Record<'_>) {
-        if !self.enabled(record.metadata()) {
-            return;
-        }
-
-        // The logger being uninitialized is impossible, as that would mean it
-        // wouldn't have been registered with the log library.
-        // Log format/detail depends on the level.
-        match record.metadata().level() {
-            log::Level::Error | log::Level::Warn => {
-                _print(format_args!(
-                    "[{}] {}: {}\n",
-                    self.name,
-                    record.metadata().level().as_str(),
-                    record.args()
-                ));
-            }
-
-            log::Level::Info => {
-                _print(format_args!("[{}] {}\n", self.name, record.args()));
-            }
-
-            log::Level::Debug | log::Level::Trace => {
-                _print(format_args!(
-                    "[{}/{}] {} {}\n",
-                    self.name,
-                    record.metadata().target(),
-                    record.metadata().level().as_str(),
-                    record.args()
-                ));
-            }
-        };
-    }
-
-    fn flush(&self) {}
-}
-
-static CONSOLE_LOGGER: ImmutAfterInitCell<ConsoleLogger> = ImmutAfterInitCell::uninit();
-
-pub fn install_console_logger(component: &'static str) -> ImmutAfterInitResult<()> {
-    CONSOLE_LOGGER.init_from_ref(&ConsoleLogger::new(component))?;
-
-    if let Err(e) = log::set_logger(&*CONSOLE_LOGGER) {
-        // Failed to install the ConsoleLogger, presumably because something had
-        // installed another logger before. No logs will appear at the console.
-        // Print an error string.
-        _print(format_args!(
-            "[{component}]: ERROR: failed to install console logger: {e:?}"
-        ));
-    }
-
-    // Log levels are to be configured via the log's library feature configuration.
-    log::set_max_level(log::LevelFilter::Trace);
-    Ok(())
-}
-
-#[macro_export]
-macro_rules! println {
-    () => (log::info!(""));
-    ($($arg:tt)*) => (log::info!($($arg)*));
 }
