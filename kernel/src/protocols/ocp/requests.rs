4690e7147db3f3977262d59d63cb819bbd6b4e73
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-06-19 12:13:38 +0200

    kernel/protocols: Implement OCP read request handler
    Add the new call identifier (`1`) to the OCP handler and implement
    `SVSM_OCP_READ` following the spec.
    
    Use the new utility to extract the source and check if the provided
    parameters are correct.
    
    Since each source is required to implement the `OcpSource` trait, the read
    method can be used directly.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/requests.rs
+++ kernel/src/protocols/ocp/requests.rs
@@ -23,6 +23,7 @@ use core::ffi::CStr;
 
 // OCP protocol services
 const SVSM_OCP_LIST: u32 = 0;
+const SVSM_OCP_READ: u32 = 1;
 
 const LOW_32_BITS: u64 = 0xffff_ffff;
 const OCP_BUFFER_MAX_SIZE: usize = PAGE_SIZE;
@@ -199,9 +200,43 @@ fn ocp_list_request(params: &mut RequestParams) -> Result<(), SvsmReqError> {
     Ok(())
 }
 
+fn ocp_read_request(params: &mut RequestParams) -> Result<(), SvsmReqError> {
+    let gpa_buffer = PhysAddr::from(params.rdx);
+
+    if !gpa_buffer.is_aligned(OCP_BUFFER_ALIGNMENT) {
+        return Err(SvsmReqError::invalid_address());
+    }
+
+    let gpa_id = PhysAddr::from(params.rcx);
+    let num_bytes = (params.r8 & LOW_32_BITS) as u32;
+    let offset = (params.r9 & LOW_32_BITS) as u32;
+
+    if num_bytes as usize > OCP_BUFFER_MAX_SIZE {
+        return Err(SvsmReqError::invalid_parameter());
+    }
+
+    let source = get_requested_source(gpa_id)?;
+
+    if !source.get_info().is_valid_access(offset, num_bytes) {
+        return Err(SvsmReqError::invalid_parameter());
+    }
+
+    if num_bytes == 0 {
+        params.r8 = 0;
+        return Ok(());
+    }
+
+    let bytes_copied = source.write_to_guest(offset, gpa_buffer, num_bytes)?;
+
+    params.r8 = bytes_copied as u64;
+
+    Ok(())
+}
+
 pub fn ocp_protocol_request(request: u32, params: &mut RequestParams) -> Result<(), SvsmReqError> {
     match request {
         SVSM_OCP_LIST => ocp_list_request(params),
+        SVSM_OCP_READ => ocp_read_request(params),
         _ => Err(SvsmReqError::unsupported_call()),
     }
 }
