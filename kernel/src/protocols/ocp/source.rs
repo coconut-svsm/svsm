8058cedaf839a15548bff3bb94f179be462e3eab
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-06-25 11:23:21 +0200

    kernel/protocols: Add OCP traits
    Add two new traits, one for object and one for sources. Each object and
    source must implement the corresponding one to be added to the protocol.
    
    The traits include default implementations for the `read` and `write`
    methods, as each object or source may not have that functionality, in
    which case an error is returned.
    
    The traits also include a way to retrieve references to the sources. This
    can be useful for iterating over the list of sources, getting the entry
    and assembling the reply to the guest OS. It also includes a way to
    retrieve information about each object, so that they can be sent back to
    the guest.
    
    Require the `Sync` bound as we want to share objects between threads
    safely. Require `Send` and `Debug` for completeness.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/source.rs
+++ kernel/src/protocols/ocp/source.rs
@@ -4,9 +4,14 @@
 //
 // Author: Nicola Ramacciotti <niko.ramak@gmail.com>
 
+extern crate alloc;
+
+use alloc::sync::Arc;
 use bitfield_struct::bitfield;
-use core::{ffi::CStr, mem};
+use core::{ffi::CStr, fmt::Debug, mem, slice::Iter};
 use zerocopy::{Immutable, IntoBytes};
+
+use crate::{address::PhysAddr, protocols::errors::SvsmReqError};
 
 pub const OCP_NAME_LEN: usize = 120;
 pub const OCP_IDENTIFIER_LEN: usize = OCP_NAME_LEN * 2;
@@ -109,3 +114,26 @@ const _: () = assert!(
         && mem::offset_of!(OcpSourceInfo, name) == 0x08
         && mem::size_of::<OcpSourceInfo>() == OCP_SOURCE_SIZE
 );
+
+/// Operations required for an OCP object
+pub trait OcpObject: Debug + Send + Sync {
+    fn get_name(&self) -> &str;
+    fn get_info(&self) -> &OcpSourceInfo;
+    fn get_source(&self, name: &str) -> Option<Arc<dyn OcpSource>>;
+    // todo: change return type of get_sources
+    fn get_sources(&self) -> Iter<'_, Arc<dyn OcpSource>>;
+    fn get_source_count(&self) -> usize;
+}
+
+/// Operations required for an OCP source
+pub trait OcpSource: Debug + Send + Sync {
+    fn read(&self, _offset: u32, _gpa: PhysAddr, _size: u32) -> Result<u32, SvsmReqError> {
+        Err(SvsmReqError::unsupported_call())
+    }
+
+    fn write(&self, _offset: u32, _gpa: PhysAddr, _size: u32) -> Result<u32, SvsmReqError> {
+        Err(SvsmReqError::unsupported_call())
+    }
+
+    fn get_info(&self) -> &OcpSourceInfo;
+}
