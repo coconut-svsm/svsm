f828347387561961a13c5d219af708c70b3e820e
Luigi Leonardi <leonardi@redhat.com>
2026-07-07 10:26:26 +0200

    bldr: always pass --build-id=none linker flag
    The build script only passed --build-id=none when the target was
    x86_64-unknown-none. Since bldr is only ever built for this target,
    the conditional is unnecessary. Remove it.
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

--- boot/bldr/build.rs
+++ boot/bldr/build.rs
@@ -5,12 +5,7 @@
 // Author: Joerg Roedel <jroedel@suse.de>
 
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
-
-    if target == "x86_64-unknown-none" {
-        println!("cargo:rustc-link-arg=--build-id=none");
-    }
-
+    println!("cargo:rustc-link-arg=--build-id=none");
     println!("cargo:rustc-link-arg=-nostdlib");
     println!("cargo:rustc-link-arg=-Tboot/bldr/src/bldr.lds");
     println!("cargo:rustc-link-arg=-no-pie");
