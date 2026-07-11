17fdd6d50114b420c757444377f8d1fbb571602c
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-07-11 15:10:57 +0200

    kernel/log: Remove userspace console handler
    The `ObjHandle` of the console is only used by the userspace. kernel tasks
    don't need that handler and can directly call `console_write` if it is
    really needed to print to the console.
    
    Additionally, with that handler, the userspace tasks could potentially
    write directly to the console, bypassing the kernel feature
    `enable-console-log`.
    
    This is not a good approach, as the userspace should leave the final
    decision to the kernel.
    
    Remove the console handler in the kernel side, leaving only log buffer
    handling. Now that there is only one handler, the log one can be updated
    to use 0 as object identifier instead of 1. Update the `syscall` side
    accordingly.
    
    After removing `LineBuffer.is_console`, the size of `LineBuffer.buffer`
    can be restored to its previous value, while keeping the size of the
    whole struct exactly 256 bytes. Add an assertion for robustness.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/fs/log_buffer.rs
+++ kernel/src/fs/log_buffer.rs
@@ -36,9 +36,9 @@ struct LogFile {
 }
 
 impl LogFile {
-    fn new<S: AsRef<str>>(component: S, is_console: bool) -> Self {
+    fn new<S: AsRef<str>>(component: S) -> Self {
         Self {
-            lb: SpinLock::new(LineBuffer::new(component, is_console)),
+            lb: SpinLock::new(LineBuffer::new(component)),
         }
     }
 }
@@ -69,15 +69,11 @@ impl File for LogFile {
     }
 }
 
-pub fn stdout_open(taskname: &str) -> (Arc<dyn Obj>, Arc<dyn Obj>) {
-    let console_file: Arc<dyn File> = Arc::new(LogFile::new(taskname, true));
-    let log_file: Arc<dyn File> = Arc::new(LogFile::new(taskname, false));
+pub fn stdout_open(taskname: &str) -> Arc<dyn Obj> {
+    let log_file: Arc<dyn File> = Arc::new(LogFile::new(taskname));
 
     // Stdout is write-only.
-    (
-        Arc::new(FsObj::new_file(FileHandle::new(&console_file, false, true))),
-        Arc::new(FsObj::new_file(FileHandle::new(&log_file, false, true))),
-    )
+    Arc::new(FsObj::new_file(FileHandle::new(&log_file, false, true)))
 }
 
 #[cfg(all(test, test_in_svsm))]
