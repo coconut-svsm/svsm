2b50ec984d23a2bc225614c29de9561f19cab6e2
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-07-14 17:32:25 +0200

    kernel/protocols: Implement OCP list objects request handler
    Add the new call identifier (`0`) to the OCP handler and implement
    `SVSM_OCP_LIST_OBJECTS` following the spec.
    
    If some guest params are incorrect, like buffer alignment or zero bytes
    requested, an appropriate error will be returned.
    
    The index and the buffer len are 4-bytes, so make sure to use only the low
    32 bits.
    
    Compute the number of entries required and use `GuestPtr<[T]>` to access
    safely the untrusted guest region.
    
    As this call requires SVSM to provide a list of entries, extract them from
    the map with the new utility and use `get_object_details` method from the
    new trait to retrieve object info.
    
    Once an entry is available, it is possible to copy it directly to the guest
    memory without using temporary heap allocation to store all the entries
    together.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/requests.rs
+++ kernel/src/protocols/ocp/requests.rs
@@ -4,8 +4,77 @@
 //
 // Author: Nicola Ramacciotti <niko.ramak@gmail.com>
 
+use crate::{
+    address::{Address, PhysAddr},
+    mm::ptguards::PerCPUPageMappingGuard,
+    protocols::{RequestParams, errors::SvsmReqError},
+    types::PAGE_SIZE,
+};
+
+use super::details::{OCP_OBJECT_DETAILS_SIZE, OcpObjectDetails};
+use super::map::explore_map;
+
+// OCP protocol services
+const SVSM_OCP_LIST_OBJECTS: u32 = 0;
+
+const LOW_32_BITS: u64 = 0xffff_ffff;
+const OCP_BUFFER_MAX_SIZE: usize = PAGE_SIZE;
+const OCP_BUFFER_ALIGNMENT: usize = 8;
+
+fn ocp_list_objects_request(params: &mut RequestParams) -> Result<(), SvsmReqError> {
+    let gpa_buffer = PhysAddr::from(params.rdx);
+
+    if !gpa_buffer.is_aligned(OCP_BUFFER_ALIGNMENT) {
+        return Err(SvsmReqError::invalid_address());
+    }
+
+    let buffer_size = (params.r8 & LOW_32_BITS) as usize;
+    let first = (params.rcx & LOW_32_BITS) as u32;
+
+    if buffer_size == 0 || buffer_size > OCP_BUFFER_MAX_SIZE {
+        return Err(SvsmReqError::invalid_parameter());
+    }
+
+    if buffer_size % OCP_OBJECT_DETAILS_SIZE != 0 {
+        return Err(SvsmReqError::invalid_parameter());
+    }
+
+    let num_entries = (buffer_size / OCP_OBJECT_DETAILS_SIZE) as u32;
+
+    let guard = PerCPUPageMappingGuard::create(
+        gpa_buffer.page_align(),
+        gpa_buffer
+            .checked_add(buffer_size)
+            .ok_or(SvsmReqError::invalid_address())?
+            .page_align_up(),
+        0,
+    )?;
+
+    let entries_ptr =
+        guard.guest_slice::<OcpObjectDetails>(gpa_buffer.page_offset(), num_entries as usize)?;
+
+    let mut entries_written = 0;
+
+    for entry in explore_map(first, num_entries)?.iter() {
+        // fixme: is it correct that the write can fail mid loop
+        // and some entries have already been written?
+        if entries_ptr
+            .write(entries_written, entry.get_object_details())
+            .is_err()
+        {
+            break;
+        }
+        entries_written += 1;
+    }
+
+    params.rcx = (entries_written * OCP_OBJECT_DETAILS_SIZE) as u64;
+
+    Ok(())
+}
+
 pub fn ocp_protocol_request(request: u32, params: &mut RequestParams) -> Result<(), SvsmReqError> {
     match request {
+        SVSM_OCP_LIST_OBJECTS => ocp_list_objects_request(params),
         _ => Err(SvsmReqError::unsupported_call()),
     }
 }
