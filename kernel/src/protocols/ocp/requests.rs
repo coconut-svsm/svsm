d5215cc1225ad22eb1f53516bfab55d0e60f5f20
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-07-15 15:19:26 +0200

    kernel/protocols: Add OCP dyn trait map
    Add a vec to keep track of all the objects that implement the new trait.
    
    Since most of the access are read, use `RWLock` to lock the access to
    the vec. Add utilities to interact with objects.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

---
+++ kernel/src/protocols/ocp/requests.rs
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (C) 2026 Nicola Ramacciotti
+//
+// Author: Nicola Ramacciotti <niko.ramak@gmail.com>
+
+extern crate alloc;
+
+use alloc::{sync::Arc, vec::Vec};
+
+use super::source::OcpObject;
+use crate::locking::RWLock;
+
+static OCP_OBJECTS: RWLock<Vec<Arc<dyn OcpObject>>> = RWLock::new(Vec::new());
+
+pub fn add_ocp_object(object: Arc<dyn OcpObject>) {
+    // todo: check unique name here
+    let mut objects = OCP_OBJECTS.lock_write();
+    objects.push(object);
+}
+
+pub fn get_ocp_object(name: &str) -> Option<Arc<dyn OcpObject>> {
+    let objects = OCP_OBJECTS.lock_read();
+    for obj in objects.iter() {
+        if obj.get_name() == name {
+            return Some(obj.clone());
+        }
+    }
+    None
+}
