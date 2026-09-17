78b8087e8b41e887bd4d7a09d6a02beacd90b6da
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-09-17 15:02:11 +0200

    kernel/protocols: Extract source from guest data
    The protocol uses OCP names to request sources or objects.
    
    A single name is sufficient for objects, but sources require an identifier
    composed of both the object and source names separated by '/'.
    
    The guest OS puts the GPA of the name in the request parameters. Add two
    utility functions to retrieve the name and extract the corresponding source
    or object.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/requests.rs
+++ kernel/src/protocols/ocp/requests.rs
@@ -8,11 +8,15 @@ extern crate alloc;
 
 use alloc::{sync::Arc, vec::Vec};
 
-use super::source::OcpObject;
+use super::source::{OCP_IDENTIFIER_LEN, OCP_NAME_LEN, OcpObject, OcpSource};
 use crate::{
+    address::{Address, PhysAddr},
     locking::RWLock,
+    mm::guestmem::read_from_guest,
     protocols::{RequestParams, errors::SvsmReqError},
 };
+
+use core::ffi::CStr;
 
 static OCP_OBJECTS: RWLock<Vec<Arc<dyn OcpObject>>> = RWLock::new(Vec::new());
 
@@ -38,6 +42,37 @@ pub fn get_ocp_object(name: &str) -> Option<Arc<dyn OcpObject>> {
         }
     }
     None
+}
+
+/// Retrieve the object name from guest memory and extract the corresponding object
+fn get_requested_object(gpa_obj_name: PhysAddr) -> Result<Arc<dyn OcpObject>, SvsmReqError> {
+    let name_slice = read_from_guest::<[u8; OCP_NAME_LEN]>(gpa_obj_name)?;
+
+    let obj_name = CStr::from_bytes_until_nul(&name_slice)
+        .ok()
+        .and_then(|s| s.to_str().ok())
+        .ok_or(SvsmReqError::invalid_parameter())?;
+
+    get_ocp_object(obj_name).ok_or(SvsmReqError::invalid_parameter())
+}
+
+/// Retrieve the identifier name from guest memory and extract the corresponding source
+fn get_requested_source(gpa_id: PhysAddr) -> Result<Arc<dyn OcpSource>, SvsmReqError> {
+    let id_slice = read_from_guest::<[u8; OCP_IDENTIFIER_LEN]>(gpa_id)?;
+
+    let (obj_name, source_name) = CStr::from_bytes_until_nul(&id_slice)
+        .ok()
+        .and_then(|s| s.to_str().ok())
+        .and_then(|s| s.split_once('/'))
+        .ok_or(SvsmReqError::invalid_parameter())?;
+
+    let Some(object) = get_ocp_object(obj_name) else {
+        return Err(SvsmReqError::invalid_parameter());
+    };
+
+    object
+        .get_source(source_name)
+        .ok_or(SvsmReqError::invalid_parameter())
 }
 
 pub fn ocp_protocol_request(request: u32, _params: &mut RequestParams) -> Result<(), SvsmReqError> {
