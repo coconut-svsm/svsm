c9092673ae79068cc9b110a9ecc8a608aba05582
Nicola Ramacciotti <niko.ramak@gmail.com>
2026-09-17 15:02:11 +0200

    kernel/protocols: Extract source from guest data
    The protocol uses OCP names to request sources or objects.
    
    A single name is sufficient for objects, but sources require an identifier
    composed of both the object and source names separated by '/'.
    
    The guest OS puts the GPA of the name in the request parameters. Add two
    utility functions to retrieve the name and extract the corresponding source
    or object.
    
    Signed-off-by: Nicola Ramacciotti <niko.ramak@gmail.com>

--- kernel/src/protocols/ocp/requests.rs
+++ kernel/src/protocols/ocp/requests.rs
@@ -8,11 +8,15 @@ extern crate alloc;
 
 use alloc::{sync::Arc, vec::Vec};
 
-use super::source::OcpObject;
+use super::source::{OCP_IDENTIFIER_LEN, OCP_NAME_LEN, OcpObject, OcpSource};
 use crate::{
+    address::{Address, PhysAddr},
     locking::RWLock,
+    mm::ptguards::PerCPUPageMappingGuard,
     protocols::{RequestParams, errors::SvsmReqError},
 };
+
+use core::ffi::CStr;
 
 static OCP_OBJECTS: RWLock<Vec<Arc<dyn OcpObject>>> = RWLock::new(Vec::new());
 
@@ -30,6 +34,61 @@ pub fn get_ocp_object(name: &str) -> Option<Arc<dyn OcpObject>> {
         }
     }
     None
+}
+
+/// Retrieve the object name from guest memory and extract the corresponding object
+fn get_requested_object(gpa_obj_name: PhysAddr) -> Result<Arc<dyn OcpObject>, SvsmReqError> {
+    let mut name_slice = [0u8; OCP_NAME_LEN];
+    let name_guard = PerCPUPageMappingGuard::create(
+        gpa_obj_name.page_align(),
+        gpa_obj_name
+            .checked_add(OCP_NAME_LEN)
+            .ok_or(SvsmReqError::invalid_address())?
+            .page_align_up(),
+        0,
+    )?;
+
+    let name_ptr = name_guard.guest_slice::<u8>(gpa_obj_name.page_offset(), OCP_NAME_LEN)?;
+    name_ptr.read_to_slice(&mut name_slice)?;
+
+    let obj_name = CStr::from_bytes_until_nul(&name_slice)
+        .map_err(|_| SvsmReqError::invalid_parameter())?
+        .to_str()
+        .map_err(|_| SvsmReqError::invalid_parameter())?;
+
+    get_ocp_object(obj_name).ok_or(SvsmReqError::invalid_parameter())
+}
+
+/// Retrieve the identifier name from guest memory and extract the corresponding source
+fn get_requested_source(gpa_id: PhysAddr) -> Result<Arc<dyn OcpSource>, SvsmReqError> {
+    let mut id_slice = [0u8; OCP_IDENTIFIER_LEN];
+
+    let id_guard = PerCPUPageMappingGuard::create(
+        gpa_id.page_align(),
+        gpa_id
+            .checked_add(OCP_IDENTIFIER_LEN)
+            .ok_or(SvsmReqError::invalid_address())?
+            .page_align_up(),
+        0,
+    )?;
+
+    let id_ptr = id_guard.guest_slice::<u8>(gpa_id.page_offset(), OCP_IDENTIFIER_LEN)?;
+    id_ptr.read_to_slice(&mut id_slice)?;
+
+    let (obj_name, source_name) = CStr::from_bytes_until_nul(&id_slice)
+        .map_err(|_| SvsmReqError::invalid_parameter())?
+        .to_str()
+        .map_err(|_| SvsmReqError::invalid_parameter())?
+        .split_once('/')
+        .ok_or(SvsmReqError::invalid_parameter())?;
+
+    let Some(object) = get_ocp_object(obj_name) else {
+        return Err(SvsmReqError::invalid_parameter());
+    };
+
+    object
+        .get_source(source_name)
+        .ok_or(SvsmReqError::invalid_parameter())
 }
 
 pub fn ocp_protocol_request(request: u32, _params: &mut RequestParams) -> Result<(), SvsmReqError> {
